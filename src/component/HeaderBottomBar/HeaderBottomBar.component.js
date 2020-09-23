4a6728200555f3f3d31d5cddb01d9cd6d541ea6d
artiskupcs <artiskupcs@scandiweb.com>
2020-09-23 14:15:46 +0300

    SIXTH-153 -implemented footer in navigation sidebar -polished code

--- src/component/HeaderBottomBar/HeaderBottomBar.component.js
+++ src/component/HeaderBottomBar/HeaderBottomBar.component.js
@@ -1,3 +1,5 @@
+import FooterMain from 'Component/FooterMain';
+import FooterMiddle from 'Component/FooterMiddle';
 import HeaderMenu from 'Component/HeaderMenu';
 import HeaderSearch from 'Component/HeaderSearch';
 import NavigationAbstract from 'Component/NavigationAbstract/NavigationAbstract.component';
@@ -16,6 +18,10 @@ class HeaderBottomBar extends NavigationAbstract {
     renderMap = {
         menu: this.renderMenu.bind(this),
         search: this.renderSearch.bind(this)
+    };
+
+    state = {
+        isMenuOn: false
     };
 
     renderMenu() {
@@ -40,13 +46,18 @@ class HeaderBottomBar extends NavigationAbstract {
     }
 
     render() {
+        console.log(this.state.isMenuOn);
         return (
-            <div block="HeaderBottomBar">
+            <div
+              block="HeaderBottomBar"
+            >
                 <div
                   block="HeaderBottomBar"
                   elem="Content"
                 >
                     { this.renderNavigationState() }
+                    <FooterMain />
+                    <FooterMiddle />
                 </div>
             </div>
         );
