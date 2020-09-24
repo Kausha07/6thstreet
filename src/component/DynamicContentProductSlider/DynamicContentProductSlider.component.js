107eac946cb59e7a5db4f041dcc9355dee32aada
karlisruza <ruza.karlis@gmail.com>
2020-09-24 13:55:03 +0300

    SIXTH-42 // Fixed arabic layout issues

--- src/component/DynamicContentProductSlider/DynamicContentProductSlider.component.js
+++ src/component/DynamicContentProductSlider/DynamicContentProductSlider.component.js
@@ -1,68 +1,208 @@
+import Slider from '@scandipwa/scandipwa/src/component/Slider';
+import isMobile from '@scandipwa/scandipwa/src/util/Mobile/isMobile';
 import PropTypes from 'prop-types';
 import { PureComponent } from 'react';
 
-import Link from 'Component/Link';
 import ProductItem from 'Component/ProductItem';
 import { Products } from 'Util/API/endpoint/Product/Product.type';
+
+import { ITEMS_PER_PAGE } from './DynamicContentProductSlider.config';
 
 import './DynamicContentProductSlider.style';
 
 class DynamicContentProductSlider extends PureComponent {
     static propTypes = {
         title: PropTypes.string.isRequired,
-        data_url: PropTypes.string.isRequired,
         isLoading: PropTypes.bool.isRequired,
         products: Products.isRequired
     };
 
+    constructor() {
+        super();
+        this.state = {
+            currentPage: 0,
+            isArabic: JSON.parse(localStorage.getItem('APP_STATE_CACHE_KEY')).data.language === 'ar'
+        };
+    }
+
     renderProduct = (product) => {
         const { sku } = product;
+        const { isArabic } = this.state;
+        const newTag = (
+        <div
+          mix={ {
+              block: 'DynamicContentProductSlider',
+              elem: 'TagOverlay',
+              mods: { isArabic }
+          } }
+        >
+            New
+        </div>
+        );
 
+        // TODO: remove if statement and add appropriate query for items with new
         return (
-            <ProductItem
-              product={ product }
-              key={ sku }
-            />
+            <div mix={ { block: 'DynamicContentProductSlider', elem: 'ProductItem', mods: { isArabic } } }>
+                <ProductItem
+                  product={ product }
+                  key={ sku }
+                />
+                { 1 ? newTag : null }
+                { this.renderCTA() }
+            </div>
         );
     };
 
-    renderProducts() {
+    renderProductsDesktop() {
         const {
             isLoading,
             products
         } = this.props;
+        const { currentPage } = this.state;
 
         if (isLoading) {
             return 'loading...';
         }
+        const productArray = products.map(this.renderProduct);
+        const lastPage = parseInt(Math.floor(products.length / ITEMS_PER_PAGE), 10); // first page is 0
+        const lastPageItemCount = products.length % ITEMS_PER_PAGE; // number of products on last page'
+        if (currentPage !== lastPage) {
+            return productArray.slice(currentPage * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE + ITEMS_PER_PAGE);
+        }
 
-        return products.map(this.renderProduct);
+        return productArray.slice(currentPage * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE + lastPageItemCount);
     }
 
     renderTitle() {
         const { title } = this.props;
 
         return (
-            <h4>{ title }</h4>
+            <h2 mix={ { block: 'DynamicContentProductSlider', elem: 'Header' } }>{ title }</h2>
         );
     }
 
     renderCTA() {
-        const { data_url } = this.props;
-
+        const { isArabic } = this.state;
         return (
-            <Link to={ data_url }>
-                See All
-            </Link>
+            <div mix={ { block: 'DynamicContentProductSlider', elem: 'FavoriteOverlay', mods: { isArabic } } } />
         );
     }
 
-    render() {
+    renderButtonNext() {
+        const {
+            isLoading,
+            products
+        } = this.props;
+        const { currentPage, isArabic } = this.state;
+
+        if (isLoading) {
+            return null;
+        }
+        const lastPage = parseInt(Math.floor(products.length / ITEMS_PER_PAGE), 10); // first page is 0
+        if (currentPage !== lastPage) {
+            return (
+                <button
+                  onClick={ this.handleClickNext }
+                  mix={ {
+                      block: 'DynamicContentProductSlider',
+                      elem: 'ButtonNext',
+                      mods: { isArabic }
+                  } }
+                >
+                    &gt;
+                </button>
+            );
+        }
+
+        return null;
+    }
+
+    renderButtonPrev() {
+        const { isLoading } = this.props;
+        const { currentPage, isArabic } = this.state;
+
+        if (isLoading) {
+            return null;
+        }
+        if (currentPage !== 0) {
+            return (
+                <button
+                  onClick={ this.handleClickPrev }
+                  mix={ {
+                      block: 'DynamicContentProductSlider',
+                      elem: 'ButtonPrev',
+                      mods: { isArabic }
+                  } }
+                >
+                    &lt;
+                </button>
+            );
+        }
+
+        return null;
+    }
+
+    handleClickNext = () => {
+        const { currentPage } = this.state;
+        this.setState({ currentPage: currentPage + 1 });
+    };
+
+    handleClickPrev = () => {
+        const { currentPage } = this.state;
+        this.setState({ currentPage: currentPage - 1 });
+    };
+
+    renderProductsMobile() {
+        const {
+            isLoading,
+            products
+        } = this.props;
+        const {
+            currentPage,
+            isArabic
+        } = this.state;
+
+        if (isLoading) {
+            return 'loading...';
+        }
+
         return (
-            <div block="DynamicContentProductSlider">
-                { this.renderTitle() }
-                { this.renderProducts() }
-                { this.renderCTA() }
+            <Slider
+              mix={ { block: 'DynamicContentProductSlider', elem: 'MobileSlider', mods: { isArabic } } }
+              activeImage={ currentPage }
+              onActiveImageChange={ this.mobileSliderCallback }
+            >
+                { products.map(this.renderProduct) }
+            </Slider>
+        );
+    }
+
+    mobileSliderCallback = (newPage) => {
+        this.setState({ currentPage: newPage });
+    };
+
+    render() {
+        const { isArabic } = this.state;
+        const products = (
+            <div mix={ { block: 'DynamicContentProductSlider', elem: 'ProductContainer', mods: { isArabic } } }>
+                { this.renderButtonPrev() }
+                { this.renderProductsDesktop() }
+                { this.renderButtonNext() }
+            </div>
+        );
+
+        return (
+            <div mix={ { block: 'DynamicContentProductSlider', mods: { isArabic } } }>
+                <hr />
+                <div mix={ { block: 'DynamicContentProductSlider', elem: 'HeaderContainer', mods: { isArabic } } }>
+                    { this.renderTitle() }
+                    <span
+                      mix={ { block: 'DynamicContentProductSlider', elem: 'SubHeader', mods: { isArabic } } }
+                    >
+                        Latest trends, new looks, must have... don&apos;t miss it
+                    </span>
+                </div>
+                { isMobile.any() ? this.renderProductsMobile() : products }
             </div>
         );
     }
