68f0d909182f72d652d85f0db5ef002803351325
Deniss <deniss.tarasevics@gmail.com>
2020-09-28 13:31:00 +0300

    added PDPDetails container and icons

---
+++ src/component/PDPDetailsSection/PDPDetailsSection.container.js
@@ -0,0 +1,41 @@
+// import PropTypes from 'prop-types';
+import { PureComponent } from 'react';
+import { connect } from 'react-redux';
+
+import { Product } from 'Util/API/endpoint/Product/Product.type';
+
+import PDPDetailsSection from './PDPDetailsSection.component';
+
+export const mapStateToProps = (state) => ({
+    product: state.PDP.product
+});
+
+export const mapDispatchToProps = (_dispatch) => ({
+    // addProduct: options => CartDispatcher.addProductToCart(dispatch, options)
+});
+
+export class PDPDetailsSectionContainer extends PureComponent {
+    static propTypes = {
+        product: Product.isRequired
+    };
+
+    containerFunctions = {
+        // getData: this.getData.bind(this)
+    };
+
+    containerProps = () => {
+        const { product } = this.props;
+        return { product };
+    };
+
+    render() {
+        return (
+            <PDPDetailsSection
+              { ...this.containerFunctions }
+              { ...this.containerProps() }
+            />
+        );
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PDPDetailsSectionContainer);
