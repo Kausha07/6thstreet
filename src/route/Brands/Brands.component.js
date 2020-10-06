fc0efd6279a08bafc4370e868ebbeeee5da8fac0
Nikoloz Gabunia <nikoloz.gabunia@scandiweb.com>
2020-10-06 12:06:44 +0400

    SIXTH-53 - Added gender filters functionality

--- src/route/Brands/Brands.component.js
+++ src/route/Brands/Brands.component.js
@@ -1,15 +1,72 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { PureComponent } from 'react';
 
 import BrandGroup from 'Component/BrandGroup';
+import Loader from 'Component/Loader';
 import { FormattedBrands } from 'Util/API/endpoint/Brands/Brands.type';
+
+import { KIDS_TYPE, MEN_TYPE, WOMEN_TYPE } from './Brands.config';
 
 import './Brands.style';
 
 class Brands extends PureComponent {
     static propTypes = {
-        brands: FormattedBrands.isRequired
+        changeBrandType: PropTypes.func.isRequired,
+        isLoading: PropTypes.bool.isRequired,
+        brands: FormattedBrands.isRequired,
+        type: PropTypes.string
     };
+
+    static defaultProps = {
+        type: null
+    };
+
+    onBrandCategoryClick = (categoryName) => () => {
+        const { changeBrandType } = this.props;
+
+        changeBrandType(categoryName);
+    }
+
+    renderCategorySelector() {
+        const { type } = this.props;
+
+        return (
+            <div block="Brands" elem="Categories">
+                <button
+                  block="Brands"
+                  elem="CategoryButton"
+                  mods={ { isSelected: !type } }
+                  onClick={ this.onBrandCategoryClick('') }
+                >
+                    { __('All') }
+                </button>
+                <button
+                  block="Brands"
+                  elem="CategoryButton"
+                  mods={ { isSelected: type === WOMEN_TYPE } }
+                  onClick={ this.onBrandCategoryClick(WOMEN_TYPE) }
+                >
+                    { __('Women') }
+                </button>
+                <button
+                  block="Brands"
+                  elem="CategoryButton"
+                  mods={ { isSelected: type === MEN_TYPE } }
+                  onClick={ this.onBrandCategoryClick(MEN_TYPE) }
+                >
+                    { __('Men') }
+                </button>
+                <button
+                  block="Brands"
+                  elem="CategoryButton"
+                  mods={ { isSelected: type === KIDS_TYPE } }
+                  onClick={ this.onBrandCategoryClick(KIDS_TYPE) }
+                >
+                    { __('Kids') }
+                </button>
+            </div>
+        );
+    }
 
     renderBrandGroup([letter, brands]) {
         return (
@@ -27,9 +84,16 @@ class Brands extends PureComponent {
     }
 
     render() {
+        const { isLoading } = this.props;
+
         return (
             <div block="Brands">
-                { this.renderBrandGroups() }
+                <Loader isLoading={ isLoading } />
+                <h2 block="Brands" elem="Heading">{ __('Brands A-Z') }</h2>
+                { this.renderCategorySelector() }
+                <div block="Brands" elem="Groups">
+                    { this.renderBrandGroups() }
+                </div>
             </div>
         );
     }
--- src/route/Brands/Brands.container.js
+++ src/route/Brands/Brands.container.js
@@ -1,14 +1,18 @@
 import { PureComponent } from 'react';
+import { withRouter } from 'react-router';
 
+import { HistoryType, LocationType } from 'Type/Common';
 import { groupByName } from 'Util/API/endpoint/Brands/Brands.format';
 import Algolia from 'Util/API/provider/Algolia';
+import { getQueryParam, setQueryParams } from 'Util/Url';
 
-// import PropTypes from 'prop-types';
 import Brands from './Brands.component';
+import { TYPES_ARRAY } from './Brands.config';
 
 class BrandsContainer extends PureComponent {
     static propTypes = {
-        // TODO: implement prop-types
+        history: HistoryType.isRequired,
+        location: LocationType.isRequired
     };
 
     state = {
@@ -16,19 +20,36 @@ class BrandsContainer extends PureComponent {
         isLoading: true
     };
 
-    constructor(props) {
-        super(props);
+    containerFunctions = {
+        changeBrandType: this.changeBrandType.bind(this)
+    };
 
-        this.requestBrands();
+    componentDidMount() {
+        const brandUrlParam = getQueryParam('type', location);
+        const brandType = TYPES_ARRAY.includes(brandUrlParam) ? brandUrlParam
+            : '';
+
+        this.requestBrands(brandType);
     }
 
-    async requestBrands() {
-        const brands = await Algolia.getBrands('kids'); // one of women, men, kids
+    changeBrandType(brandUrlParam) {
+        const { location, history } = this.props;
+        const brandType = TYPES_ARRAY.includes(brandUrlParam) ? brandUrlParam
+            : '';
 
-        this.setState({
-            brands: groupByName(brands),
-            isLoading: false
-        });
+        setQueryParams({ type: brandType }, location, history);
+        this.requestBrands(brandType);
+    }
+
+    requestBrands(brandType = '') {
+        this.setState({ isLoading: true });
+
+        this._brandRequest = Algolia.getBrands(brandType).then((data) => {
+            this.setState({
+                brands: groupByName(data),
+                isLoading: false
+            });
+        }).catch((error) => console.error(error));
     }
 
     containerProps = () => {
@@ -36,17 +57,19 @@ class BrandsContainer extends PureComponent {
 
         return {
             brands,
-            isLoading
+            isLoading,
+            type: getQueryParam('type', location)
         };
     };
 
     render() {
         return (
             <Brands
+              { ...this.containerFunctions }
               { ...this.containerProps() }
             />
         );
     }
 }
 
-export default BrandsContainer;
+export default withRouter(BrandsContainer);
