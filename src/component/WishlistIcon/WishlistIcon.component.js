97dfe429ee910ef59f1843270b6da048bf3fcdb5
Deniss <deniss.tarasevics@gmail.com>
2020-10-12 13:49:03 +0300

    wishlist Icon click functionality done

--- src/component/WishlistIcon/WishlistIcon.component.js
+++ src/component/WishlistIcon/WishlistIcon.component.js
@@ -1,3 +1,4 @@
+/* eslint-disable fp/no-let */
 import PropTypes from 'prop-types';
 import { PureComponent } from 'react';
 
@@ -6,7 +7,8 @@ import './WishlistIcon.style';
 class WishlistIcon extends PureComponent {
     static propTypes = {
         sku: PropTypes.string.isRequired,
-        addToWishlist: PropTypes.func.isRequired
+        addToWishlist: PropTypes.func.isRequired,
+        items: PropTypes.array.isRequired
     };
 
     handleClick = () => {
@@ -16,11 +18,22 @@ class WishlistIcon extends PureComponent {
     };
 
     renderIcon() {
+        const { sku, items } = this.props;
+
+        let isBlack = false;
+
+        items.forEach((item) => {
+            if (item.product.sku === sku) {
+                isBlack = true;
+            }
+        });
+
         return (
             <div
               role="button"
               block="WishlistIcon"
               elem="Icon"
+              mods={ { black: isBlack } }
               tabIndex={ 0 }
               aria-label="Wishlist"
               onClick={ this.handleClick }
