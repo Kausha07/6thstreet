e34e1ddcc3308b1172a2dde6868ba506338f8589
Nikoloz Gabunia <nikoloz.gabunia@scandiweb.com>
2020-10-16 17:53:50 +0400

    SIXTH-100 - Added message validation

--- src/component/MyAccountReturnCreateList/MyAccountReturnCreateList.container.js
+++ src/component/MyAccountReturnCreateList/MyAccountReturnCreateList.container.js
@@ -1,23 +1,35 @@
+import PropTypes from 'prop-types';
 import { PureComponent } from 'react';
 
+import { showNotification } from 'Store/Notification/Notification.action';
 import MagentoAPI from 'Util/API/provider/MagentoAPI';
 
 import MyAccountReturnCreateList from './MyAccountReturnCreateList.component';
 
+export const mapStateToProps = () => ({});
+
+export const mapDispatchToProps = (dispatch) => ({
+    showErrorNotification: (error) => dispatch(showNotification('error', error))
+});
+
 export class MyAccountReturnCreateListContainer extends PureComponent {
+    static propTypes = {
+        showErrorNotification: PropTypes.func.isRequired
+    };
+
     state = {
         isLoading: false,
         orders: []
     };
 
     componentDidMount() {
-        this._isMonted = true;
+        this._isMounted = true;
 
         this.getOrderList();
     }
 
     componentWillUnmount() {
-        this._isMonted = false;
+        this._isMounted = false;
     }
 
     containerProps = () => {
@@ -27,20 +39,22 @@ export class MyAccountReturnCreateListContainer extends PureComponent {
     };
 
     getOrderList() {
+        const { showErrorNotification } = this.props;
+
         this.setState({ isLoading: true });
 
         MagentoAPI.get('orders/list').then(({ data: { orders } }) => {
-            if (!this._isMonted) {
+            if (!this._isMounted) {
                 return;
             }
 
             this.setState({ orders, isLoading: false });
         }).catch(() => {
-            if (!this._isMonted) {
+            if (!this._isMounted) {
                 return;
             }
 
-            console.log('err)');
+            showErrorNotification(__('Error appeared while fetching orders'));
             this.setState({ isLoading: false });
         });
     }
