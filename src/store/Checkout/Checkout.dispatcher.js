f3871d15389d79d98bc18a8482ecb41ef7f044c8
maxmislin <maksims_mislins@maksims.mislins>
2020-11-10 13:22:53 +0200

    SIXTH-300 - Fix

--- src/store/Checkout/Checkout.dispatcher.js
+++ src/store/Checkout/Checkout.dispatcher.js
@@ -1,6 +1,6 @@
-/* eslint-disable */
 import { getStore } from 'Store';
 import { setShipping } from 'Store/Checkout/Checkout.action';
+import { showNotification } from 'Store/Notification/Notification.action';
 import {
     createOrder,
     estimateShippingMethods,
@@ -14,9 +14,7 @@ import {
     getInstallmentForValue,
     verifyPayment
 } from 'Util/API/endpoint/Tabby/Tabby.enpoint';
-import { showNotification } from 'Store/Notification/Notification.action';
 import Logger from 'Util/Logger';
-import { TABBY_PAYMENT_CODES } from "Component/CheckoutPayments/CheckoutPayments.config";
 
 export class CheckoutDispatcher {
     /* eslint-disable-next-line */
@@ -24,26 +22,21 @@ export class CheckoutDispatcher {
         const { Cart: { cartId } } = getStore().getState();
         const { area, street } = address;
         try {
-            console.log(address);
             const response = await validateShippingAddress({ address });
-            console.log(response);
             const { success: isAddressValid } = response;
 
-            if (!isAddressValid & (area !== undefined || street !== undefined)) {
-                console.log('here');
-                const { error: {parameters} } = response;
-                const message = parameters.length > 1 ? 
-                `(${parameters}) ${__('fields are not valid')}` : 
-                `(${parameters}) ${__('field is not valid')}`;
-                
+            if (!isAddressValid && (area !== undefined || street !== undefined)) {
+                const { error: { parameters } } = response;
+                const message = parameters.length > 1
+                    ? `(${parameters}) ${__('fields are not valid')}`
+                    : `(${parameters}) ${__('field is not valid')}`;
+
                 dispatch(showNotification('error', message));
             }
             if (isAddressValid) {
-                console.log(await estimateShippingMethods({ cartId, address }))
                 return await estimateShippingMethods({ cartId, address });
             }
         } catch (e) {
-            console.log('here');
             dispatch(showNotification('error', __('The address or phone field is incorrect')));
             Logger.log(e);
         }
