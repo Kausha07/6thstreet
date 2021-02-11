6a296e652de7f0e053c8ec12f2829ab0a5d5d25a
Deniss <deniss.tarasevics@gmail.com>
2021-02-11 16:56:41 +0200

    SIXTH-375-2 code cleared

--- src/component/MenuCategory/MenuCategory.component.js
+++ src/component/MenuCategory/MenuCategory.component.js
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/jsx-no-bind */
 import PropTypes from 'prop-types';
 import { PureComponent } from 'react';
