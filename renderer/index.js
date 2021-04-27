11474663049f3fd506f2158909da7b80653b5fe3
Raghav Dua <Raghav.Dua@6thstreet.com>
2021-04-27 11:38:05 +0530

    Bug fixes in renderer

--- renderer/index.js
+++ renderer/index.js
@@ -1,11 +1,10 @@
+/* eslint-disable fp/no-let */
 const os = require('os');
 const express = require('express');
 const path = require('path');
 
-
 const chrome = require('./chrome.js');
 const cache = require('./cache.js');
-
 
 let browserWSEndpoint = null;
 
@@ -15,13 +14,18 @@ renderer.use(express.static('build', {
 }));
 
 renderer.get('*', async (req, res) => {
-    const locale = req.headers.host.split('.')[0];
-    console.log(`http://${locale}.localhost:3000${req.originalUrl}`)
+    let host = 'localhost:3000';
+    const result = req.hostname.match(/(en|ar)-(ae|sa|kw|om|bh|qa)/);
+    if (result) {
+        host = `${result[0]}.${host}`;
+    }
+
     // Try to get the rendered HTML of the requested URL from cache
-    let {html, ttRenderMs} = await cache.get(req.originalUrl);
+    let { html, ttRenderMs } = await cache.get(req.originalUrl);
+
     // If not in cache than, render the page in a headless instance of Google Chrome
-    if(!html){
-        let renderResponse = await chrome.instance(`http://${locale}.localhost:3000${req.originalUrl}`, browserWSEndpoint);
+    if (!html) {
+        const renderResponse = await chrome.instance(`http://${host}${req.originalUrl}`, browserWSEndpoint);
         html = renderResponse.html;
         ttRenderMs = renderResponse.ttRenderMs;
         browserWSEndpoint = renderResponse.browserWSEndpoint;
@@ -32,29 +36,24 @@ renderer.get('*', async (req, res) => {
     // Add Server-Timing HTTP header! See https://w3c.github.io/server-timing/.
     res.set({
         'Server-Timing': `Prerender;dur=${ttRenderMs};desc="Headless render time (ms)"`,
-        'Content-Security-Policy': `script-src 'self'`
+        'Content-Security-Policy': 'script-src \'self\''
     });
 
     // Serve the rendered HTML
     res.status(200).send(html); // Serve prerendered page as response.
-})
+});
 
 cache.init(
     path.resolve(os.tmpdir(), '6thstreet')
-)
-.then(() => {
-    return new Promise((resolve, reject) => {
-        renderer.listen(5000, (err) => {
-            if(err){
-                reject(err);
-            }
-            resolve();
-        })
-    })
-})
-.then(() => {
+).then(() => new Promise((resolve, reject) => {
+    renderer.listen(5000, (err) => {
+        if (err) {
+            reject(err);
+        }
+        resolve();
+    });
+})).then(() => {
     console.log('Renderer started. Press Ctrl+C to quit');
-})
-.catch((err) => {
-    console.error(err)
+}).catch((err) => {
+    console.error(err);
 });
