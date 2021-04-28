04cca1483ead20d0713a84e4e729b2bc380da1b1
Raghav Dua <Raghav.Dua@6thstreet.com>
2021-04-28 13:29:22 +0530

    Added config file for renderer

--- renderer/cache.js
+++ renderer/cache.js
@@ -1,105 +1,129 @@
 const os = require('os');
 const path = require('path');
-const NodeCache = require( "node-cache");
-const diskCache = require('cacache');
 const fs = require('fs');
+const NodeCache = require('node-cache');
+const DISK_CACHE = require('cacache');
 
-const cache =  {
-    diskCachePath: path.resolve(os.tmpdir(), '6thstreet'),
-    memoryCache: null,
-    accessRecord: [],
-    init: async (diskCachePath) => {
-        if(diskCachePath){
-            cache.diskCachePath = diskCachePath;
+const MEMORY_CACHE = new NodeCache();
+
+// 'CACHE_ACCESS_RECORD' maintains a record of items accessed from the cache.
+const CACHE_ACCESS_RECORD = [];
+
+const config = {
+    MEMORY_CACHE_MAX_SIZE: 50000000,
+    DISK_CACHE_PATH: path.resolve(os.tmpdir(), 'DISK_CACHE')
+};
+
+const cache = {
+    init: async (options) => {
+        const { MEMORY_CACHE_MAX_SIZE, DISK_CACHE_DIRECTORY } = options;
+
+        // Set Memory Cache's Max Size
+        if (MEMORY_CACHE_MAX_SIZE) {
+            config.MEMORY_CACHE_MAX_SIZE = MEMORY_CACHE_MAX_SIZE;
         }
-        cache.memoryCache = new NodeCache();
-        cache.memoryCache.on("del", ( key, value ) => {
-            diskCache.put(cache.diskCachePath, key, value);
+
+        // Set Disk Cache path
+        if (DISK_CACHE_DIRECTORY) {
+            config.DISK_CACHE_PATH = path.resolve(os.tmpdir(), DISK_CACHE_DIRECTORY);
+        }
+
+        // When an item is deleted from the Memory Cache, store it in the Disk Cache
+        MEMORY_CACHE.on('del', (key, value) => {
+            DISK_CACHE.put(config.DISK_CACHE_PATH, key, value);
         });
+
         return cache.flush();
     },
+
     get: async (key) => {
-        let html = cache.memoryCache.get(key);
-        if(!html && await diskCache.get.info(cache.diskCachePath, key)){
-            html = (await diskCache.get(cache.diskCachePath, key)).data
-            cache.set(key, html);
+        // Try to retreive the item from Memory Cache
+        let value = MEMORY_CACHE.get(key);
+
+        // If not found in Memory Cache, check in Disk Cache
+        if (!value && await DISK_CACHE.get.info(config.DISK_CACHE_PATH, key)) {
+            // If found in Disck Cache, store it in the Memory Cache
+            value = (await DISK_CACHE.get(config.DISK_CACHE_PATH, key)).data;
+            cache.set(key, value);
         }
 
-        const keyIndexInCacheAccessRecord = cache.accessRecord.indexOf(key);
-        if(keyIndexInCacheAccessRecord >= 0){
-            cache.accessRecord.splice(keyIndexInCacheAccessRecord, 1);
+        // Update Cache Access Record
+        const keyIndexInCacheAccessRecord = CACHE_ACCESS_RECORD.indexOf(key);
+        if (keyIndexInCacheAccessRecord >= 0) {
+            CACHE_ACCESS_RECORD.splice(keyIndexInCacheAccessRecord, 1);
         }
-        cache.accessRecord.push(key);
+        CACHE_ACCESS_RECORD.push(key);
         return {
-            html,
+            value,
             ttRenderMs: 0
         };
     },
+
     set: async (key, value) => {
-        if(cache.memoryCache.getStats().vsize >= 50000000){
-            cache.del(cache.accessRecord[0])
+        // Check if Memory Cache is full
+        if (MEMORY_CACHE.getStats().vsize >= config.MEMORY_CACHE_MAX_SIZE) {
+            // If Memory Cache is full, delete the least recently accessed item
+            cache.del(CACHE_ACCESS_RECORD[0])
+                .then(() => {
+                    CACHE_ACCESS_RECORD.shift();
+                    // After the deletion of the least recently accessed item, store the new item in Memory Cache
+                    return MEMORY_CACHE.set(key, value);
+                })
+                .catch((err) => {
+                    console.error(`${err}, resetting the entire disk and memory cache`);
+                    CACHE_ACCESS_RECORD.length = 0;
+                    cache.flush();
+                });
+        } else {
+            // If Memory Cache is not full, store the new item in it
+            MEMORY_CACHE.set(key, value);
+        }
+    },
+
+    // First check if item is in Disk Cache
+    del: async (key) => DISK_CACHE.get.info(config.DISK_CACHE_PATH, key).finally((info) => new Promise((resolve, reject) => {
+        if (info) {
+            // If found in Disk Cache, delete the item from there
+            fs.unlink(info.path, (err) => {
+                err
+                    ? console.error(`Unable to delete item with key: ${key} from disk cache, ${err}`)
+                    : DISK_CACHE.rm.entry(config.DISK_CACHE_PATH, key);
+            });
+        }
+
+        // Finally, delete the item from Memory Cache
+        const del = MEMORY_CACHE.del(key);
+        if (del) {
+            resolve();
+        } else {
+            reject(`Unable to delete item with key${key} from memory cache`);
+        }
+    })),
+
+    flush: async () => {
+        // First delete all items from Disk Cache
+        console.log('Cleaning up disk cache');
+        return DISK_CACHE.rm.all(config.DISK_CACHE_PATH)
             .then(() => {
-                cache.accessRecord.shift();
-                return cache.memoryCache.set(key, value);
+                console.log('Disk cache cleared');
             })
             .catch((err) => {
-                console.error(`${err}, resetting the entire disk and memory cache`);
-                cache.accessRecord = [];
-                cache.flush();
+                console.error(`Unable to clear disk cache: ${err}`);
             })
-        }
-        else{
-            cache.memoryCache.set(key, value);
-        }
-    },
-    del: async (key) => {
-        return diskCache.get.info(cache.diskCachePath, key).finally((info) => {
-            return new Promise((resolve, reject) => {
-                if(info){
-                    // Delete from disk cache as well
-                    fs.unlink(info.path, (err) => {
-                        err
-                        ?
-                        console.error(`Unable to delete item with key: ${key} from disk cache, ${err}`)
-                        :
-                        diskCache.rm.entry(cache.diskCachePath, key);
-                    })
+            .finally(() => {
+                // Finally, clear all items from Memory Cache
+                if (MEMORY_CACHE) {
+                    console.log('Cleaning up memory cache');
+                    MEMORY_CACHE.flushAll();
+                    MEMORY_CACHE.flushStats();
+                    console.log('Memory cache cleared');
+                } else {
+                    console.log('No memory cache exists');
                 }
-                const del = cache.memoryCache.del(key);
-                if(del){
-                    resolve();
-                }
-                else{
-                    reject(`Unable to delete item with key${key} from memory cache`);
-                }
-                
-            })
-        })
+            });
     },
-    flush: async () => {
-        console.log("Cleaning up disk cache")
-        return diskCache.rm.all(cache.diskCachePath)
-        .then(() => {
-            console.log("Disk cache cleared")
-        })
-        .catch((err) => {
-            console.error(`Unable to clear disk cache: ${err}`)
-        })
-        .finally(() => {
-            if(cache.memoryCache){
-                console.log("Cleaning up memory cache");
-                cache.memoryCache.flushAll();
-                cache.memoryCache.flushStats();
-                console.log("Memory cache cleared")
-            }
-            else{
-                console.log("No memory cache exists")
-            }
-        }); 
-    },
-    close: () => {
-        return cache.memoryCache.close();
-    }
-}
+
+    close: () => MEMORY_CACHE.close()
+};
 
 module.exports = cache;
--- renderer/index.js
+++ renderer/index.js
@@ -6,6 +6,8 @@ const path = require('path');
 const chrome = require('./chrome.js');
 const cache = require('./cache.js');
 
+const config = require('./config.json');
+
 let browserWSEndpoint = null;
 
 const renderer = express();
@@ -14,47 +16,56 @@ renderer.use(express.static('build', {
 }));
 
 renderer.get('*', async (req, res) => {
-    let host = 'localhost:3000';
+    let host = `${config.APP_HOSTNAME}:${config.APP_PORT}`;
     const result = req.hostname.match(/(en|ar)-(ae|sa|kw|om|bh|qa)/);
     if (result) {
         host = `${result[0]}.${host}`;
     }
 
     const url = `http://${host}${req.originalUrl}`;
-    // Try to get the rendered HTML of the requested URL from cache
-    let { html, ttRenderMs } = await cache.get(url);
 
-    // If not in cache than, render the page in a headless instance of Google Chrome
-    if (!html) {
-        const renderResponse = await chrome.instance(url, browserWSEndpoint);
-        html = renderResponse.html;
-        ttRenderMs = renderResponse.ttRenderMs;
-        browserWSEndpoint = renderResponse.browserWSEndpoint;
-        // Save the rendered HTML page in cache
-        cache.set(url, html);
-    }
+    try {
+        // Try to get the rendered HTML of the requested URL from cache
+        let { value: html, ttRenderMs } = await cache.get(url);
 
-    // Add Server-Timing HTTP header! See https://w3c.github.io/server-timing/.
-    res.set({
-        'Server-Timing': `Prerender;dur=${ttRenderMs};desc="Headless render time (ms)"`,
-        'Content-Security-Policy': 'script-src \'self\''
-    });
+        // If not in cache than, render the page in a headless instance of Google Chrome
+        if (!html) {
+            const renderResponse = await chrome.instance(url, browserWSEndpoint);
+            html = renderResponse.html;
+            ttRenderMs = renderResponse.ttRenderMs;
+            browserWSEndpoint = renderResponse.browserWSEndpoint;
 
-    // Serve the rendered HTML
-    res.status(200).send(html); // Serve prerendered page as response.
-});
-
-cache.init(
-    path.resolve(os.tmpdir(), '6thstreet')
-).then(() => new Promise((resolve, reject) => {
-    renderer.listen(5000, (err) => {
-        if (err) {
-            reject(err);
+            // Save the rendered HTML page in cache
+            cache.set(url, html);
         }
-        resolve();
-    });
-})).then(() => {
-    console.log('Renderer started. Press Ctrl+C to quit');
-}).catch((err) => {
-    console.error(err);
+
+        // Add Server-Timing HTTP header! See https://w3c.github.io/server-timing/.
+        res.set({
+            'Server-Timing': `Prerender;dur=${ttRenderMs};desc="Headless render time (ms)"`,
+            'Content-Security-Policy': 'script-src \'self\''
+        });
+
+        // Serve the rendered HTML
+        res.status(200).send(html); // Serve prerendered page as response.
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).send(err);
+    }
 });
+
+(async () => {
+    const CACHE_OPTIONS = {
+        MEMORY_CACHE_MAX_SIZE: config.MEMORY_CACHE_MAX_SIZE,
+        DISK_CACHE_DIRECTORY: config.DISK_CACHE_DIRECTORY
+    };
+    const PORT = config.PORT || 5000;
+    try {
+        await cache.init(CACHE_OPTIONS);
+        renderer.listen(PORT);
+        console.log('Renderer started. Press Ctrl+C to quit');
+    }
+    catch (err) {
+        console.error(err);
+    }
+})();
