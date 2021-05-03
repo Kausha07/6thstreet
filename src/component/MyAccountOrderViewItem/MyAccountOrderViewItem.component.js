35128f88faf64a4e9b3d80acda11606fbf748706
Raghav Dua <Raghav.Dua@6thstreet.com>
2021-05-03 13:34:29 +0530

    Fix: https://6thstreet.atlassian.net/browse/MIC-32

--- src/component/MyAccountOrderViewItem/MyAccountOrderViewItem.component.js
+++ src/component/MyAccountOrderViewItem/MyAccountOrderViewItem.component.js
@@ -27,7 +27,6 @@ export class MyAccountOrderViewItem extends SourceComponent {
 
         // eslint-disable-next-line no-magic-numbers
         const discountPercentage = Math.round(100 * (1 - (price / original_price)));
-
         return (
             <div block="MyAccountReturnSuccessItem" elem="Details">
                 <h2>{ brand_name }</h2>
@@ -60,7 +59,7 @@ export class MyAccountOrderViewItem extends SourceComponent {
                     >
                         { `${ formatPrice(+original_price, currency) }` }
                     </span>
-                    { !!(price < original_price) && (
+                    { !!(parseFloat(price) < parseFloat(original_price)) && (
                         <>
                             <span block="MyAccountReturnSuccessItem" elem="PriceDiscountPercent">
                                 { `(-${discountPercentage}%)` }
