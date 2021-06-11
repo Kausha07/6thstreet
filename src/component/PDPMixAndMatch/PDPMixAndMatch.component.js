a9a7c97a16e3dfebed7d400649f40a891bd97ba2
Raghav Dua <Raghav.Dua@6thstreet.com>
2021-06-11 10:45:11 +0530

    Added Missing Files

---
+++ src/component/PDPMixAndMatch/PDPMixAndMatch.component.js
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { PureComponent } from 'react';
+
+import PDPMixAndMatchProduct from 'Component/PDPMixAndMatchProduct';
+
+import './PDPMixAndMatch.style';
+
+class PDPMixAndMatch extends PureComponent {
+    static propTypes = {
+        products: PropTypes.array.isRequired,
+        isAlsoAvailable: PropTypes.bool.isRequired
+    };
+
+    renderAvailableProduct = (product) => {
+        const { sku } = product;
+        return (
+            <PDPMixAndMatchProduct
+              product={ product }
+              key={ sku }
+            />
+        );
+    };
+
+    render() {
+        const { products = [] } = this.props;
+        return (
+            <div block="PDPMixAndMatch">
+                <h2 block="PDPMixAndMatch" elem="title">{__( "Mix n Match" )}</h2>
+                <ul block="PDPMixAndMatch" elem="List">
+                    { products.map(this.renderAvailableProduct) }
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default PDPMixAndMatch;
