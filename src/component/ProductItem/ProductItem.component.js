aa187dd1f1875b9f74e004fedbab6defd2dcaa06
bhuvanb-wmt <bhuvanb-wmt@webmob.tech>
2021-06-16 13:06:04 +0530

    algolia for production

--- src/component/ProductItem/ProductItem.component.js
+++ src/component/ProductItem/ProductItem.component.js
@@ -1,153 +1,190 @@
-import PropTypes from 'prop-types';
-import { PureComponent } from 'react';
-
-import Image from 'Component/Image';
-import Link from 'Component/Link';
-import Price from 'Component/Price';
-import ProductLabel from 'Component/ProductLabel/ProductLabel.component';
-import WishlistIcon from 'Component/WishlistIcon';
-import { Product } from 'Util/API/endpoint/Product/Product.type';
-import { isArabic } from 'Util/App';
-import Event, { EVENT_GTM_PRODUCT_CLICK } from 'Util/Event';
-
-import './ProductItem.style';
+import Image from "Component/Image";
+import Link from "Component/Link";
+import Price from "Component/Price";
+import ProductLabel from "Component/ProductLabel/ProductLabel.component";
+import WishlistIcon from "Component/WishlistIcon";
+import PropTypes from "prop-types";
+import { PureComponent } from "react";
+import { Product } from "Util/API/endpoint/Product/Product.type";
+import Algolia from "Util/API/provider/Algolia";
+import { isArabic } from "Util/App";
+import { getUUIDToken } from "Util/Auth";
+import Event, {
+  EVENT_GTM_PRODUCT_CLICK,
+  SELECT_ITEM_ALGOLIA,
+} from "Util/Event";
+import "./ProductItem.style";
 
 class ProductItem extends PureComponent {
-    static propTypes = {
-        product: Product.isRequired,
-        page: PropTypes.string
+  static propTypes = {
+    product: Product.isRequired,
+    page: PropTypes.string,
+  };
+
+  static defaultProps = {
+    page: "",
+  };
+
+  state = {
+    isArabic: isArabic(),
+  };
+
+  handleClick = this.handleProductClick.bind(this);
+
+  handleProductClick() {
+    const { product, position, qid } = this.props;
+    Event.dispatch(EVENT_GTM_PRODUCT_CLICK, product);
+    var data = localStorage.getItem("customer");
+    let userData = JSON.parse(data);
+    let userToken;
+    let queryID;
+    if (!qid) {
+      queryID = getStore().getState().SearchSuggestions.queryID;
+    } else {
+      queryID = qid;
+    }
+    if (userData?.data) {
+      userToken = userData.data.id;
+    }
+    if (queryID) {
+      new Algolia().logAlgoliaAnalytics("click", SELECT_ITEM_ALGOLIA, [], {
+        objectIDs: [product.objectID],
+        queryID,
+        userToken: userToken ? `user-${userToken}` : getUUIDToken(),
+        position: [position],
+      });
+    }
+  }
+
+  renderWishlistIcon() {
+    const {
+      product: { sku },
+    } = this.props;
+
+    return <WishlistIcon sku={sku} />;
+  }
+
+  renderLabel() {
+    const { product } = this.props;
+    return <ProductLabel product={product} />;
+  }
+
+  renderColors() {
+    const {
+      product: { also_available_color, promotion },
+    } = this.props;
+
+    if (also_available_color !== undefined && !promotion) {
+      const count = also_available_color.split(",").length - 2;
+
+      return count > 0 ? (
+        <span block="PLPSummary" elem="Colors">
+          {`+${count} `}
+          {__("Colors")}
+        </span>
+      ) : null;
+    }
+
+    return null;
+  }
+
+  renderExclusive() {
+    const {
+      product: { promotion },
+    } = this.props;
+
+    if (promotion !== undefined) {
+      return promotion !== null ? (
+        <span block="PLPSummary" elem="Exclusive">
+          {promotion}
+        </span>
+      ) : null;
+    }
+
+    return null;
+  }
+
+  renderImage() {
+    const {
+      product: { thumbnail_url },
+    } = this.props;
+
+    return (
+      <div>
+        <Image src={thumbnail_url} />
+        {this.renderExclusive()}
+        {this.renderColors()}
+      </div>
+    );
+  }
+
+  renderBrand() {
+    const {
+      product: { brand_name },
+    } = this.props;
+
+    return (
+      <h2 block="ProductItem" elem="Brand">
+        {brand_name}
+      </h2>
+    );
+  }
+
+  renderTitle() {
+    const {
+      product: { name },
+    } = this.props;
+
+    return (
+      <p block="ProductItem" elem="Title">
+        {name}
+      </p>
+    );
+  }
+
+  renderPrice() {
+    const {
+      product: { price },
+      page,
+    } = this.props;
+
+    return <Price price={price} page={page} />;
+  }
+
+  renderLink() {
+    const {
+      product,
+      product: { url },
+    } = this.props;
+
+    const { pathname } = new URL(url);
+
+    const linkTo = {
+      pathname,
+      state: { product },
     };
 
-    static defaultProps = {
-        page: ''
-    };
+    return (
+      <Link to={linkTo} onClick={this.handleClick}>
+        {this.renderImage()}
+        {this.renderBrand()}
+        {this.renderTitle()}
+        {this.renderPrice()}
+      </Link>
+    );
+  }
 
-    state = {
-        isArabic: isArabic()
-    };
+  render() {
+    const { isArabic } = this.state;
 
-    handleClick = this.handleProductClick.bind(this);
-
-    handleProductClick() {
-        const { product } = this.props;
-        Event.dispatch(EVENT_GTM_PRODUCT_CLICK, product);
-    }
-
-    renderWishlistIcon() {
-        const { product: { sku } } = this.props;
-
-        return <WishlistIcon sku={ sku } />;
-    }
-
-    renderLabel() {
-        const { product } = this.props;
-        return <ProductLabel product={ product } />;
-    }
-
-    renderColors() {
-        const { product: { also_available_color, promotion } } = this.props;
-
-        if (also_available_color !== undefined && !promotion) {
-            const count = also_available_color.split(',').length - 2;
-
-            return count > 0
-                ? (
-                    <span block="PLPSummary" elem="Colors">
-                        { `+${count} ` }
-                        { __('Colors') }
-                    </span>
-                )
-                : null;
-        }
-
-        return null;
-    }
-
-    renderExclusive() {
-        const { product: { promotion } } = this.props;
-
-        if (promotion !== undefined) {
-            return promotion !== null
-                ? <span block="PLPSummary" elem="Exclusive">{ promotion }</span>
-                : null;
-        }
-
-        return null;
-    }
-
-    renderImage() {
-        const { product: { thumbnail_url } } = this.props;
-
-        return (
-            <div>
-                <Image src={ thumbnail_url } />
-                { this.renderExclusive() }
-                { this.renderColors() }
-            </div>
-        );
-    }
-
-    renderBrand() {
-        const { product: { brand_name } } = this.props;
-
-        return <h2 block="ProductItem" elem="Brand">{ brand_name }</h2>;
-    }
-
-    renderTitle() {
-        const { product: { name } } = this.props;
-
-        return (
-            <p block="ProductItem" elem="Title">
-                { name }
-            </p>
-        );
-    }
-
-    renderPrice() {
-        const { product: { price }, page } = this.props;
-
-        return (
-            <Price price={ price } page={ page } />
-        );
-    }
-
-    renderLink() {
-        const {
-            product,
-            product: { url }
-        } = this.props;
-
-        const { pathname } = new URL(url);
-
-        const linkTo = {
-            pathname,
-            state: { product }
-        };
-
-        return (
-            <Link to={ linkTo } onClick={ this.handleClick }>
-                { this.renderImage() }
-                { this.renderBrand() }
-                { this.renderTitle() }
-                { this.renderPrice() }
-            </Link>
-        );
-    }
-
-    render() {
-
-        const {
-            isArabic
-        } = this.state;
-
-        return (
-            <li block="ProductItem" mods={ { isArabic } }>
-                { this.renderLabel() }
-                { this.renderWishlistIcon() }
-                { this.renderLink() }
-            </li>
-        );
-    }
+    return (
+      <li block="ProductItem" mods={{ isArabic }}>
+        {this.renderLabel()}
+        {this.renderWishlistIcon()}
+        {this.renderLink()}
+      </li>
+    );
+  }
 }
 
 export default ProductItem;
--- src/store/SearchSuggestions/SearchSuggestions.reducer.js
+++ src/store/SearchSuggestions/SearchSuggestions.reducer.js
@@ -1,26 +1,27 @@
-import { SET_SEARCH_SUGGESTIONS } from './SearchSuggestions.action';
+import { SET_SEARCH_SUGGESTIONS } from "./SearchSuggestions.action";
 
 export const getInitialState = () => ({
-    search: '',
-    data: {}
+  search: "",
+  data: {},
 });
 
 export const SearchSuggestionsReducer = (state = getInitialState(), action) => {
-    const { type } = action;
+  const { type } = action;
 
-    switch (type) {
+  switch (type) {
     case SET_SEARCH_SUGGESTIONS:
-        const { search, data } = action;
+      const { search, data, queryID } = action;
 
-        return {
-            ...state,
-            search,
-            data
-        };
+      return {
+        ...state,
+        search,
+        data,
+        queryID,
+      };
 
     default:
-        return state;
-    }
+      return state;
+  }
 };
 
 export default SearchSuggestionsReducer;
