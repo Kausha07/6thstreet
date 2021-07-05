740335882b529421b4050694199ef86360e08012
neelv-wmt <84140232+neelv-wmt@users.noreply.github.com>
2021-07-05 10:48:09 +0530

    console removed

--- src/component/ProductItem/ProductItem.component.js
+++ src/component/ProductItem/ProductItem.component.js
@@ -152,7 +152,6 @@ class ProductItem extends PureComponent {
       product: { price },
       page,
     } = this.props;
-    console.log("price", price)
     return <Price price={price} page={page} />;
   }
 
