22cce24df7ef03ccbe862bf87072ff368c8ee550
Raghav Dua <Raghav.Dua@6thstreet.com>
2021-07-09 20:48:15 +0530

    Updated Cart UI according to new design(PWA-772)

--- src/component/Icons/index.js
+++ src/component/Icons/index.js
@@ -1,10 +1,11 @@
 import ChevronDown from './ChevronDown';
 import ChevronRight from './ChevronRight';
 import ChevronUp from './ChevronUp';
+import Close from './Close';
 import FavouriteFilled from './FavouriteFilled';
 import Favourite from './Favourite';
 import ShareFilled from './ShareFilled';
+import Select from './Select';
 import Share from './Share';
 
-
-export { ChevronDown, ChevronRight, ChevronUp, FavouriteFilled, Favourite, ShareFilled, Share }
+export { ChevronDown, ChevronRight, ChevronUp, Close, FavouriteFilled, Favourite, ShareFilled, Select, Share }
