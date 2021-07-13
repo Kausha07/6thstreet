31682c77f30e592bffe9b4e2433003eba7712111
SKumarAppereluae <50903374+SKumarAppereluae@users.noreply.github.com>
2021-07-13 18:32:26 +0530

    New Home main menu addition (#1582)
    * New Home main menu addition
    
    * Home condition updated

--- src/util/Url/Url.js
+++ src/util/Url/Url.js
@@ -126,7 +126,8 @@ export const formatCDNLink = (url, isFormatEnabled = false) => {
     .replace("/men.html", ".html")
     .replace("/women.html", ".html")
     .replace("/kids-baby_boy-boy-girl-baby_girl.html", ".html")
-    .replace("/kids.html", ".html");
+    .replace("/kids.html", ".html")
+    .replace("/home.html", ".html");
 };
 
 export const getCountryCurrencyCode = () => {
