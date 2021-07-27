3b4229f28f86d995c301db8fc7d09446d6379e99
bhuvanb-wmt <bhuvanb-wmt@webmob.tech>
2021-07-27 18:24:16 +0530

    wishlist implementation on search screen

---
+++ src/component/WishlistSlider/WishlistSlider.component.js
@@ -0,0 +1,135 @@
+import DragScroll from "Component/DragScroll/DragScroll.component";
+import PropTypes from "prop-types";
+import React, { PureComponent } from "react";
+import { isArabic } from "Util/App";
+import WishlistSliderItem from "./WishlistSlider.Item";
+import "./WishlistSlider.style.scss";
+class WishlistSlider extends PureComponent {
+  static propTypes = {
+    sliderLength: PropTypes.number,
+    heading: PropTypes.string.isRequired,
+    products: PropTypes.array.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.itemRef = React.createRef();
+    this.cmpRef = React.createRef();
+    this.scrollerRef = React.createRef();
+    this.state = {
+      customScrollWidth: null,
+      isArabic: isArabic(),
+    };
+  }
+  componentDidMount() {
+    if (this.state.customScrollWidth < 0) {
+      this.renderScrollbar();
+    }
+  }
+
+  getProducts = () => {
+    const { products, sliderLength } = this.props;
+    if (products.length > sliderLength) {
+      products.length = sliderLength;
+    }
+    return products;
+  };
+
+  renderHeader() {
+    const { heading } = this.props;
+    return (
+      <div block="VueProductSlider" elem="HeaderContainer">
+        <h4>{heading}</h4>
+      </div>
+    );
+  }
+
+  handleScroll = (event) => {
+    const target = event.nativeEvent.target;
+    const prentComponent = [...this.cmpRef.current.childNodes].filter(
+      (node) => node.id == "ScrollWrapper"
+    )[0];
+    prentComponent.scrollLeft = target.scrollLeft;
+  };
+
+  renderScrollbar = () => {
+    console.log(this.state);
+    let items = this.getProducts();
+
+    const width =
+      (this.itemRef &&
+        this.itemRef.current &&
+        this.itemRef.current.childRef.current.clientWidth) *
+        items.length +
+      items.length * 7 * 2 -
+      690;
+    this.setState({
+      customScrollWidth: width,
+    });
+
+    // return null;
+
+    return (
+      <div
+        block="Outer"
+        ref={this.scrollerRef}
+        mods={{
+          Hidden:
+            this.scrollerRef.current &&
+            this.scrollerRef.current.clientWidth >=
+              this.state.customScrollWidth,
+        }}
+        onScroll={this.handleScroll}
+      >
+        <div
+          block="Outer"
+          style={{ width: this.state.customScrollWidth }}
+          elem="Inner"
+        ></div>
+      </div>
+    );
+  };
+  renderSliderContainer() {
+    const items = this.getProducts();
+    const { isHome } = this.props;
+    // debugger
+    return (
+      <DragScroll data={{ rootClass: "ScrollWrapper", ref: this.cmpRef }}>
+        <>
+          <div
+            block="VueProductSlider"
+            elem="SliderContainer"
+            id="ScrollWrapper"
+            ref={this.cmpRef}
+            mods={{ isHome }}
+          >
+            {isHome && <div block="SliderHelper" mods={{ isHome }}></div>}
+            {items.map((item) => {
+              const { product } = item;
+              return (
+                <WishlistSliderItem
+                  key={product.sku}
+                  data={product}
+                  ref={this.itemRef}
+                />
+              );
+            })}
+            {isHome && <div block="SliderHelper" mods={{ isHome }}></div>}
+          </div>
+          {this.renderScrollbar()}
+        </>
+      </DragScroll>
+    );
+  }
+
+  render() {
+    return (
+      <div block="VueProductSlider" elem="Container">
+        {this.renderHeader()}
+        {this.renderSliderContainer()}
+      </div>
+    );
+  }
+}
+
+export default WishlistSlider;
