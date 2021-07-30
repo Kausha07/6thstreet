fa3e034f7aa83c94ee6c90a2046fdece7d8ac410
bhuvanb-wmt <bhuvanb-wmt@webmob.tech>
2021-07-30 16:08:28 +0530

    tracking of trending brands, products, tags & product suggestion

---
+++ src/component/GoogleTagManager/events/TrendingTagsClick.event.js
@@ -0,0 +1,42 @@
+/* eslint-disable import/no-cycle */
+import Event, { EVENT_GTM_TRENDING_TAGS_CLICK } from "Util/Event";
+import BaseEvent from "./Base.event";
+
+/**
+ * Trending brands click event
+ */
+class TrendingTagsClickEvent extends BaseEvent {
+  /**
+   * Set delay
+   *
+   * @type {number}
+   */
+  eventHandleDelay = 0;
+
+  /**
+   * Bind click events
+   */
+  bindEvent() {
+    Event.observer(EVENT_GTM_TRENDING_TAGS_CLICK, (trendingTags) => {
+      this.handle(trendingTags);
+    });
+  }
+
+  /**
+   * Handle trending brands click
+   */
+  handler(trendingTags) {
+    this.pushEventData({
+      ecommerce: {
+        click: {
+          actionField: {
+            action: "click",
+          },
+          trendingTags: trendingTags,
+        },
+      },
+    });
+  }
+}
+
+export default TrendingTagsClickEvent;
