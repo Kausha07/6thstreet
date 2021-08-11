29a87afb641dc6556071403049901aab1e195372
Raghav Dua <Raghav.Dua@6thstreet.com>
2021-08-11 17:36:42 +0530

    Staging (#1673)
    * fix search autocomplete ticket 1269
    
    * Mobile bootom bar hidden issue fixed
    
    * phone number removal issue resolved
    
    * resolve slider issue
    
    Co-authored-by: bhuvanb-wmt <bhuvanb-wmt@webmob.tech>
    Co-authored-by: Brijesh <brijesh.yadav@appinventiv.com>
    Co-authored-by: Neel Vyas <neelv@webmob.tech>

--- src/util/API/endpoint/Suggestions/Suggestions.format.js
+++ src/util/API/endpoint/Suggestions/Suggestions.format.js
@@ -1,27 +1,27 @@
 const BRANDS_RESULT_LIMIT = 4;
 
 const getBrandsArrayFromFacets = ({ facets = {} }) => {
-    try {
-        if (!facets.brand_name) {
-            return [];
-        }
-
-        return Object.keys(facets.brand_name || {})
-            .map((item) => ({ brand_name: item, count: facets.brand_name[item] }))
-            .slice(0, BRANDS_RESULT_LIMIT);
-    } catch (err) {
-        // eslint-disable-next-line no-console
-        console.error(err);
-        return [];
+  try {
+    if (!facets.brand_name) {
+      return [];
     }
+
+    return Object.keys(facets.brand_name || {})
+      .map((item) => ({ brand_name: item, count: facets.brand_name[item] }))
+      .slice(0, BRANDS_RESULT_LIMIT);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    return [];
+  }
 };
 
 // eslint-disable-next-line import/prefer-default-export
 export const formatProductSuggestions = (rawData) => {
-    const data = {
-        brands: getBrandsArrayFromFacets(rawData),
-        products: rawData.data
-    };
+  const data = {
+    brands: getBrandsArrayFromFacets(rawData),
+    products: rawData.data,
+  };
 
-    return data;
+  return data;
 };
