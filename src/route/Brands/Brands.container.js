002bac601d7a1ffbd5b1d47fc43e43f0f712dacf
bhuvanb-wmt <bhuvanb-wmt@webmob.tech>
2021-08-20 16:24:43 +0530

    bug fixes related to search

--- src/route/Brands/Brands.container.js
+++ src/route/Brands/Brands.container.js
@@ -1,166 +1,172 @@
-import PropTypes from 'prop-types';
-import { PureComponent } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
-
-import { doFetch } from '../../util/API/helper/Fetch';
-import ThirdPartyAPI from "../../util/API/provider/ThirdPartyAPI";
-import { DEFAULT_STATE_NAME } from 'Component/NavigationAbstract/NavigationAbstract.config';
-import { changeNavigationState } from 'Store/Navigation/Navigation.action';
-import { TOP_NAVIGATION_TYPE } from 'Store/Navigation/Navigation.reducer';
-import { showNotification } from 'Store/Notification/Notification.action';
-import { HistoryType, LocationType } from 'Type/Common';
-import { groupByName } from 'Util/API/endpoint/Brands/Brands.format';
-import Algolia from 'Util/API/provider/Algolia';
-import { getQueryParam, setQueryParams } from 'Util/Url';
-
-import Brands from './Brands.component';
-import { TYPES_ARRAY } from './Brands.config';
+import { DEFAULT_STATE_NAME } from "Component/NavigationAbstract/NavigationAbstract.config";
+import PropTypes from "prop-types";
+import { PureComponent } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
+import { changeNavigationState } from "Store/Navigation/Navigation.action";
+import { TOP_NAVIGATION_TYPE } from "Store/Navigation/Navigation.reducer";
+import { showNotification } from "Store/Notification/Notification.action";
+import { HistoryType, LocationType } from "Type/Common";
+import { groupByName } from "Util/API/endpoint/Brands/Brands.format";
+import Algolia from "Util/API/provider/Algolia";
+import { getQueryParam, setQueryParams } from "Util/Url";
+import Brands from "./Brands.component";
+import { TYPES_ARRAY } from "./Brands.config";
 
 export const BreadcrumbsDispatcher = import(
-    /* webpackMode: "lazy", webpackChunkName: "dispatchers" */
-    'Store/Breadcrumbs/Breadcrumbs.dispatcher'
+  /* webpackMode: "lazy", webpackChunkName: "dispatchers" */
+  "Store/Breadcrumbs/Breadcrumbs.dispatcher"
 );
 
 export const mapStateToProps = () => ({});
 
 export const mapDispatchToProps = (dispatch) => ({
-    showErrorNotification: (message) => dispatch(showNotification('error', message)),
-    updateBreadcrumbs: (breadcrumbs) => {
-        BreadcrumbsDispatcher.then(({ default: dispatcher }) => dispatcher.update(breadcrumbs, dispatch));
-    },
-    changeHeaderState: (state) => dispatch(changeNavigationState(TOP_NAVIGATION_TYPE, state))
+  showErrorNotification: (message) =>
+    dispatch(showNotification("error", message)),
+  updateBreadcrumbs: (breadcrumbs) => {
+    BreadcrumbsDispatcher.then(({ default: dispatcher }) =>
+      dispatcher.update(breadcrumbs, dispatch)
+    );
+  },
+  changeHeaderState: (state) =>
+    dispatch(changeNavigationState(TOP_NAVIGATION_TYPE, state)),
 });
 
 class BrandsContainer extends PureComponent {
-    static propTypes = {
-        history: HistoryType.isRequired,
-        location: LocationType.isRequired,
-        showErrorNotification: PropTypes.func.isRequired,
-        updateBreadcrumbs: PropTypes.func.isRequired,
-        changeHeaderState: PropTypes.func.isRequired
-    };
+  static propTypes = {
+    history: HistoryType.isRequired,
+    location: LocationType.isRequired,
+    showErrorNotification: PropTypes.func.isRequired,
+    updateBreadcrumbs: PropTypes.func.isRequired,
+    changeHeaderState: PropTypes.func.isRequired,
+  };
 
-    state = {
-        brands: [],
-        isLoading: true,
-        brandMapping: {}
-    };
+  state = {
+    brands: [],
+    isLoading: true,
+    brandMapping: {},
+  };
 
-    containerFunctions = {
-        changeBrandType: this.changeBrandType.bind(this)
-    };
+  containerFunctions = {
+    changeBrandType: this.changeBrandType.bind(this),
+  };
 
-    componentDidMount() {
-        const brandUrlParam = getQueryParam('type', location);
-        const brandType = TYPES_ARRAY.includes(brandUrlParam) ? brandUrlParam : '';
+  componentDidMount() {
+    const brandUrlParam = getQueryParam("type", location);
+    const brandType = TYPES_ARRAY.includes(brandUrlParam) ? brandUrlParam : "";
 
-        this.requestBrands(brandType);
-        this.updateBreadcrumbs();
-        this.updateHeaderState();
-        this.requestBrandMapping()
-        console.log("hiiiii")
-    }
+    this.requestBrands(brandType);
+    this.updateBreadcrumbs();
+    this.updateHeaderState();
+    this.requestBrandMapping();
+  }
 
-    requestBrandMapping = () => {
-        let brandMapping = this.getBrandMappingData();
-    }
+  requestBrandMapping = () => {
+    let brandMapping = this.getBrandMappingData();
+  };
 
-    async getBrandMappingData() {
-        const apiUrl = '/cdn/config/brands.json';
-        fetch(apiUrl)
-        .then((response) => response.json())
+  async getBrandMappingData() {
+    const apiUrl = "/cdn/config/brands.json";
+    fetch(apiUrl)
+      .then((response) => response.json())
       .then((data) => {
         let ret = {};
-        Object.keys(data).forEach(key => {
+        Object.keys(data).forEach((key) => {
           ret[data[key]] = key;
         });
-        this.setState({
-            brandMapping: ret
-        }, ()=>{console.log(this.state.brandMapping)})
-      })
-    }
-
-    updateHeaderState() {
-        const { changeHeaderState } = this.props;
-
-        changeHeaderState({
-            name: DEFAULT_STATE_NAME,
-            isHiddenOnMobile: true
-        });
-    }
-
-    updateBreadcrumbs() {
-        const { updateBreadcrumbs } = this.props;
-        const breadcrumbs = [
-            {
-                url: '',
-                name: __('Brands')
-            },
-            {
-                url: '/',
-                name: __('Home')
-            }
-        ];
-
-        updateBreadcrumbs(breadcrumbs);
-    }
-
-    changeBrandType(brandUrlParam) {
-        const { location, history } = this.props;
-        const brandType = TYPES_ARRAY.includes(brandUrlParam) ? brandUrlParam : '';
-
-        setQueryParams({ type: brandType }, location, history);
-        this.requestBrands(brandType);
-    }
-
-    requestBrands(brandType = '') {
-        const { showErrorNotification } = this.props;
-
-        this.setState({ isLoading: true });
-
-        this._brandRequest = new Algolia().getBrands(brandType).then((data) => {
-            const groupedBrands = groupByName(data) || {};
-
-            // This sort places numeric brands to the end of the list
-            const sortedBrands = Object.entries(groupedBrands).sort(([letter1], [letter2]) => {
-                if (letter1 === '0-9') {
-                    return 1;
-                }
-
-                if (letter2 === '0-9') {
-                    return -1;
-                }
-
-                return letter1 - letter2;
-            });
-
-            this.setState({
-                brands: sortedBrands,
-                isLoading: false
-            });
-        }).catch((error) => showErrorNotification(error));
-    }
-
-    containerProps = () => {
-        const { brands, isLoading, brandMapping } = this.state;
-
-        return {
-            brands,
-            isLoading,
-            brandMapping,
-            type: getQueryParam('type', location)
-        };
-    };
-
-    render() {
-        return (
-            <Brands
-              { ...this.containerFunctions }
-              { ...this.containerProps() }
-            />
+        this.setState(
+          {
+            brandMapping: ret,
+          },
+          () => {
+            console.log(this.state.brandMapping);
+          }
         );
-    }
+      });
+  }
+
+  updateHeaderState() {
+    const { changeHeaderState } = this.props;
+
+    changeHeaderState({
+      name: DEFAULT_STATE_NAME,
+      isHiddenOnMobile: true,
+    });
+  }
+
+  updateBreadcrumbs() {
+    const { updateBreadcrumbs } = this.props;
+    const breadcrumbs = [
+      {
+        url: "",
+        name: __("Brands"),
+      },
+      {
+        url: "/",
+        name: __("Home"),
+      },
+    ];
+
+    updateBreadcrumbs(breadcrumbs);
+  }
+
+  changeBrandType(brandUrlParam) {
+    const { location, history } = this.props;
+    const brandType = TYPES_ARRAY.includes(brandUrlParam) ? brandUrlParam : "";
+
+    setQueryParams({ type: brandType }, location, history);
+    this.requestBrands(brandType);
+  }
+
+  requestBrands(brandType = "") {
+    const { showErrorNotification } = this.props;
+
+    this.setState({ isLoading: true });
+
+    this._brandRequest = new Algolia()
+      .getBrands(brandType)
+      .then((data) => {
+        const groupedBrands = groupByName(data) || {};
+
+        // This sort places numeric brands to the end of the list
+        const sortedBrands = Object.entries(groupedBrands).sort(
+          ([letter1], [letter2]) => {
+            if (letter1 === "0-9") {
+              return 1;
+            }
+
+            if (letter2 === "0-9") {
+              return -1;
+            }
+
+            return letter1 - letter2;
+          }
+        );
+
+        this.setState({
+          brands: sortedBrands,
+          isLoading: false,
+        });
+      })
+      .catch((error) => showErrorNotification(error));
+  }
+
+  containerProps = () => {
+    const { brands, isLoading, brandMapping } = this.state;
+
+    return {
+      brands,
+      isLoading,
+      brandMapping,
+      type: getQueryParam("type", location),
+    };
+  };
+
+  render() {
+    return <Brands {...this.containerFunctions} {...this.containerProps()} />;
+  }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(BrandsContainer));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(BrandsContainer)
+);
