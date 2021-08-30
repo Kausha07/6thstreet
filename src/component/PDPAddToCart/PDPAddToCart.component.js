24e38a609bb9f1a854f7f112c0ec3c2a5effd4ac
Muskan soni <muskan.soni@appinventiv.com>
2021-08-30 12:52:44 +0530

    Fixed arabic version of out of stock text on PLP

--- src/component/PDPAddToCart/PDPAddToCart.component.js
+++ src/component/PDPAddToCart/PDPAddToCart.component.js
@@ -224,6 +224,10 @@ class PDPAddToCart extends PureComponent {
     );
   }
 
+  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   getSizeSelect() {
     const {
       product,
@@ -252,7 +256,11 @@ class PDPAddToCart extends PureComponent {
       );
     }
 
-    return <span id="notavailable">{__("Out of stock")}</span>;
+    return (
+      <span id="notavailable">
+        {this.capitalizeFirstLetter(`${__("out of stock")}`)}
+      </span>
+    );
   }
 
   renderSizeInfo() {
@@ -487,7 +495,7 @@ class PDPAddToCart extends PureComponent {
       isOutOfStock,
     } = this.props;
     if (in_stock === 0 && !isOutOfStock && !notifyMeSuccess) {
-      return <span id="notavailable">{__("Out of stock")}</span>;
+      return <span id="notavailable">{this.capitalizeFirstLetter(`${__("out of stock")}`)}</span>;
     }
     return null;
   }
