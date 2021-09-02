17d680d95cbd3fc6fe3b9d92ee4fc9553dbee6ee
Venkat <venkatesan.r@6thstreet.com>
2021-09-02 15:25:28 +0530

    Fixed issue in Discount shoing

--- src/component/CheckoutOrderSummary/CheckoutOrderSummary.component.js
+++ src/component/CheckoutOrderSummary/CheckoutOrderSummary.component.js
@@ -216,7 +216,7 @@ export class CheckoutOrderSummary extends SourceCheckoutOrderSummary {
               getDiscountFromTotals(totals, "clubapparel"),
               __("Club Apparel Redemption")
             )}
-            {(couponCode || discount) && this.renderPriceLine(discount, __("Discount")) }
+            {(couponCode || (discount && discount != 0)) ? this.renderPriceLine(discount, __("Discount")) : ''}
 
 
             {this.renderPriceLine(
