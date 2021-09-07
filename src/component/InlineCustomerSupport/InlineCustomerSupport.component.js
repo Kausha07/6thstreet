0c13655256ce84f319f98c5b502ff70e2095f70c
Muskan soni <muskan.soni@appinventiv.com>
2021-09-07 10:49:13 +0530

    Fixed demo feedback of 7 sep 2021

--- src/component/InlineCustomerSupport/InlineCustomerSupport.component.js
+++ src/component/InlineCustomerSupport/InlineCustomerSupport.component.js
@@ -58,28 +58,26 @@ class InlineCustomerSupport extends PureComponent {
 
     return (
       <div>
-        { isMobile.any() ?
+        {isMobile.any() ? (
           <a
-          block="InlineCustomerSupport"
-          elem="Phone"
-          mods={{ isArabic }}
-          href={`https://api.whatsapp.com/send?phone=${phone}`}
-        >
-          <bdi>{phone}</bdi>
-        </a>
-         :
-         <a
-         block="InlineCustomerSupport"
-         elem="Phone"
-         mods={{ isArabic }}
-         href={`tel:${phone}`}
-       >
-         <bdi>{phone}</bdi>
-       </a>
-
-        }
+            block="InlineCustomerSupport"
+            elem="Phone"
+            mods={{ isArabic }}
+            href={`https://api.whatsapp.com/send?phone=${phone}`}
+          >
+            <bdi>{phone}</bdi>
+          </a>
+        ) : (
+          <a
+            block="InlineCustomerSupport"
+            elem="Phone"
+            mods={{ isArabic }}
+            href={`tel:${phone}`}
+          >
+            <bdi>{phone}</bdi>
+          </a>
+        )}
       </div>
-
     );
   };
 
@@ -101,7 +99,6 @@ class InlineCustomerSupport extends PureComponent {
     const Email = this.renderEmail();
     const Phone = this.renderPhone();
     // debugger
-
     return (
       <div>
         <button
@@ -113,7 +110,7 @@ class InlineCustomerSupport extends PureComponent {
             elem: "Button",
             mods: { isArabic },
           }}
-          onClick={this.onDropdownClick}
+          onClick={() => this.onDropdownClick()}
         >
           {__("customer service")}
         </button>
@@ -168,7 +165,7 @@ class InlineCustomerSupport extends PureComponent {
     const contactRenderer = isPhoneSupported
       ? this.renderPhone
       : this.renderEmail;
-      // debugger
+    // debugger
 
     return (
       <div
@@ -179,7 +176,7 @@ class InlineCustomerSupport extends PureComponent {
         {this.renderCirclePulse()}
         <p>
           {/* {openHoursLabel} */}
-           {isArabic ? 'مفتوحة من':'Open'}
+          {isArabic ? "مفتوحة من" : "Open"}
           {contactRenderer() && !isArabic ? " at" : ""}
         </p>
         {contactRenderer()}
@@ -188,6 +185,7 @@ class InlineCustomerSupport extends PureComponent {
   }
 
   render() {
+    const { isArabic } = this.state;
     return (
       <div block="InlineCustomerSupport" mods={{ isArabic }}>
         <ClickOutside onClick={this.onClickOutside}>
