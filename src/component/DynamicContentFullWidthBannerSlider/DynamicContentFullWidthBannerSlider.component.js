1906772715542444f7c11a663aec7db1714c9182
Raghav Dua <Raghav.Dua@6thstreet.com>
2021-10-07 19:22:01 +0530

    Fixed: https://6thstreet.atlassian.net/browse/SUP-6180

--- src/component/DynamicContentFullWidthBannerSlider/DynamicContentFullWidthBannerSlider.component.js
+++ src/component/DynamicContentFullWidthBannerSlider/DynamicContentFullWidthBannerSlider.component.js
@@ -8,7 +8,7 @@ import PropTypes from "prop-types";
 import { PureComponent } from "react";
 import TinySlider from "tiny-slider-react";
 // import { getUUID } from "Util/Auth";
-import Event from "Util/Event";
+import Event, { EVENT_GTM_BANNER_CLICK } from "Util/Event";
 import { formatCDNLink } from "Util/Url";
 import DynamicContentHeader from "../DynamicContentHeader/DynamicContentHeader.component";
 import "./DynamicContentFullWidthBannerSlider.style";
@@ -88,6 +88,34 @@ class DynamicContentFullWidthBannerSlider extends PureComponent {
     this.setState({ activeSlide });
   };
 
+  onclick = (item) => {
+    // vue analytics
+    // const locale = VueIntegrationQueries.getLocaleFromUrl();
+    // VueIntegrationQueries.vueAnalayticsLogger({
+    //   event_name: VUE_CAROUSEL_CLICK,
+    //   params: {
+    //     event: VUE_CAROUSEL_CLICK,
+    //     pageType: "plp",
+    //     currency: VueIntegrationQueries.getCurrencyCodeFromLocale(locale),
+    //     clicked: Date.now(),
+    //     uuid: getUUID(),
+    //     referrer: "desktop",
+    //     widgetID: "vue_visually_similar_slider", // // TODO: will be added after vue product slider.
+    //   },
+    // });
+    if(!!!item){
+      return;
+    }
+  
+    const banner = {
+      link: item?.link,
+      promotion_name: item?.promotion_name,
+    };
+  
+    Event.dispatch(EVENT_GTM_BANNER_CLICK, banner);
+    this.sendBannerClickImpression(item);
+  };
+
   sendBannerClickImpression(item) {
     Event.dispatch(HOME_PAGE_BANNER_CLICK_IMPRESSIONS, [item]);
   }
@@ -123,9 +151,6 @@ class DynamicContentFullWidthBannerSlider extends PureComponent {
       <TinySlider
         settings={settings}
         block=""
-        // onIndexChanged={() => {
-        //   this.onSwipeChange();
-        // }}
       >
         {items.map(this.renderSlide)}
       </TinySlider>
