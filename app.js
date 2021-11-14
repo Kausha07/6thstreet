960bbcf21c5e45d8c463215b2f80c1ed3ee715a5
Raghav Dua <Raghav.Dua@6thstreet.com>
2021-11-14 16:54:20 +0400

    Added Cache-Control header for static resources Enabled discount message on Qatar

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv-flow').config();
 const express = require('express');
+var serveStatic = require('serve-static')
 const serverTimings = require('server-timings');
 const proxy = require('./src/setupProxy');
 const path = require('path');
@@ -7,14 +8,26 @@ const path = require('path');
 const PORT = 3000;
 const app = express();
 
-app.use((req, res, next) => {
+function setCustomCacheControl (res, path) {
     res.append('Access-Control-Allow-Origin', ['*']);
-    next();
-});
+    if (serveStatic.mime.lookup(path) === 'text/html') {
+      // Custom Cache-Control for HTML files
+      res.setHeader('Cache-Control', 'public, max-age=0')
+    }
+    else {
+        res.append('cache-control', 'public, max-age=259200, must-revalidate');
+    }
+}
+
 app.use(serverTimings);
 proxy(app);
+
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(serveStatic(path.join(__dirname, 'build'), {
+    setHeaders: setCustomCacheControl
+}))
+
+// app.use(express.static(path.join(__dirname, 'build')));
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
     res.sendFile(path.join(`${__dirname}/build/index.html`));
