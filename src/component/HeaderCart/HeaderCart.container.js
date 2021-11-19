e823bfe53401b34c80582b473fb97b774e0cdc18
Raghav Dua <Raghav.Dua@6thstreet.com>
2021-11-19 18:47:54 +0400

    Fix: SUP-6493

--- src/component/HeaderCart/HeaderCart.container.js
+++ src/component/HeaderCart/HeaderCart.container.js
@@ -48,7 +48,7 @@ export class HeaderCartContainer extends PureComponent {
 
   static getDerivedStateFromProps(props, state) {
     const {
-      totals: { items = [], subtotal, total, id, total_segments },
+      totals: { items = [], discount = 0, subtotal, total, id, total_segments },
       showNotification,
       updateTotals,
       isMinicartOpen,
@@ -69,7 +69,7 @@ export class HeaderCartContainer extends PureComponent {
         const { value: appliedStoreCredit = 0 } = storeCredits || {};
         const { value: appliedClubApparelCredit = 0 } = clubApparelCredits || {};
 
-        if (subtotal > (Math.abs(appliedStoreCredit) + Math.abs(appliedClubApparelCredit))) {
+        if (subtotal > (Math.abs(appliedStoreCredit) + Math.abs(appliedClubApparelCredit) + Math.abs(discount) )) {
           updateTotals(id);
         }
       }
