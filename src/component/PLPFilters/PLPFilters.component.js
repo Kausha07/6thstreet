e5b132a85b95efda0172b348bdb135a9b466353b
Subodh Kumar <Subodh.Kumar@appareluae.com>
2021-12-23 18:27:55 +0530

    merges in PLP branch

--- src/component/PLPFilters/PLPFilters.component.js
+++ src/component/PLPFilters/PLPFilters.component.js
@@ -214,7 +214,7 @@ class PLPFilters extends PureComponent {
         aria-label={__("Reset")}
         onClick={this.resetFilters}
       >
-        {__("clear")}
+        {__("Clear all")}
       </button>
     ) : null;
   }
