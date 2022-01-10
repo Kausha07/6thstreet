23cb6dc43ecb20de5dac9ac62586fa7827e66f11
Neel Vyas <neelv@webmob.tech>
2022-01-10 10:31:19 +0530

    tabby retain cart debug

--- src/store/Cart/Cart.dispatcher.js
+++ src/store/Cart/Cart.dispatcher.js
@@ -32,7 +32,9 @@ export class CartDispatcher {
     const {
       Cart: { cartId },
     } = getStore().getState();
+    console.log("get cart called (store cart id)", cartId)
     const cart_id = BrowserDatabase.getItem(LAST_CART_ID_CACHE_KEY);
+    console.log("get cart called (last cart id)", cart_id)
     if (!cartId || isNewCart) {
       try {
         const { data: requestedCartId = null } = await createCart(cart_id);
@@ -109,6 +111,8 @@ export class CartDispatcher {
       dispatch(processingCartRequest());
       const { data } = await getCart(cartId);
       const cart_id = BrowserDatabase.getItem(LAST_CART_ID_CACHE_KEY);
+      console.log("get cart totals (store cart id )", cartId)
+      console.log("get cart totals (last cart id )", cart_id)
       if (!data) {
         try {
           const { data: requestedCartId = null } = await createCart(cart_id);
