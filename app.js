11982b713c06aa4af24af7ba03e8920bfd8d198d
Raghav Dua <56474@appareluae.com>
2022-01-21 11:37:36 +0530

    Configrued Resource prehinting in index.html

--- app.js
+++ app.js
@@ -7,32 +7,12 @@ const path = require('path');
 
 const PORT = 3000;
 const app = express();
-function setCustomCacheControl (res, path) {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    if (serveStatic.mime.lookup(path) === 'text/html') {
-      // Custom Cache-Control for HTML files
-      res.setHeader('Cache-Control', 'public, max-age=0')
-      
-      CDN_URL = process.env?.PUBLIC_URL || '';
-      S3_CDN_URL = process.env?.REACT_APP_CDN_API_URL || '';
-      CONFIG_DIRECTORY = process.env?.REACT_APP_REMOTE_CONFIG_DIR || '';
-      if(CDN_URL || S3_CDN_URL){
-        res.setHeader('link', `<${CDN_URL}>; rel="preconnect", <${S3_CDN_URL}>; rel="preconnect", <${S3_CDN_URL}${CONFIG_DIRECTORY}/default.json>; rel="preload" as="fetch"`);
-      }
-    
-    }
-    else {
-        res.append('cache-control', 'public, max-age=259200, must-revalidate');
-    }
-}
 
 app.use(serverTimings);
 proxy(app);
 
 // Serve the static files from the React app
-app.use(serveStatic(path.join(__dirname, 'build'), {
-    setHeaders: setCustomCacheControl
-}))
+app.use(serveStatic(path.join(__dirname, 'build')));
 
 // app.use(express.static(path.join(__dirname, 'build')));
 // Handles any requests that don't match the ones above
