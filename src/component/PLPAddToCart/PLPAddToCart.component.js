169ac04de723c880f78961ead9b9e7c0a9aedbf2
Simmy Gupta <simmy.gupta@6thstreet.com>
2022-01-21 10:47:09 +0530

    onclick drag scroll for add to cart plp

--- src/component/PLPAddToCart/PLPAddToCart.component.js
+++ src/component/PLPAddToCart/PLPAddToCart.component.js
@@ -97,6 +97,39 @@ class PLPAddToCart extends PureComponent {
     this.setState({
       isOutOfStock: outOfStockStatus,
     });
+
+  }
+
+  componentDidUpdate(){
+    const sliders = document.querySelectorAll('.PLPAddToCart-SizeSelector-SizeContainer-AvailableSizes');
+    sliders.forEach((slider)=>{
+      let isDown = false;
+      let startX;
+      let scrollLeft;
+
+      slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+      });
+      slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.classList.remove('active');
+      });
+      slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.classList.remove('active');
+      });
+      slider.addEventListener('mousemove', (e) => {
+        if(!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 3; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+      });
+    })
+
   }
 
   setSizeData = () => {
