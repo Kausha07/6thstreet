7c9d1ebac53b8e07ad952347cd6e73968e31aa47
Barath <barath.kumar@6thstreet.com>
2022-02-06 18:59:31 +0530

    PWA-1978 H1, H2, H3

--- src/component/PDPDetailsSection/PDPDetailsSection.component.js
+++ src/component/PDPDetailsSection/PDPDetailsSection.component.js
@@ -205,13 +205,13 @@ class PDPDetailsSection extends PureComponent {
       <div block="PDPDetailWrapper">
         <div block="PDPDetailWrapper" elem="Items" mods={{ showMore }}>
           <div block="PDPDetailWrapper" elem="LeftDescription">
-            <p
+            <h2
               block="PDPDetailWrapper"
               elem="Title"
               mods={{ isMobile: !!isMobile }}
             >
               {__("PRODUCT DETAILS:")}
-            </p>
+            </h2>
             <p block="PDPDetailsSection" elem="Description">
               {description}
             </p>
