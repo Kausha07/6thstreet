a5854c86fb30783f8a4ff445da5dc0303d2b1fd9
Raghav Dua <56474@appareluae.com>
2022-02-07 16:41:32 +0530

    Fixed size for chat icon Implemented dynamic config for showing discount percentage Fixed API call for VUE Analytics

--- src/component/Price/Price.config.js
+++ src/component/Price/Price.config.js
@@ -8,12 +8,3 @@ export const getFinalPrice = (price, code) => {
     const fixedPrice = FIXED_CURRENCIES.includes(code);
     return fixedPrice && !isNaN(price) ? Number(price).toFixed(3) : price;
 };
-
-export const DISPLAY_DISCOUNT_PERCENTAGE = {
-    "AE": true,
-    "BH": true,
-    "KW": true,
-    "OM": true,
-    "SA": true,
-    "QA": false,
-}
