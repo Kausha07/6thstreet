9ec98de203d0efd31183830f676aee5cfc748214
muskan-appinventiv <muskan.soni@appinventiv.com>
2022-02-08 18:14:40 +0530

    Fixed issue for multiple calls on pressing save address

--- src/store/MyAccount/MyAccount.action.js
+++ src/store/MyAccount/MyAccount.action.js
@@ -12,10 +12,16 @@ export {
 export const SET_IS_MOBILE_TAB_ACTIVE = "SET_IS_MOBILE_TAB_ACTIVE";
 export const SET_GUEST_USER_EMAIL = "SET_GUEST_USER_EMAIL";
 export const SET_CUSTOMER_ADDRESS_DATA = "SET_CUSTOMER_ADDRESS_DATA";
+export const SET_ADDRESS_LOADING_STATUS = "SET_ADDRESS_LOADING_STATUS";
 
 export const setIsMobileTabActive = (isActive) => ({
   type: SET_IS_MOBILE_TAB_ACTIVE,
   isActive,
+});
+
+export const setAddressLoadingStatus = (isLoading) => ({
+  type: SET_ADDRESS_LOADING_STATUS,
+  isLoading,
 });
 
 export const updateGuestUserEmail = (guestUserEmail) => ({
