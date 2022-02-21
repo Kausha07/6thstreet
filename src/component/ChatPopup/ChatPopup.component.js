0f25882976626629ebf99f700f2d5980172546c2
muskan-appinventiv <muskan.soni@appinventiv.com>
2022-02-21 13:18:05 +0530

    Fixed chat bot script loading on window load

--- src/component/ChatPopup/ChatPopup.component.js
+++ src/component/ChatPopup/ChatPopup.component.js
@@ -1,21 +1,22 @@
-import { PureComponent } from 'react';
+import { PureComponent } from "react";
 
-import './ChatPopup.style';
+import "./ChatPopup.style";
 
 class ChatPopup extends PureComponent {
-    render() {
-        if (location.pathname.match(/checkout|cart/)) {
-            return null;
-        }
-
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = process.env.REACT_APP_CHAT_URL;
-
-        document.getElementsByTagName('head')[0].appendChild(script);
-
-        return null;
+  render() {
+    if (location.pathname.match(/checkout|cart/)) {
+      return null;
     }
+    window.onload = function () {
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = process.env.REACT_APP_CHAT_URL;
+
+      document.getElementsByTagName("head")[0].appendChild(script);
+    };
+
+    return null;
+  }
 }
 
 export default ChatPopup;
