d8a46b21c016decd61e5067fccc9c094df7dbc62
muskan-appinventiv <muskan.soni@appinventiv.com>
2022-03-07 11:55:35 +0530

    Fixed scroll issue on explore button tap

--- src/component/HeaderMenu/HeaderMenu.component.js
+++ src/component/HeaderMenu/HeaderMenu.component.js
@@ -1,84 +1,94 @@
-import PropTypes from 'prop-types';
-import { PureComponent } from 'react';
-import { withRouter } from 'react-router';
+import PropTypes from "prop-types";
+import { PureComponent } from "react";
+import { withRouter } from "react-router";
 
-import Menu from 'Component/Menu';
-import { MOBILE_MENU_SIDEBAR_ID } from 'Component/MobileMenuSideBar/MoblieMenuSideBar.config';
-import browserHistory from 'Util/History';
+import Menu from "Component/Menu";
+import { MOBILE_MENU_SIDEBAR_ID } from "Component/MobileMenuSideBar/MoblieMenuSideBar.config";
+import browserHistory from "Util/History";
 
-import './HeaderMenu.style';
+import "./HeaderMenu.style";
 
 class HeaderMenu extends PureComponent {
-    static propTypes = {
-        location: PropTypes.object.isRequired,
-        toggleOverlayByKey: PropTypes.func.isRequired,
-        newMenuGender: PropTypes.string.isRequired,
-        gender: PropTypes.string.isRequired,
-        activeOverlay: PropTypes.string.isRequired,
-        setGender: PropTypes.func.isRequired
+  static propTypes = {
+    location: PropTypes.object.isRequired,
+    toggleOverlayByKey: PropTypes.func.isRequired,
+    newMenuGender: PropTypes.string.isRequired,
+    gender: PropTypes.string.isRequired,
+    activeOverlay: PropTypes.string.isRequired,
+    setGender: PropTypes.func.isRequired,
+  };
+
+  state = {
+    expanded: false,
+  };
+
+  renderMap = {
+    renderCategoriesButton: this.renderCategoriesButton.bind(this),
+  };
+
+  static getDerivedStateFromProps(props) {
+    const {
+      location: { pathname },
+      gender,
+      activeOverlay,
+    } = props;
+
+    return {
+      expanded:
+        pathname.includes(`/${gender}.html`) &&
+        activeOverlay === MOBILE_MENU_SIDEBAR_ID,
     };
+  }
 
-    state = {
-        expanded: false
-    };
+  onCategoriesClick = () => {
+    const { toggleOverlayByKey, gender, setGender, setLastTapItemOnHome } =
+      this.props;
 
-    renderMap = {
-        renderCategoriesButton: this.renderCategoriesButton.bind(this)
-    };
-
-    static getDerivedStateFromProps(props) {
-        const { location: { pathname }, gender, activeOverlay } = props;
-
-        return {
-            expanded: pathname.includes(`/${ gender }.html`) && activeOverlay === MOBILE_MENU_SIDEBAR_ID
-        };
+    if (gender === "home_beauty_women") {
+      setGender("women");
     }
 
-    onCategoriesClick = () => {
-        const { toggleOverlayByKey, gender, setGender } = this.props;
+    this.setState(({ expanded }) => ({ expanded: !expanded }));
+    toggleOverlayByKey(MOBILE_MENU_SIDEBAR_ID);
+    setLastTapItemOnHome("");
+    browserHistory.push(
+      `/${gender === "home_beauty_women" ? "women" : gender}.html`
+    );
+  };
 
-        if (gender === 'home_beauty_women') {
-            setGender('women');
-        }
+  renderMenu() {
+    const { newMenuGender } = this.props;
 
-        this.setState(({ expanded }) => ({ expanded: !expanded }));
-        toggleOverlayByKey(MOBILE_MENU_SIDEBAR_ID);
-        browserHistory.push(`/${ gender === 'home_beauty_women' ? 'women' : gender }.html`);
-    };
+    return <Menu newMenuGender={newMenuGender} />;
+  }
 
-    renderMenu() {
-        const { newMenuGender } = this.props;
+  renderCategoriesButton() {
+    const { expanded } = this.state;
 
-        return (
-            <Menu newMenuGender={ newMenuGender } />
-        );
-    }
+    return (
+      <button
+        mix={{
+          block: "HeaderMenu",
+          elem: "Button",
+          mods: { isExpanded: expanded },
+        }}
+        onClick={this.onCategoriesClick}
+      >
+        {!this.props.isMobileBottomBar && (
+          <label htmlFor="Categories">{__("Categories")}</label>
+        )}
+      </button>
+    );
+  }
 
-    renderCategoriesButton() {
-        const { expanded } = this.state;
-
-        return (
-            <button
-              mix={ {
-                  block: 'HeaderMenu',
-                  elem: 'Button',
-                  mods: { isExpanded: expanded }
-              } }
-              onClick={ this.onCategoriesClick }
-            >
-                {!this.props.isMobileBottomBar && <label htmlFor="Categories">{ __('Categories') }</label>}
-            </button>
-        );
-    }
-
-    render() {
-        return (
-            <div block="HeaderMenu">
-                { this.renderCategoriesButton() }
-                { this.renderMenu() }
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div block="HeaderMenu">
+        {this.renderCategoriesButton()}
+        {this.renderMenu()}
+      </div>
+    );
+  }
 }
 
 export default withRouter(HeaderMenu);
