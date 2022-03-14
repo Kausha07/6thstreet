c6ede7bcbab7f3a8987ef1d47c121a51a8ec6c0c
Simmy Gupta <simmy.gupta@6thstreet.com>
2022-03-14 10:52:51 +0530

    Circle item slider video implementation

--- src/component/DynamicContentCircleItemSlider/CircleItemSliderSubPage.js
+++ src/component/DynamicContentCircleItemSlider/CircleItemSliderSubPage.js
@@ -11,14 +11,15 @@ class CircleItemSliderSubPage extends PureComponent {
     };
 
     render() {
-        let url = this.props.bannerData.plp_config.banner.url
+        let banner = this.props.bannerData.plp_config && this.props.bannerData.plp_config.banner
+        let url = banner.url
         let mWidth = (screen.width - 20).toString() + "px"
-        let mHeight = ((screen.width - 20) / this.props.bannerData.plp_config.banner.aspect_ratio).toString() + "px"
+        let mHeight = ((screen.width - 20) / banner.aspect_ratio).toString() + "px"
         return (
             <div block="CircleItemSliderSubPage">
                 <div block="CircleItemSliderSubPage-Video" style={isMobile.any() && { height: mHeight, width: mWidth }}>
                     {
-                        this.props.bannerData.plp_config.banner.type === "image" ?
+                        banner.type === "image" ?
                             <img src={url} />
                             :
                             <video controls autoplay="true" loop muted>
@@ -27,8 +28,8 @@ class CircleItemSliderSubPage extends PureComponent {
                     }
                 </div>
                 <div block="CircleItemSliderSubPage-Discription">
-                    <h2>{this.props.bannerData.plp_config.banner.title}</h2>
-                    <p>{this.props.bannerData.plp_config.banner.description}</p>
+                    <h2>{banner.title}</h2>
+                    <p>{banner.description}</p>
                 </div>
 
             </div>
