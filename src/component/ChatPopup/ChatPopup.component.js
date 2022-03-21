1d9bcdb9d8548e6653db3a2f7817baee827eae0f
muskan-appinventiv <muskan.soni@appinventiv.com>
2022-03-21 18:48:38 +0530

    Rvereted chat bot script code

--- src/component/ChatPopup/ChatPopup.component.js
+++ src/component/ChatPopup/ChatPopup.component.js
@@ -5,11 +5,11 @@ import "./ChatPopup.style";
 class ChatPopup extends PureComponent {
 
   componentDidMount() {
-    // const script = document.createElement("script");
-    // script.type = "text/javascript";
-    // script.src = process.env.REACT_APP_CHAT_URL;
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = process.env.REACT_APP_CHAT_URL;
 
-    // document.getElementsByTagName("head")[0].appendChild(script);
+    document.getElementsByTagName("head")[0].appendChild(script);
   }
   
   render() {
