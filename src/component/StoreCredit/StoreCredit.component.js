3c38bcc4f08f6045a2ad857df8690d484c7a2688
bhuvanb-wmt <bhuvanb-wmt@webmob.tech>
2022-04-18 13:31:27 +0530

    store credit issue resolved in KW

--- src/component/StoreCredit/StoreCredit.component.js
+++ src/component/StoreCredit/StoreCredit.component.js
@@ -1,106 +1,104 @@
-import PropTypes from 'prop-types';
-import { PureComponent } from 'react';
-
-import Field from 'Component/Field';
-import Loader from 'Component/Loader';
-
-import './StoreCredit.style';
+import Field from "Component/Field";
+import Loader from "Component/Loader";
+import PropTypes from "prop-types";
+import { PureComponent } from "react";
+import "./StoreCredit.style";
 
 export class StoreCredit extends PureComponent {
-    static propTypes = {
-        isLoading: PropTypes.bool.isRequired,
-        canApply: PropTypes.bool.isRequired,
-        hideIfZero: PropTypes.bool.isRequired,
-        creditIsApplied: PropTypes.bool,
-        storeCreditBalance: PropTypes.string.isRequired,
-        toggleStoreCredit: PropTypes.func.isRequired
-    };
+  static propTypes = {
+    isLoading: PropTypes.bool.isRequired,
+    canApply: PropTypes.bool.isRequired,
+    hideIfZero: PropTypes.bool.isRequired,
+    creditIsApplied: PropTypes.bool,
+    storeCreditBalance: PropTypes.string.isRequired,
+    toggleStoreCredit: PropTypes.func.isRequired,
+  };
 
-    static defaultProps = {
-        creditIsApplied: false
-    };
+  static defaultProps = {
+    creditIsApplied: false,
+  };
 
-    componentDidMount() {
-        const { creditIsApplied } = this.props;
-        const { pathname } = location;
+  componentDidMount() {
+    const { creditIsApplied } = this.props;
+    const { pathname } = location;
 
-        if (this.hasCredit() && pathname === '/checkout' && !creditIsApplied) {
-            this.handleCheckboxChange();
-        }
+    if (this.hasCredit() && pathname === "/checkout" && !creditIsApplied) {
+      this.handleCheckboxChange();
+    }
+  }
+
+  hasCredit() {
+    const { storeCreditBalance } = this.props;
+    if (storeCreditBalance && storeCreditBalance.length) {
+      // const formattedStoreCreditBalance = storeCreditBalance.replace("  ", " ");
+      const getCharacters = storeCreditBalance.slice(
+        0,
+        storeCreditBalance.search(/\d/)
+      );
+      //   const [, amount] = formattedStoreCreditBalance.split(" ");
+      const amount = storeCreditBalance.replace(getCharacters, "");
+      return parseFloat(amount.trim()) > 0;
     }
 
-    hasCredit() {
-        const { storeCreditBalance } = this.props;
+    return false;
+  }
 
-        if (storeCreditBalance && storeCreditBalance.length) {
-            const formattedStoreCreditBalance = storeCreditBalance.replace('  ', ' ');
-            const [, amount] = formattedStoreCreditBalance.split(' ');
+  handleCheckboxChange = () => {
+    const { toggleStoreCredit, creditIsApplied } = this.props;
 
-            return parseFloat(amount) > 0;
-        }
+    toggleStoreCredit(!creditIsApplied);
+  };
 
-        return false;
+  renderAmount() {
+    const { canApply, storeCreditBalance } = this.props;
+    const amount = canApply ? `(${storeCreditBalance})` : storeCreditBalance;
+
+    return (
+      <span block="StoreCredit" elem="Amount">
+        {`${amount}`}
+      </span>
+    );
+  }
+
+  renderCheckbox(checkboxId) {
+    const { creditIsApplied } = this.props;
+
+    return (
+      <Field
+        block="StoreCredit"
+        elem="Toggle"
+        type="toggle"
+        id={checkboxId}
+        name={checkboxId}
+        value={checkboxId}
+        checked={creditIsApplied}
+        onClick={this.handleCheckboxChange}
+      />
+    );
+  }
+
+  render() {
+    const { isLoading, canApply, hideIfZero } = this.props;
+    if (hideIfZero && !this.hasCredit()) {
+      return null;
     }
 
-    handleCheckboxChange = () => {
-        const { toggleStoreCredit, creditIsApplied } = this.props;
+    const checkboxId = "store_credit_applied";
+    const label = canApply ? __("Use Store Credit") : __("Store Credit:");
 
-        toggleStoreCredit(!creditIsApplied);
-    };
+    return (
+      <div block="StoreCredit" mods={{ canApply }}>
+        <Loader isLoading={isLoading} />
 
-    renderAmount() {
-        const { canApply, storeCreditBalance } = this.props;
-        const amount = canApply ? `(${ storeCreditBalance })` : storeCreditBalance;
+        {canApply && this.renderCheckbox(checkboxId)}
 
-        return (
-            <span block="StoreCredit" elem="Amount">
-                { `${ amount }` }
-            </span>
-        );
-    }
-
-    renderCheckbox(checkboxId) {
-        const { creditIsApplied } = this.props;
-
-        return (
-            <Field
-              block="StoreCredit"
-              elem="Toggle"
-              type="toggle"
-              id={ checkboxId }
-              name={ checkboxId }
-              value={ checkboxId }
-              checked={ creditIsApplied }
-              onClick={ this.handleCheckboxChange }
-            />
-        );
-    }
-
-    render() {
-        const { isLoading, canApply, hideIfZero } = this.props;
-
-        if (hideIfZero && !this.hasCredit()) {
-            return null;
-        }
-
-        const checkboxId = 'store_credit_applied';
-        const label = canApply
-            ? __('Use Store Credit')
-            : __('Store Credit:');
-
-        return (
-            <div block="StoreCredit" mods={ { canApply } }>
-                <Loader isLoading={ isLoading } />
-
-                { canApply && this.renderCheckbox(checkboxId) }
-
-                <label block="StoreCredit" elem="Label" htmlFor={ checkboxId }>
-                    { label }
-                    { this.renderAmount() }
-                </label>
-            </div>
-        );
-    }
+        <label block="StoreCredit" elem="Label" htmlFor={checkboxId}>
+          {label}
+          {this.renderAmount()}
+        </label>
+      </div>
+    );
+  }
 }
 
 export default StoreCredit;
