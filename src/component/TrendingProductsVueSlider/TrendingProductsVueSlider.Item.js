4a081797e7c57b51d6f1cbe9e63282c6aa6f7908
Simmy-6S <simmy.gupta@6thstreet.com>
2022-04-20 12:31:29 +0530

    Hotfix for sustainable tags

--- src/component/TrendingProductsVueSlider/TrendingProductsVueSlider.Item.js
+++ src/component/TrendingProductsVueSlider/TrendingProductsVueSlider.Item.js
@@ -122,6 +122,14 @@ class DynamicContentVueProductSliderItem extends PureComponent {
     return null;
   }
 
+  renderProductTag(productTag) {
+    return (
+      <div block="VueProductSlider" elem="VueProductTag">
+        <span>{__(productTag)}</span>
+      </div>
+    );
+  }
+
   render() {
     const {
       data: {
@@ -140,6 +148,7 @@ class DynamicContentVueProductSliderItem extends PureComponent {
       widgetID,
     } = this.props;
     const { isArabic } = this.state;
+    let productTag = this.props.data.product_tag ? this.props.data.product_tag : ""
     return (
       <div
         block="VueProductSlider"
@@ -167,7 +176,12 @@ class DynamicContentVueProductSliderItem extends PureComponent {
         <h6 id="brandName">{brand_name}</h6>
         <span id="productName">{name}</span>
         {this.renderPrice(price)}
-        {this.renderIsNew(is_new_in)}
+        {
+          productTag ?
+            this.renderProductTag(productTag)
+            :
+            this.renderIsNew(is_new_in)
+        }
         <WishlistIcon
           sku={sku}
           data={data}
