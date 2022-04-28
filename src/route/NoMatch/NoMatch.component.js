148b1aa97b58302f08fd40496e36a1fc3c29d298
Subodh Kumar <Subodh.Kumar@appareluae.com>
2022-04-28 11:34:13 +0530

    404 page Dynamic widget UI

--- src/route/NoMatch/NoMatch.component.js
+++ src/route/NoMatch/NoMatch.component.js
@@ -41,15 +41,35 @@ export class NoMatch extends PureComponent {
   };
 
   componentDidMount() {
+    this.addTag();
     this.updateBreadcrumbs();
     this.updateHeaderState();
     this.cleanUpTransition();
     window.pageType = TYPE_NOTFOUND;
-    this.requestNoMatchWidgetData();
+    this.requestNoMatchWidgetData();    
+  }
+
+  addTag() {
+    const meta = document.createElement('meta')
+    meta.name = "robots";
+    meta.content = "noindex";
+    if(meta){
+      document.head.append(meta);
+    }
+  }
+
+  removeTag() {
+    const tags = document.querySelectorAll("meta[name=robots]");
+    if(tags){
+      tags.forEach((tag) => {
+        tag.remove();
+      })
+    }
   }
 
   componentWillUnmount() {
     window.pageType=undefined;
+    this.removeTag();
   }
 
   cleanUpTransition() {
@@ -97,76 +117,19 @@ export class NoMatch extends PureComponent {
     // if (gender) {
       try {
         const notFoundWidget = await getStaticFile(HOME_STATIC_FILE_KEY, {
-          //$FILE_NAME: `${devicePrefix}not_found.json`,
-          $FILE_NAME: `${devicePrefix}${gender}.json`,
+          $FILE_NAME: `${devicePrefix}not_found.json`,
+          //$FILE_NAME: `${devicePrefix}${gender}.json`,
         });
         
         if (typeof notFoundWidget === 'object') {
-
-          let newWidgetData = notFoundWidget.filter((data)=>{
-              return data.type=== "grid" && data.tag=== "SBC-Grid-1" || data.type=== "vue_recently_viewed_slider"
-          })
-          const exploreMore = {
-            type: "grid",
-            index: 3,
-            promotion_name: "Women-EN",
-            tag: "",
-            background_color: "#000000",
-            items_per_row: 4,
-            item_height: 240,
-            header:
-            {
-                title: "EXPLORE MORE"
-            },
-            items: [
-            {
-                promotion_name: "Home-SBC-Accessories",
-                footer:
-                {
-                    title: "Women"
-                },
-                link: "/women/accessories.html?q=Women+Accessories&p=0&hFR%5Bcategories.level0%5D%5B0%5D=Women+%2F%2F%2F+Accessories&nR%5Bvisibility_catalog%5D%5B%3D%5D%5B0%5D=1&dFR%5Bgender%5D%5B0%5D=Women&idx=enterprise_magento_english_products",
-                url: "https://mobilecdn.6thstreet.com/AllBanners/bmt/08-04-2022/Shop+by+category/Abaya+copy+4.jpg",
-                tag: "Women"
-            },
-            {
-                promotion_name: "Home-SBC-Footwear",
-                footer:
-                {
-                    title: "Men"
-                },
-                link: "/women/shoes.html?q=Women+Shoes&p=0&hFR%5Bcategories.level0%5D%5B0%5D=Women+%2F%2F%2F+Shoes&nR%5Bvisibility_catalog%5D%5B%3D%5D%5B0%5D=1&dFR%5Bin_stock%5D%5B0%5D=1&dFR%5Bgender%5D%5B0%5D=Women&idx=enterprise_magento_english_products",
-                url: "https://mobilecdn.6thstreet.com/AllBanners/bmt/08-04-2022/Shop+by+category/Shoes+copy+2.jpg",
-                tag: "Men"
-            },
-            {
-                promotion_name: "Home-SBC-Clothing",
-                footer:
-                {
-                    title: "Kids"
-                },
-                link: "/women/clothing.html?q=Women+Clothing&p=0&hFR%5Bcategories.level0%5D%5B0%5D=Women+%2F%2F%2F+Clothing&nR%5Bvisibility_catalog%5D%5B%3D%5D%5B0%5D=1&dFR%5Bgender%5D%5B0%5D=Women&dFR%5Bin_stock%5D%5B0%5D=1&idx=enterprise_magento_english_products",
-                url: "https://mobilecdn.6thstreet.com/AllBanners/bmt/08-04-2022/Shop+by+category/Sleeves+dress+copy.jpg",
-                tag: "Kids"
-            },
-            {
-                promotion_name: "Home-SBC-Sports",
-                footer:
-                {
-                    title: "Home"
-                },
-                link: "/sportswear.html?q=Sportswear&p=0&hFR%5Bcategories.level0%5D%5B0%5D=Sportswear&nR%5Bvisibility_catalog%5D%5B=%5D%5B0%5D%3D1&dFR%5Bgender%5D%5B0%5D=Women&dFR%5Bin_stock%5D%5B0%5D=1&idx=enterprise_magento_english_products",
-                url: "https://mobilecdn.6thstreet.com/AllBanners/bmt/08-04-2022/Shop+by+category/sports+copy.jpg",
-                tag: "Home"
-            }],
-            image_size:
-            {
-                height: 400,
-                width: 400
-            }
-        }
-        newWidgetData.push(exploreMore);
-        this.setState({ notFoundWidgetData: newWidgetData || [] });
+          // let newWidgetData = notFoundWidget.filter((data)=>{
+          //     return data.type=== "grid" && data.tag.includes("Home") || data.type=== "vue_recently_viewed_slider"
+          // })
+          // const exploreMore = {
+          //   ......
+          // }
+          // newWidgetData.push(exploreMore);
+          this.setState({ notFoundWidgetData: notFoundWidget[gender] || [] });
         } else {
           this.setState({ notFoundWidgetData: [] });
         }
