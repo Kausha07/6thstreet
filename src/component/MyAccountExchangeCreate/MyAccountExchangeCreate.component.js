cbbfea667a427453b5f3847288329f0e81890256
muskan-appinventiv <muskan.soni@appinventiv.com>
2022-05-16 18:29:01 +0530

    Added code for slect size and color option on exchange

--- src/component/MyAccountExchangeCreate/MyAccountExchangeCreate.component.js
+++ src/component/MyAccountExchangeCreate/MyAccountExchangeCreate.component.js
@@ -33,8 +33,174 @@ export class MyAccountExchangeCreate extends PureComponent {
     incrementId: "",
     resolutions: [],
   };
-
   
+  static getDerivedStateFromProps(props, state) {
+    const { product } = props;
+
+    const { alsoAvailable, prevAlsoAvailable } = state;
+
+    const derivedState = {};
+
+    if (prevAlsoAvailable !== product["6s_also_available"]) {
+      Object.assign(derivedState, {
+        alsoAvailable: product["6s_also_available"],
+        prevAlsoAvailable: alsoAvailable !== undefined ? alsoAvailable : null,
+      });
+    }
+    return Object.keys(derivedState).length ? derivedState : null;
+  }
+
+  getSizeSelect = () => {
+    const { product, productStock, isArabic, sizeObject, selectedSizeType } =
+      this.props;
+    if (
+      sizeObject?.sizeCodes !== undefined &&
+      Object.keys(productStock || []).length !== 0 &&
+      product[`size_${selectedSizeType}`].length !== 0
+    ) {
+      return (
+        <div
+          block="PLPAddToCart-SizeSelector-SizeContainer"
+          elem="AvailableSizes"
+          mods={{ isArabic }}
+        >
+          {sizeObject.sizeCodes.reduce((acc, code) => {
+            const label = productStock[code].size[selectedSizeType];
+
+            if (label) {
+              acc.push(this.renderSizeOption(productStock, code, label));
+            }
+
+            return acc;
+          }, [])}
+        </div>
+      );
+    }
+
+    return <span id="notavailable">{__("OUT OF STOCK")}</span>;
+  };
+
+  getSizeTypeSelect() {
+    const { selectedSizeType, sizeObject = {}, isArabic, product } = this.props;
+
+    if (this.state.isOutOfStock) {
+      return null;
+    }
+
+    if (sizeObject.sizeTypes !== undefined) {
+      return (
+        <div block="PLPAddToCart" elem="SizeTypeSelect" mods={{ isArabic }}>
+          <select
+            key="SizeTypeSelect"
+            block="PLPAddToCart"
+            elem="SizeTypeSelectElement"
+            value={selectedSizeType}
+            onChange={this.onSizeTypeSelect}
+          >
+            {sizeObject.sizeTypes.map((type = "") => {
+              if (product[`size_${type}`].length > 0) {
+                return (
+                  <option
+                    key={type}
+                    block="PLPAddToCart"
+                    elem="SizeTypeOption"
+                    value={type}
+                  >
+                    {type.toUpperCase()}
+                  </option>
+                );
+              }
+              return null;
+            })}
+          </select>
+        </div>
+      );
+    }
+
+    return null;
+  }
+
+  renderSizeOption(productStock, code, label) {
+    const { selectedSizeCode } = this.props;
+    const isNotAvailable = parseInt(productStock[code].quantity) === 0;
+    const selectedLabelStyle = {
+      fontSize: "14px",
+      color: "#ffffff",
+      fontWeight: 600,
+      letterSpacing: 0,
+      backgroundColor: "#000000",
+    };
+    const selectedStrikeThruLineStyle = {
+      opacity: 0.6,
+      filter: "none",
+    };
+    const isCurrentSizeSelected = selectedSizeCode === code;
+
+    return (
+      <div
+        block="PLPAddToCart-SizeSelector"
+        elem={isNotAvailable ? "SizeOptionContainerOOS" : "SizeOptionContainer"}
+        key={v4()}
+        className="SizeOptionList"
+        onClick={() => {
+          this.onSizeSelect({ target: { value: code } });
+        }}
+      >
+        <input
+          id={code}
+          key={code}
+          type="radio"
+          elem="SizeOption"
+          name="size"
+          block="PLPAddToCart"
+          value={code}
+          checked={isCurrentSizeSelected}
+        />
+        <div>
+          <label
+            htmlFor={code}
+            style={isCurrentSizeSelected ? selectedLabelStyle : {}}
+          >
+            {label}
+          </label>
+          {isNotAvailable && (
+            <Image
+              lazyLoad={false}
+              src={StrikeThrough}
+              className="lineImg"
+              style={isCurrentSizeSelected ? selectedStrikeThruLineStyle : {}}
+            />
+          )}
+        </div>
+        <div />
+      </div>
+    );
+  }
+
+  onSizeSelect = ({ target }) => {
+    const { value } = target;
+    const { productStock, isOutOfStock } = this.props;
+    let outOfStockVal = isOutOfStock;
+    if (productStock && productStock[value]) {
+      const selectedSize = productStock[value];
+      if (
+        selectedSize["quantity"] !== undefined &&
+        selectedSize["quantity"] !== null &&
+        (typeof selectedSize["quantity"] === "string"
+          ? parseInt(selectedSize["quantity"], 0) === 0
+          : selectedSize["quantity"] === 0)
+      ) {
+        outOfStockVal = true;
+      } else {
+        outOfStockVal = false;
+      }
+    }
+    this.setState({
+      selectedSizeCode: value,
+      isOutOfStock: outOfStockVal,
+    });
+  };
+
   renderOrderItem = (item) => {
     const { item_id } = item;
     const { onItemClick, onResolutionChange, onReasonChange, resolutions } =
@@ -43,7 +209,7 @@ export class MyAccountExchangeCreate extends PureComponent {
     if (!item.is_exchangable) {
       return false;
     }
-    
+
     return (
       <li block="MyAccountExchangeCreate" elem="Item" key={item_id}>
         <MyAccountReturnCreateItem
@@ -68,8 +234,7 @@ export class MyAccountExchangeCreate extends PureComponent {
   }
 
   renderResolutions() {
-    const { resolutions, onResolutionChangeValue } =
-      this.props;
+    const { resolutions, onResolutionChangeValue } = this.props;
     const { pathname = "" } = location;
     const isCancel = pathname.includes("/exchange-item/cancel");
     const resolutionValue = resolutions.map(({ id, label }) => ({
@@ -169,6 +334,51 @@ export class MyAccountExchangeCreate extends PureComponent {
     );
   }
 
+  renderSizeContent = () => {
+    const { sizeObject } = this.props;
+    return (
+      <div block="PLPAddToCart" elem="SizeSelector">
+        {sizeObject.sizeTypes !== undefined &&
+        sizeObject.sizeTypes.length !== 0 ? (
+          <>
+            <div block="PDPAddToCart" elem="SizeInfoContainer">
+              <h2 block="PDPAddToCart-SizeInfoContainer" elem="title">
+                {__("Select a Size")}
+              </h2>
+            </div>
+            <div block="PLPAddToCart-SizeSelector" elem="SizeTypeContainer">
+              {this.getSizeTypeSelect()}
+            </div>
+            <div block="PLPAddToCart-SizeSelector" elem="SizeContainer">
+              {this.getSizeSelect()}
+            </div>
+          </>
+        ) : null}
+      </div>
+    );
+  };
+  
+  renderAvailableItemsSection() {
+    const {
+      product: { sku },
+      isLoading,
+      alsoAvailable
+    } = this.props;
+
+    if (alsoAvailable) {
+      if (alsoAvailable.length > 0 && !isLoading) {
+        return (
+          <PDPAlsoAvailable
+            productsAvailable={alsoAvailable}
+            renderMySignInPopup={renderMySignInPopup}
+            productSku={sku}
+          />
+        );
+      }
+    }
+
+    return null;
+  }
   render() {
     return (
       <div block="MyAccountExchangeCreate">
--- src/component/MyAccountExchangeCreate/MyAccountExchangeCreate.container.js
+++ src/component/MyAccountExchangeCreate/MyAccountExchangeCreate.container.js
@@ -1,186 +1,198 @@
-import PropTypes from 'prop-types';
-import { PureComponent } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
+import PropTypes from "prop-types";
+import { PureComponent } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
 
-import { showNotification } from 'Store/Notification/Notification.action';
-import { HistoryType, MatchType } from 'Type/Common';
-import MagentoAPI from 'Util/API/provider/MagentoAPI';
+import { showNotification } from "Store/Notification/Notification.action";
+import { HistoryType, MatchType } from "Type/Common";
+import MagentoAPI from "Util/API/provider/MagentoAPI";
 
-import MyAccountExchangeCreate from './MyAccountExchangeCreate.component';
+import MyAccountExchangeCreate from "./MyAccountExchangeCreate.component";
 
 export const mapStateToProps = () => ({});
 
 export const mapDispatchToProps = (dispatch) => ({
-    showErrorMessage: (message) => dispatch(showNotification('error', message))
+  showErrorMessage: (message) => dispatch(showNotification("error", message)),
 });
 
 export class MyAccountExchangeCreateContainer extends PureComponent {
-    static propTypes = {
-        match: MatchType.isRequired,
-        history: HistoryType.isRequired,
-        showErrorMessage: PropTypes.func.isRequired
+  static propTypes = {
+    match: MatchType.isRequired,
+    history: HistoryType.isRequired,
+    showErrorMessage: PropTypes.func.isRequired,
+  };
+
+  containerFunctions = {
+    onFormSubmit: this.onFormSubmit.bind(this),
+    onItemClick: this.onItemClick.bind(this),
+    onReasonChange: this.onReasonChange.bind(this),
+    onResolutionChange: this.onResolutionChange.bind(this),
+    handleDiscardClick: this.onDiscardClick.bind(this),
+    onResolutionChangeValue: this.onResolutionChangeValue.bind(this),
+  };
+
+  state = {
+    selectedItems: {},
+    isLoading: true,
+    incrementId: "",
+    items: [],
+    resolutionId: null,
+    reasonId: 0,
+    sizeObject: {},
+    selectedSizeCode: "",
+    selectedSizeType: "eu",
+    isOutOfStock: false,
+    isArabic: isArabic(),
+    alsoAvailable: [],
+    prevAlsoAvailable: [],
+  };
+
+  componentDidMount() {
+    this.getExchangableItems();
+  }
+
+  containerProps = () => {
+    const { history } = this.props;
+    const {
+      isLoading,
+      incrementId,
+      items,
+      selectedItems = {},
+      resolutions,
+      resolutionId,
+      reasonId,
+    } = this.state;
+
+    return {
+      isLoading,
+      incrementId,
+      items,
+      selectedNumber: Object.keys(selectedItems).length,
+      history,
+      resolutions,
+      resolutionId,
+      reasonId,
     };
+  };
+  onDiscardClick() {
+    const { history } = this.props;
+    const orderId = this.getOrderId();
+    history.push(`/my-account/my-orders/${orderId}`);
+  }
 
-    containerFunctions = {
-        onFormSubmit: this.onFormSubmit.bind(this),
-        onItemClick: this.onItemClick.bind(this),
-        onReasonChange: this.onReasonChange.bind(this),
-        onResolutionChange: this.onResolutionChange.bind(this),
-        handleDiscardClick: this.onDiscardClick.bind(this),
-        onResolutionChangeValue: this.onResolutionChangeValue.bind(this)
-    };
+  getOrderId() {
+    const { match: { params: { order } = {} } = {} } = this.props;
 
-    state = {
-        selectedItems: {},
-        isLoading: true,
-        incrementId: '',
-        items: [],
-        resolutionId: null,
-        reasonId:0,
-    };
+    return order;
+  }
 
-    componentDidMount() {
-        this.getExchangableItems();
-    }
+  getExchangableItems() {
+    const { showErrorMessage } = this.props;
+    const orderId = this.getOrderId();
 
-    containerProps = () => {
-        const { history } = this.props;
-        const {
-            isLoading,
-            incrementId,
-            items,
-            selectedItems = {},
-            resolutions,
-            resolutionId,
-            reasonId
-        } = this.state;
+    this.setState({ isLoading: true });
 
-        return {
-            isLoading,
-            incrementId,
-            items,
-            selectedNumber: Object.keys(selectedItems).length,
-            history,
-            resolutions,
-            resolutionId,
-            reasonId
-        };
-    };
-    onDiscardClick() {
-        const { history } = this.props;
-        const orderId = this.getOrderId();
-        history.push(`/my-account/my-orders/${orderId}`);
-    }
-
-    getOrderId() {
-        const {
-            match: {
-                params: {
-                    order
-                } = {}
-            } = {}
-        } = this.props;
-
-        return order;
-    }
-
-    getExchangableItems() {
-        const { showErrorMessage } = this.props;
-        const orderId = this.getOrderId();
-
-        this.setState({ isLoading: true });
-
-        MagentoAPI.get(`orders/${orderId}/exchangable-items`)
-            .then(({ data: { items, order_increment_id, resolution_options } }) => {
-                this.setState({
-                    items,
-                    incrementId: order_increment_id,
-                    isLoading: false,
-                    resolutions: resolution_options
-                });
-            }).catch(() => {
-                showErrorMessage(__('Error appeared while fetching exchangable items'));
-                this.setState({ isLoading: false });
-            });
-    }
-
-    onItemClick(itemId, isSelected) {
-        this.setState(({ selectedItems }) => {
-            if (!isSelected) {
-                // eslint-disable-next-line no-unused-vars
-                const { [itemId]: _, ...newSelectedItems } = selectedItems;
-                return { selectedItems: newSelectedItems };
-            }
-
-            return { selectedItems: { ...selectedItems, [itemId]: false } };
+    MagentoAPI.get(`orders/${orderId}/exchangable-items`)
+      .then(({ data: { items, order_increment_id, resolution_options } }) => {
+        this.setState({
+          items,
+          incrementId: order_increment_id,
+          isLoading: false,
+          resolutions: resolution_options,
         });
-    }
+      })
+      .catch(() => {
+        showErrorMessage(__("Error appeared while fetching exchangable items"));
+        this.setState({ isLoading: false });
+      });
+  }
 
-    onResolutionChange(itemId, resolutionId) {
-        const { selectedItems: { [itemId]: item }, } = this.state;
+  onItemClick(itemId, isSelected) {
+    this.setState(({ selectedItems }) => {
+      if (!isSelected) {
+        // eslint-disable-next-line no-unused-vars
+        const { [itemId]: _, ...newSelectedItems } = selectedItems;
+        return { selectedItems: newSelectedItems };
+      }
 
-        this.setState(({ selectedItems }) => ({
-            selectedItems: { ...selectedItems, [itemId]: { ...item, resolutionId } }
-        }));
-    }
+      return { selectedItems: { ...selectedItems, [itemId]: false } };
+    });
+  }
 
-    onReasonChange(itemId, reasonId) {
-        const { selectedItems: { [itemId]: item } } = this.state;
-        
-        this.setState(({ selectedItems }) => ({
-            selectedItems: { ...selectedItems, [itemId]: { ...item, reasonId } }
-        }));
+  onResolutionChange(itemId, resolutionId) {
+    const {
+      selectedItems: { [itemId]: item },
+    } = this.state;
 
-        this.setState({reasonId: reasonId})
+    this.setState(({ selectedItems }) => ({
+      selectedItems: { ...selectedItems, [itemId]: { ...item, resolutionId } },
+    }));
+  }
 
-        this.onResolutionChangeValue({resolutionId:false});
-    }
-    onResolutionChangeValue(value) {
-        this.setState({ resolutionId: value })
-    }
-    onFormSubmit() {
-        const { history, showErrorMessage } = this.props;
-        const { selectedItems = {}, items, resolutionId } = this.state;
-        const payload = {
-            order_id: this.getOrderId(),
-            items: Object.entries(selectedItems).map(([order_item_id, { reasonId, resolutionIdd }]) => {
-                const {
-                    qty_shipped = 0
-                } = items.find(({ item_id }) => item_id === order_item_id) || {};
+  onReasonChange(itemId, reasonId) {
+    const {
+      selectedItems: { [itemId]: item },
+    } = this.state;
 
-                return {
-                    order_item_id,
-                    qty_requested: qty_shipped,
-                    resolution: {
-                        id: resolutionId,
-                        data: null
-                    },
-                    reason: {
-                        id: reasonId,
-                        data: null
-                    }
-                };
-            })
-        };
-        this.setState({ isLoading: true });
+    this.setState(({ selectedItems }) => ({
+      selectedItems: { ...selectedItems, [itemId]: { ...item, reasonId } },
+    }));
 
-        MagentoAPI.post('exchange/request', payload).then(({ data: { id } }) => {
-            history.push(`/my-account/exchange-item/create/success/${id}`);
-        }).catch(() => {
-            showErrorMessage(__('Error appeared while requesting a exchange'));
-            this.setState({ isLoading: false });
-        });
-    }
+    this.setState({ reasonId: reasonId });
 
-    render() {
-        return (
-            <MyAccountExchangeCreate
-                {...this.containerFunctions}
-                {...this.containerProps()}
-            />
-        );
-    }
+    this.onResolutionChangeValue({ resolutionId: false });
+  }
+  onResolutionChangeValue(value) {
+    this.setState({ resolutionId: value });
+  }
+  onFormSubmit() {
+    const { history, showErrorMessage } = this.props;
+    const { selectedItems = {}, items, resolutionId } = this.state;
+    const payload = {
+      order_id: this.getOrderId(),
+      items: Object.entries(selectedItems).map(
+        ([order_item_id, { reasonId, resolutionIdd }]) => {
+          const { qty_shipped = 0 } =
+            items.find(({ item_id }) => item_id === order_item_id) || {};
+
+          return {
+            order_item_id,
+            qty_requested: qty_shipped,
+            resolution: {
+              id: resolutionId,
+              data: null,
+            },
+            reason: {
+              id: reasonId,
+              data: null,
+            },
+          };
+        }
+      ),
+    };
+    this.setState({ isLoading: true });
+
+    MagentoAPI.post("exchange/request", payload)
+      .then(({ data: { id } }) => {
+        history.push(`/my-account/exchange-item/create/success/${id}`);
+      })
+      .catch(() => {
+        showErrorMessage(__("Error appeared while requesting a exchange"));
+        this.setState({ isLoading: false });
+      });
+  }
+
+  render() {
+    return (
+      <MyAccountExchangeCreate
+        {...this.state}
+        {...this.containerFunctions}
+        {...this.containerProps()}
+      />
+    );
+  }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(MyAccountExchangeCreateContainer));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(MyAccountExchangeCreateContainer)
+);
