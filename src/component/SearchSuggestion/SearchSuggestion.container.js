7a7f00069810bbb7d42494596b533e188e4708c8
muskan-appinventiv <muskan.soni@appinventiv.com>
2022-05-19 11:56:24 +0530

    Fixed multiple algolia call at search bar tap by wrapping all into one

--- src/component/SearchSuggestion/SearchSuggestion.container.js
+++ src/component/SearchSuggestion/SearchSuggestion.container.js
@@ -196,7 +196,7 @@ export class SearchSuggestionContainer extends PureComponent {
     browserHistory.push(`${location.pathname}${location.search}`);
     window.onpopstate = () => {
       closeSearch();
-    }
+    };
   }
 
   componentWillUnmount() {
@@ -225,18 +225,21 @@ export class SearchSuggestionContainer extends PureComponent {
   async requestTopSearches() {
     const topSearches = await new Algolia().getTopSearches();
     let refinedTopSearches = [];
+    let searchItem = [];
     await Promise.all(
       topSearches?.data
         ?.filter((ele) => ele !== "")
         .map(async (item) => {
-          const filteredItem = await this.checkForSKU(item.search);
-          if (filteredItem) {
-            refinedTopSearches.push({ link: filteredItem.url, ...item });
-          } else {
-            refinedTopSearches.push({ link: null, ...item });
-          }
+          searchItem.push(item.search);
         })
     );
+    if (topSearches?.data) {
+      refinedTopSearches = await this.checkForSearchSKU(
+        searchItem,
+        topSearches.data
+      );
+    }
+
     this.setState({
       topSearches: refinedTopSearches || [],
     });
@@ -246,16 +249,18 @@ export class SearchSuggestionContainer extends PureComponent {
     let recentSearches =
       JSON.parse(localStorage.getItem("recentSearches")) || [];
     let refinedRecentSearches = [];
+    let searchItem = [];
     if (recentSearches.length > 0) {
       await Promise.all(
         recentSearches?.map(async (item) => {
-          const filteredItem = await this.checkForSKU(item.name);
-          if (filteredItem) {
-            refinedRecentSearches.push({ link: filteredItem.url, ...item });
-          } else {
-            refinedRecentSearches.push({ link: null, ...item });
-          }
+          searchItem.push(item.name);
         })
+      );
+    }
+    if (recentSearches.length > 0) {
+      refinedRecentSearches = await this.checkForSearchSKU(
+        searchItem,
+        recentSearches
       );
     }
     this.setState({
@@ -263,17 +268,22 @@ export class SearchSuggestionContainer extends PureComponent {
     });
   }
 
-  checkForSKU = async (search) => {
-    const config = {
-      q: search,
-      page: 0,
-      limit: 2,
-    };
-    const { data } = await new Algolia().getPLP(config);
-    if (data && data.length === 1) {
-      return data[0];
+  checkForSearchSKU = async (searchArr, searchList) => {
+    const { data } = await new Algolia().getSearchPLP(searchArr);
+    let refinedSearches = [];
+    if (data && data.length > 0) {
+      data.map((subData, index) => {
+        if (subData && subData.hits.length === 1) {
+          refinedSearches.push({
+            link: subData.hits[0].url,
+            ...searchList[index],
+          });
+        } else {
+          refinedSearches.push({ link: null, ...searchList[index] });
+        }
+      });
     }
-    return null;
+    return refinedSearches;
   };
 
   containerProps = () => {
@@ -294,7 +304,7 @@ export class SearchSuggestionContainer extends PureComponent {
       renderMySignInPopup,
       // wishlistData,
       isPDPSearchVisible,
-      prevPath
+      prevPath,
     } = this.props;
     const { brands = [], products = [] } = data;
     const isEmpty = search === "";
@@ -318,7 +328,7 @@ export class SearchSuggestionContainer extends PureComponent {
       trendingProducts,
       renderMySignInPopup,
       isPDPSearchVisible,
-      prevPath
+      prevPath,
       // wishlistData,
     };
   };
