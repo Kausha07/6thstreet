f5e84273a744e59e8573fe5eafa3e4ff4ce3cce5
Bhuvan Bakshi <bhuvanb@webmob.tech>
2022-05-19 14:10:50 +0530

    resolve multiple query in pdp

--- src/component/PDPSummary/PDPSummary.container.js
+++ src/component/PDPSummary/PDPSummary.container.js
@@ -6,78 +6,83 @@ import { Product } from 'Util/API/endpoint/Product/Product.type';
 
 import PDPSummary from './PDPSummary.component';
 
-import Algolia from "Util/API/provider/Algolia"; 
+import Algolia from "Util/API/provider/Algolia";
 
 export const mapStateToProps = (state) => ({
-    product: state.PDP.product,
-    isLoading: state.PDP.isLoading,
-    brand_url : state.PLP.brand_url
+  product: state.PDP.product,
+  isLoading: state.PDP.isLoading,
+  brand_url: state.PLP.brand_url
 });
 
 export const mapDispatchToProps = (_dispatch) => ({});
 export class PDPSummaryContainer extends PureComponent {
-    static propTypes = {
-        product: Product.isRequired,
-        isLoading: PropTypes.bool.isRequired
-    };
+  static propTypes = {
+    product: Product.isRequired,
+    isLoading: PropTypes.bool.isRequired
+  };
 
-    containerFunctions = {};
+  containerFunctions = {};
 
-    containerProps = () => {
-        const { product, isLoading,renderMySignInPopup } = this.props;
-        return { product, isLoading,renderMySignInPopup };
-    };
+  containerProps = () => {
+    const { product, isLoading, renderMySignInPopup } = this.props;
+    return { product, isLoading, renderMySignInPopup };
+  };
 
-    constructor(props) {
-        super(props);
-        this.getBrandDetails = this.getBrandDetails.bind(this);
-        this.state = {
-            url_path : ""
-        }
+  constructor(props) {
+    super(props);
+    this.getBrandDetails = this.getBrandDetails.bind(this);
+    this.state = {
+      url_path: ""
     }
-    
-    componentDidUpdate() {
-        const { brand_url} = this.props;
-        if(!brand_url) {
-            this.getBrandDetails(); 
-        }
-        else
-        {
-            this.setState({
-                url_path: brand_url
-            })
-        }
-    }
+  }
 
-    async getBrandDetails() {
-        const { product: { brand_name } } = this.props;
-        try {
-            const data = await new Algolia({
-              index: "brands_info",
-            })
-            .getBrandsDetails({
-              query: brand_name,
-              limit: 1,
-            });
-            this.setState({
-              url_path: data?.hits[0]?.url_path
-            });
-        }
-        catch(err) {
-            console.error(err);
-        }
+  componentDidMount() {
+    const { brand_url } = this.props;
+    if (!brand_url) {
+      this.getBrandDetails();
     }
+    else {
+      this.setState({
+        url_path: brand_url
+      })
+    }
+  }
 
-    render() {
-        const {url_path} = this.state;
-        return (
-            <PDPSummary
-              { ...this.containerFunctions }
-              { ...this.containerProps() }
-              url_path={url_path}
-            />
-        );
+  componentDidUpdate() {
+    // 
+  }
+
+  async getBrandDetails() {
+    const { product: { brand_name } } = this.props;
+    if(brand_name) {
+      try {
+      const data = await new Algolia({
+        index: "brands_info",
+      })
+        .getBrandsDetails({
+          query: brand_name,
+          limit: 1,
+        });
+      this.setState({
+        url_path: data?.hits[0]?.url_path
+      });
     }
+    catch (err) {
+      console.error(err);
+    }
+    }
+  }
+
+  render() {
+    const { url_path } = this.state;
+    return (
+      <PDPSummary
+        {...this.containerFunctions}
+        {...this.containerProps()}
+        url_path={url_path}
+      />
+    );
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PDPSummaryContainer);
