b1e0a6b8870753476011b2969eaef8559b6a40b2
muskan-appinventiv <muskan.soni@appinventiv.com>
2022-05-24 09:56:37 +0530

    Added event tracking on order view page

--- src/component/MyAccountOrderView/MyAccountOrderView.component.js
+++ src/component/MyAccountOrderView/MyAccountOrderView.component.js
@@ -29,6 +29,7 @@ import {
   CHECK_MONEY,
   TABBY_ISTALLMENTS,
 } from "../CheckoutPayments/CheckoutPayments.config";
+import Event, { EVENT_GTM_EDD_VISIBILITY } from "Util/Event";
 import Applepay from "./icons/apple.png";
 import CancelledImage from "./icons/cancelled.png";
 import CloseImage from "./icons/close.png";
@@ -110,7 +111,7 @@ class MyAccountOrderView extends PureComponent {
       eddResponse,
       edd_info,
     } = this.props;
-    const {eddEventSent} = this.state
+    const { eddEventSent } = this.state;
     let finalEdd =
       item.status === "Processing" || item.status === "processing"
         ? eddItem?.edd
@@ -399,9 +400,21 @@ class MyAccountOrderView extends PureComponent {
   }
   renderEdd = (finalEdd, colorCode) => {
     let actualEddMess = finalEdd;
-
+    const { eddEventSent } = this.state;
+    const { edd_info } = this.props;
     if (!actualEddMess) {
       return null;
+    }
+    if (actualEddMess && eddEventSent) {
+      Event.dispatch(EVENT_GTM_EDD_VISIBILITY, {
+        edd_details: {
+          edd_status: edd_info.has_order_detail,
+          default_edd_status: null,
+          edd_updated: null,
+        },
+        page: "my_order",
+      });
+      this.setEddEventSent();
     }
     let splitKey = isArabic() ? "بواسطه" : "by";
     let finalColorCode = colorCode ? colorCode : SPECIAL_COLORS["shamrock"];
