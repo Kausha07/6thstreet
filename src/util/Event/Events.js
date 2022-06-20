1c1c9db4cc946f0b82bec77aa4b106a4602d4af2
imbarathk <barath.kumar@6thstreet.com>
2022-06-20 14:54:30 +0530

    Search event chnage

--- src/util/Event/Events.js
+++ src/util/Event/Events.js
@@ -3,65 +3,65 @@
  *
  * @type {string}
  */
- export const EVENT_GTM_CHECKOUT = "gtm_checkout";
- export const EVENT_GTM_CHECKOUT_OPTION = "gtm_checkout_option";
- export const EVENT_GTM_IMPRESSIONS_PLP = "gtm_impressions_plp";
- export const EVENT_GTM_IMPRESSIONS_HOME = "gtm_impressions_home";
- export const EVENT_GTM_IMPRESSIONS_CROSS_SELL = "gtm_impressions_cross_sell";
- export const EVENT_GTM_IMPRESSIONS_WISHLIST = "gtm_impressions_wishlist";
- export const EVENT_GTM_IMPRESSIONS_SEARCH = "gtm_impressions_search";
- export const EVENT_GTM_IMPRESSIONS_LINKED = "gtm_impressions_linked";
- export const EVENT_GTM_META_UPDATE = "gtm_meta_update";
- export const EVENT_GTM_GENERAL_INIT = "gtm_general_init";
- export const EVENT_GTM_PRODUCT_ADD_TO_CART = "gtm_product_add_to_cart";
- export const EVENT_GTM_EDD_VISIBILITY = "gtm_edd_visibility";
- export const EVENT_GTM_EDD_TRACK_ON_ORDER = "gtm_edd_track_on_order";
- export const EVENT_GTM_PRODUCT_CLICK = "gtm_product_click";
- export const EVENT_GTM_VUE_PRODUCT_CLICK = "gtm_vue_product_click";
- export const EVENT_GTM_WISHLIST_PRODUCT_CLICK = "gtm_wishlist_product_click";
- export const EVENT_GTM_PRODUCT_DETAIL = "gtm_product_detail";
- export const EVENT_GTM_PRODUCT_REMOVE_FROM_CART = "gtm_product_remove_from_cart";
- export const EVENT_GTM_PURCHASE = "gtm_purchase";
- export const EVENT_GTM_BANNER_CLICK = "gtm_banner_widget_click";
- 
- // new events
- export const EVENT_GTM_TRENDING_BRANDS_CLICK = "gtm_trending_brands_click";
- export const EVENT_GTM_TRENDING_TAGS_CLICK = "gtm_trending_tags_click";
- export const EVENT_GTM_BRANDS_CLICK = "gtm_brands_click";
- export const EVENT_GTM_RECOMMENDED_CLICK = "gtm_recommended_click";
- export const EVENT_GTM_PRODUCT_ADD_TO_WISHLIST = "gtm_product_add_to_wishlist";
- export const EVENT_GTM_PRODUCT_REMOVE_FROM_WISHLIST = "gtm_product_remove_from_wishlist";
- 
- export const VIEW_SEARCH_RESULTS_ALGOLIA = "view_search_results_algolia";
- export const SELECT_ITEM_ALGOLIA = "select_item_algolia";
- export const ADD_TO_CART_ALGOLIA = "add_to_cart_algolia";
- export const VUE_CAROUSEL_SHOW = "carouselShow";
- export const VUE_CAROUSEL_CLICK = "carouselClick";
- export const VUE_CAROUSEL_SWIPE = "carouselSwipe";
- export const VUE_PAGE_VIEW = "pageView";
- export const VUE_ADD_TO_CART = "addToCart";
- export const VUE_REMOVE_FROM_CART = "removeFromCart";
- export const VUE_ADD_TO_WISHLIST = "addToWishlist";
- export const VUE_REMOVE_TO_WISHLIST = "removeFromWishlist";
- export const VUE_BUY = "buy";
- export const VUE_PLACE_ORDER = "placeOrder";
- export const EVENT_PROMOTION_IMPRESSION = "promotionImpression";
- export const EVENT_CLICK_PROMOTION_IMPRESSION = "promotionClick";
- export const EVENT_PRODUCT_IMPRESSION = "productImpression";
- 
- export const EVENT_GTM_CANCEL_SEARCH = "cancel_search";
- export const EVENT_GTM_CLEAR_SEARCH = "clear_search";
- export const EVENT_GTM_GO_TO_SEARCH = "go_to_search";
- export const EVENT_CLICK_SEARCH_QUERY_SUGGESSTION_CLICK = "search_query_suggestion_click";
- export const EVENT_CLICK_SEARCH_WISH_LIST_CLICK = "search_wish_list_click";
- export const EVENT_GTM_VIEW_SEARCH_RESULTS = "view_search_results";
- export const EVENT_GTM_NO_RESULT_SEARCH_SCREEN_VIEW = "no_result_search_screen_view";
- export const EVENT_CLICK_RECENT_SEARCHES_CLICK = "recent_searches_click";
- export const EVENT_EXPLORE_MORE_SEARCH_CLICK = "search_explore_more";
- export const EVENT_GTM_SEARCH_LOGS_SCREEN_VIEW = "search_logs_screen_view";
- export const EVENT_GTM_SEARCH_SCREEN_VIEW = "search_screen_view";
- export const EVENT_GTM_SEARCH = "search";
- export const EVENT_CLICK_TOP_SEARCHES_CLICK = "top_searches_click";
- export const EVENT_CLICK_RECOMMENDATION_CLICK = "recommendation_clicked";
- export const EVENT_SEARCH_SUGGESTION_PRODUCT_CLICK = "search_product_suggestion_click";
- export const EVENT_PAGE_NOT_FOUND = "pageNotFound";
+export const EVENT_GTM_CHECKOUT = "gtm_checkout";
+export const EVENT_GTM_CHECKOUT_OPTION = "gtm_checkout_option";
+export const EVENT_GTM_IMPRESSIONS_PLP = "gtm_impressions_plp";
+export const EVENT_GTM_IMPRESSIONS_HOME = "gtm_impressions_home";
+export const EVENT_GTM_IMPRESSIONS_CROSS_SELL = "gtm_impressions_cross_sell";
+export const EVENT_GTM_IMPRESSIONS_WISHLIST = "gtm_impressions_wishlist";
+export const EVENT_GTM_IMPRESSIONS_SEARCH = "gtm_impressions_search";
+export const EVENT_GTM_IMPRESSIONS_LINKED = "gtm_impressions_linked";
+export const EVENT_GTM_META_UPDATE = "gtm_meta_update";
+export const EVENT_GTM_GENERAL_INIT = "gtm_general_init";
+export const EVENT_GTM_PRODUCT_ADD_TO_CART = "gtm_product_add_to_cart";
+export const EVENT_GTM_EDD_VISIBILITY = "gtm_edd_visibility";
+export const EVENT_GTM_EDD_TRACK_ON_ORDER = "gtm_edd_track_on_order";
+export const EVENT_GTM_PRODUCT_CLICK = "gtm_product_click";
+export const EVENT_GTM_VUE_PRODUCT_CLICK = "gtm_vue_product_click";
+export const EVENT_GTM_WISHLIST_PRODUCT_CLICK = "gtm_wishlist_product_click";
+export const EVENT_GTM_PRODUCT_DETAIL = "gtm_product_detail";
+export const EVENT_GTM_PRODUCT_REMOVE_FROM_CART = "gtm_product_remove_from_cart";
+export const EVENT_GTM_PURCHASE = "gtm_purchase";
+export const EVENT_GTM_BANNER_CLICK = "gtm_banner_widget_click";
+
+// new events
+export const EVENT_GTM_TRENDING_BRANDS_CLICK = "gtm_trending_brands_click";
+export const EVENT_GTM_TRENDING_TAGS_CLICK = "gtm_trending_tags_click";
+export const EVENT_GTM_BRANDS_CLICK = "gtm_brands_click";
+export const EVENT_GTM_RECOMMENDED_CLICK = "gtm_recommended_click";
+export const EVENT_GTM_PRODUCT_ADD_TO_WISHLIST = "gtm_product_add_to_wishlist";
+export const EVENT_GTM_PRODUCT_REMOVE_FROM_WISHLIST = "gtm_product_remove_from_wishlist";
+
+export const VIEW_SEARCH_RESULTS_ALGOLIA = "view_search_results_algolia";
+export const SELECT_ITEM_ALGOLIA = "select_item_algolia";
+export const ADD_TO_CART_ALGOLIA = "add_to_cart_algolia";
+export const VUE_CAROUSEL_SHOW = "carouselShow";
+export const VUE_CAROUSEL_CLICK = "carouselClick";
+export const VUE_CAROUSEL_SWIPE = "carouselSwipe";
+export const VUE_PAGE_VIEW = "pageView";
+export const VUE_ADD_TO_CART = "addToCart";
+export const VUE_REMOVE_FROM_CART = "removeFromCart";
+export const VUE_ADD_TO_WISHLIST = "addToWishlist";
+export const VUE_REMOVE_TO_WISHLIST = "removeFromWishlist";
+export const VUE_BUY = "buy";
+export const VUE_PLACE_ORDER = "placeOrder";
+export const EVENT_PROMOTION_IMPRESSION = "promotionImpression";
+export const EVENT_CLICK_PROMOTION_IMPRESSION = "promotionClick";
+export const EVENT_PRODUCT_IMPRESSION = "productImpression";
+
+export const EVENT_GTM_CANCEL_SEARCH = "cancel_search";
+export const EVENT_GTM_CLEAR_SEARCH = "clear_search";
+export const EVENT_GTM_GO_TO_SEARCH = "go_to_search";
+export const EVENT_CLICK_SEARCH_QUERY_SUGGESSTION_CLICK = "search_query_suggestion_click";
+export const EVENT_CLICK_SEARCH_WISH_LIST_CLICK = "search_wish_list_click";
+export const EVENT_GTM_VIEW_SEARCH_RESULTS = "view_search_results";
+export const EVENT_GTM_NO_RESULT_SEARCH_SCREEN_VIEW = "no_result_search_screen_view";
+export const EVENT_CLICK_RECENT_SEARCHES_CLICK = "recent_searches_click";
+export const EVENT_EXPLORE_MORE_SEARCH_CLICK = "search_explore_more";
+export const EVENT_GTM_SEARCH_LOGS_SCREEN_VIEW = "search_logs_screen_view";
+export const EVENT_GTM_SEARCH_SCREEN_VIEW = "search_screen_view";
+export const EVENT_GTM_SEARCH = "search";
+export const EVENT_CLICK_TOP_SEARCHES_CLICK = "top_searches_click";
+export const EVENT_CLICK_RECOMMENDATION_CLICK = "recommendation_clicked";
+export const EVENT_SEARCH_SUGGESTION_PRODUCT_CLICK = "search_product_suggestion_click";
+export const EVENT_PAGE_NOT_FOUND = "pageNotFound";
