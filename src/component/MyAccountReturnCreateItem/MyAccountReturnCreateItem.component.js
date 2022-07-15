7a714bdaa8345ecbd097af5b019fe949f72b7ea3
muskan-appinventiv <muskan.soni@appinventiv.com>
2022-07-15 15:02:07 +0530

    Added few files reveretd from exchnage

--- src/component/MyAccountReturnCreateItem/MyAccountReturnCreateItem.component.js
+++ src/component/MyAccountReturnCreateItem/MyAccountReturnCreateItem.component.js
@@ -386,13 +386,13 @@ export class MyAccountReturnCreateItem extends PureComponent {
           block="PDPAlsoAvailableProduct"
           elem={
             isNotAvailable ? "SizeOptionContainerOOS" :
-              selectedAvailProduct[item_id] &&
-                sku === selectedAvailProduct[item_id]["id"]
-                ? "selectedProduct"
-                : ""
+            selectedAvailProduct[item_id] &&
+              sku === selectedAvailProduct[item_id]["id"]
+              ? "selectedProduct"
+              : ""
           }
           id={sku}
-          onClick={isNotAvailable ? () => { } : (event) => onAvailableProductClick(event, item_id, sku)}
+          onClick={isNotAvailable ? ()=>{} :(event) => onAvailableProductClick(event, item_id, sku)}
         >
           <div
             block="PDPAlsoAvailableProduct-Link"
@@ -453,116 +453,6 @@ export class MyAccountReturnCreateItem extends PureComponent {
     }
     sendNotifyMeEmail(notifyMeEmail, item_id);
   };
-
-  onQuantityChange = (quantity, itemId) => {
-    const {
-      onQuantitySelection,
-    } = this.props;
-    onQuantitySelection(parseInt(quantity), itemId);
-  };
-
-  renderQuantitySelection = (maxSaleQuantity) => {
-    const {
-      minSaleQuantity = 1,
-      item: { item_id },
-      returnableQty = {},
-      isArabic
-    } = this.props;
-
-    const qtyList = Array.from(
-      { length: maxSaleQuantity - minSaleQuantity + 1 },
-      (v, k) => k + minSaleQuantity
-    );
-
-    return (
-      <div block="CartItem" elem="Quantity" mods={{ isArabic }}>
-        <select
-          value={returnableQty[item_id] ? returnableQty[item_id].quantity : maxSaleQuantity}
-          onChange={(e) => this.onQuantityChange(e.target.value, item_id)}
-        >
-          {qtyList.map((item, index) => {
-            return (
-              <option
-                key={index}
-                selected={returnableQty[item_id] ? returnableQty[item_id].quantity === item : maxSaleQuantity === item}
-                block="CartItem"
-                elem="QuantityOption"
-                value={item}
-              >
-                {item}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-    );
-  };
-
-  renderDetails() {
-    const {
-      displayDiscountPercentage,
-      item: {
-        name,
-        color,
-        row_total,
-        discount_percent,
-        discount_amount,
-        size: sizeField,
-        item_id,
-        qty_shipped,
-        product_options: { info_buyRequest: { qty } }
-      }
-    } = this.props;
-    const size = typeof sizeField === 'string' ? sizeField : (sizeField || {}).value;
-    return (
-      <div block="MyAccountReturnCreateItem" elem="Details">
-        <h2>{name}</h2>
-        <div block="MyAccountReturnCreateItem" elem="DetailsOptions">
-          {!!color && (
-            <p>
-              {__('Color: ')}
-              <span>{color}</span>
-            </p>
-          )}
-          {!!qty_shipped && (
-            <p block="Quantity">
-              {__('Qty: ')}
-              {this.renderQuantitySelection(+qty_shipped, item_id)}
-              {/* <span>{+qty_shipped}</span> */}
-            </p>
-          )}
-          {!!size && (
-            <p>
-              {__('Size: ')}
-              <span>{size}</span>
-            </p>
-          )}
-        </div>
-        <p block="MyAccountReturnCreateItem" elem="Price">
-          <span
-            block="MyAccountReturnCreateItem"
-            elem="PriceRegular"
-            mods={{ isDiscount: (!!(+discount_amount) && !!(+discount_percent)) }}
-          >
-            {`${formatPrice(+row_total)}`}
-          </span>
-          {(!!(+discount_amount) && !!(+discount_percent)) && (
-            <>
-              {
-                displayDiscountPercentage &&
-                <span block="MyAccountReturnCreateItem" elem="PriceDiscountPercent">
-                  {`(-${+discount_percent}%)`}
-                </span>
-              }
-              <span block="MyAccountReturnCreateItem" elem="PriceDiscount">
-                {`${formatPrice(+row_total - +discount_amount)}`}
-              </span>
-            </>
-          )}
-        </p>
-      </div>
-    );
-  }
 
   renderOutOfStockPopUp() {
     const {
