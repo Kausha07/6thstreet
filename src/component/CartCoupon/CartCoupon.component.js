7806e3a9315f74ec70f0a40d4500f83080076800
muskan-appinventiv <muskan.soni@appinventiv.com>
2022-07-15 11:29:36 +0530

    ...

--- src/component/CartCoupon/CartCoupon.component.js
+++ src/component/CartCoupon/CartCoupon.component.js
@@ -6,117 +6,116 @@ import { isArabic } from "Util/App";
 import "./CartCoupon.extended.style";
 
 export class CartCoupon extends SourceCartCoupon {
+  handleCouponCodeChange = (enteredCouponCode) => {
+    this.setState({
+      enteredCouponCode: this.removeCouponSpace(enteredCouponCode),
+    });
+  };
 
-    handleCouponCodeChange = (enteredCouponCode) => {
-        this.setState({
-            enteredCouponCode: this.removeCouponSpace(enteredCouponCode),
-        });
-    };
+  removeCouponSpace = (value) => {
+    // if (value.includes(" ")) {
+    return value.replace(/\s/g, "");
+    //} else {
+    //  return value;
+    //}
+  };
 
-    removeCouponSpace = (value) => {
-        // if (value.includes(" ")) {
-        return value.replace(/\s/g, "");
-        //} else {
-        //  return value;
-        //}
-    };
+  handleApplyCoupon = () => {
+    const { handleApplyCouponToCart } = this.props;
+    const { enteredCouponCode } = this.state;
+    const formattedCouponValue = this.removeCouponSpace(enteredCouponCode);
+    handleApplyCouponToCart(formattedCouponValue);
+  };
 
-    handleApplyCoupon = () => {
-        const { handleApplyCouponToCart } = this.props;
-        const { enteredCouponCode } = this.state;
-        const formattedCouponValue = this.removeCouponSpace(enteredCouponCode);
-        handleApplyCouponToCart(formattedCouponValue);
-    };
+  handleApplyCode = async (e, couponCode) => {
+    e.stopPropagation();
+    try {
+      let apiResponse =
+        (await this.props.applyCouponToCart(couponCode)) || null;
+      if (typeof apiResponse !== "string") {
+        this.props.closePopup();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    handleApplyCode = async (e, couponCode) => {
-        e.stopPropagation();
-        try {
-            let apiResponse =
-                (await this.props.applyCouponToCart(couponCode)) || null;
-            if (typeof apiResponse !== "string") {
-                this.props.closePopup();
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  handleFormSubmit = (e) => {
+    const { couponCode } = this.props;
+    e.preventDefault();
 
-    handleFormSubmit = (e) => {
-        const { couponCode } = this.props;
-        e.preventDefault();
-
-        if (couponCode) {
-            this.handleRemoveCoupon();
-            return;
-        }
-
-        this.handleApplyCoupon();
-    };
-
-    renderApplyCoupon() {
-        const { enteredCouponCode } = this.state;
-        const formattedCouponValue = this.removeCouponSpace(enteredCouponCode);
-        return (
-            <>
-                <Field
-                    type="text"
-                    id="couponCode"
-                    name="couponCode"
-                    value={formattedCouponValue}
-                    placeholder={__("Enter a Coupon or Discount Code")}
-                    onChange={this.handleCouponCodeChange}
-                    mix={{ block: "CartCoupon", elem: "Input" }}
-                />
-                <button
-                    block="CartCoupon"
-                    elem="Button"
-                    type="button"
-                    mix={{ block: "Button" }}
-                    disabled={!formattedCouponValue}
-                    onClick={(e) => {
-                        this.handleApplyCode(e, formattedCouponValue);
-                    }}
-                >
-                    {__("Add")}
-                </button>
-            </>
-        );
+    if (couponCode) {
+      this.handleRemoveCoupon();
+      return;
     }
 
-    renderRemoveCoupon() {
-        const { couponCode } = this.props;
+    this.handleApplyCoupon();
+  };
 
-        return (
-            <>
-                <p block="CartCoupon" elem="Message">
-                    <strong>{couponCode.toUpperCase()}</strong>
-                </p>
-                <button
-                    block="CartCoupon"
-                    elem="Button"
-                    type="button"
-                    mix={{ block: "Button" }}
-                    onClick={this.handleRemoveCoupon}
-                >
-                    {__("Remove")}
-                </button>
-            </>
-        );
-    }
+  renderApplyCoupon() {
+    const { enteredCouponCode } = this.state;
+    const formattedCouponValue = this.removeCouponSpace(enteredCouponCode);
+    return (
+      <>
+        <Field
+          type="text"
+          id="couponCode"
+          name="couponCode"
+          value={formattedCouponValue}
+          placeholder={__("Enter a Coupon or Discount Code")}
+          onChange={this.handleCouponCodeChange}
+          mix={{ block: "CartCoupon", elem: "Input" }}
+        />
+        <button
+          block="CartCoupon"
+          elem="Button"
+          type="button"
+          mix={{ block: "Button" }}
+          disabled={!formattedCouponValue}
+          onClick={(e) => {
+            this.handleApplyCode(e, formattedCouponValue);
+          }}
+        >
+          {__("Add")}
+        </button>
+      </>
+    );
+  }
 
-    render() {
-        const { isLoading, couponCode } = this.props;
-        return (
-            <form
-                block="CartCoupon"
-                mods={{ active: !!couponCode, isArabic: isArabic() }}
-                onSubmit={this.handleFormSubmit}
-            >
-                <Loader isLoading={isLoading} />
-                {couponCode ? this.renderRemoveCoupon() : this.renderApplyCoupon()}
-            </form>
-        );
-    }
+  renderRemoveCoupon() {
+    const { couponCode } = this.props;
+
+    return (
+      <>
+        <p block="CartCoupon" elem="Message">
+          <strong>{couponCode.toUpperCase()}</strong>
+        </p>
+        <button
+          block="CartCoupon"
+          elem="Button"
+          type="button"
+          mix={{ block: "Button" }}
+          onClick={this.handleRemoveCoupon}
+        >
+          {__("Remove")}
+        </button>
+      </>
+    );
+  }
+
+  render() {
+    const { isLoading, couponCode } = this.props;
+    return (
+      <form
+        block="CartCoupon"
+        mods={{ active: !!couponCode, isArabic: isArabic() }}
+        onSubmit={this.handleFormSubmit}
+      >
+        <Loader isLoading={isLoading} />
+        {couponCode ? this.renderRemoveCoupon() : this.renderApplyCoupon()}
+      </form>
+    );
+  }
 }
 
 export default CartCoupon;
