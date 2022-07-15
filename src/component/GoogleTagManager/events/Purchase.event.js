76215b06f58c3459d2e765ae8b8cf3f36a72e6c5
muskan-appinventiv <muskan.soni@appinventiv.com>
2022-07-15 10:12:42 +0530

    ...

--- src/component/GoogleTagManager/events/Purchase.event.js
+++ src/component/GoogleTagManager/events/Purchase.event.js
@@ -12,90 +12,90 @@ export const SPAM_PROTECTION_DELAY = 10000;
  * On order success page "Purchase"
  */
 class PurchaseEvent extends BaseEvent {
-    /**
-     * Event delay
-     *
-     * @type {number}
-     */
-    eventHandleDelay = PURCHASE_EVENT_HANDLE_DELAY;
+  /**
+   * Event delay
+   *
+   * @type {number}
+   */
+  eventHandleDelay = PURCHASE_EVENT_HANDLE_DELAY;
 
-    /**
-     * Bind on product detail
-     */
-    bindEvent() {
-        Event.observer(EVENT_GTM_PURCHASE, ({ orderID: orderId, totals }) => {
-            this.handle(orderId, totals);
-        });
+  /**
+   * Bind on product detail
+   */
+  bindEvent() {
+    Event.observer(EVENT_GTM_PURCHASE, ({ orderID: orderId, totals }) => {
+      this.handle(orderId, totals);
+    });
+  }
+
+  /**
+   * Handle
+   *
+   * @param orderId
+   * @param totals
+   * @param cartData
+   */
+  handler(orderId, totals) {
+    if (this.spamProtection(SPAM_PROTECTION_DELAY)) {
+      return;
     }
 
-    /**
-     * Handle
-     *
-     * @param orderId
-     * @param totals
-     * @param cartData
-     */
-    handler(orderId, totals) {
-        if (this.spamProtection(SPAM_PROTECTION_DELAY)) {
-            return;
-        }
+    this.pushEventData({
+      ecommerce: {
+        currencyCode: this.getCurrencyCode(),
+        purchase: {
+          actionField: this.getActionFields(orderId, totals),
+          products: this.getProducts(totals),
+        },
+      },
+    });
+  }
 
-        this.pushEventData({
-            ecommerce: {
-                currencyCode: this.getCurrencyCode(),
-                purchase: {
-                    actionField: this.getActionFields(orderId, totals),
-                    products: this.getProducts(totals),
-                },
-            },
-        });
-    }
+  /**
+   * Get order information
+   *
+   * @return {{revenue: number, coupon_discount_abs: string, coupon: string, shipping: number, affiliation: string, coupon_discount_amount: string, tax: number, id: *}}
+   */
+  getActionFields(
+    orderId = "",
+    { tax_amount, total, shipping_fee, coupon_code = "" }
+  ) {
+    return {
+      id: orderId,
+      affiliation: "Online Store",
+      revenue: +roundPrice(total),
+      tax: +roundPrice(tax_amount),
+      shipping: +roundPrice(shipping_fee),
+      coupon: coupon_code,
+    };
+  }
 
-    /**
-     * Get order information
-     *
-     * @return {{revenue: number, coupon_discount_abs: string, coupon: string, shipping: number, affiliation: string, coupon_discount_amount: string, tax: number, id: *}}
-     */
-    getActionFields(
-        orderId = "",
-        { tax_amount, total, shipping_fee, coupon_code = "" }
-    ) {
-        return {
-            id: orderId,
-            affiliation: "Online Store",
-            revenue: +roundPrice(total),
-            tax: +roundPrice(tax_amount),
-            shipping: +roundPrice(shipping_fee),
-            coupon: coupon_code,
-        };
-    }
+  /**
+   * Get product detail
+   *
+   * @param totals
+   *
+   * @return {{quantity: number, price: number, name: string, variant: string, id: string, category: string, brand: string, url: string}[]}
+   * @param cartData
+   */
+  getProducts({ items = [] }) {
+    const products = items.reduce(
+      (acc, item) => [
+        ...acc,
+        {
+          ...ProductHelper.getItemData(item),
+          quantity: ProductHelper.getQuantity(item),
+        },
+      ],
+      []
+    );
 
-    /**
-     * Get product detail
-     *
-     * @param totals
-     *
-     * @return {{quantity: number, price: number, name: string, variant: string, id: string, category: string, brand: string, url: string}[]}
-     * @param cartData
-     */
-    getProducts({ items = [] }) {
-        const products = items.reduce(
-            (acc, item) => [
-                ...acc,
-                {
-                    ...ProductHelper.getItemData(item),
-                    quantity: ProductHelper.getQuantity(item),
-                },
-            ],
-            []
-        );
+    const groupedProducts = this.getGroupedProducts();
+    Object.values(groupedProducts || {}).forEach(({ data }) =>
+      products.push(data)
+    );
 
-        const groupedProducts = this.getGroupedProducts();
-        Object.values(groupedProducts || {}).forEach(({ data }) =>
-            products.push(data)
-        );
-
-        return products;
-    }
+    return products;
+  }
 }
 export default PurchaseEvent;
