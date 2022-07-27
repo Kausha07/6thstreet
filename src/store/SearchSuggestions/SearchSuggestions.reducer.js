04d0c505dc7df3ac7c8dd7fcfa52b4eee1c0845d
Bhuvan Bakshi <bhuvanb@webmob.tech>
2022-07-27 13:40:26 +0530

    improve reducer

--- src/store/SearchSuggestions/SearchSuggestions.reducer.js
+++ src/store/SearchSuggestions/SearchSuggestions.reducer.js
@@ -1,32 +1,40 @@
-import { SET_SEARCH_SUGGESTIONS,SET_ALGOLIA_INDEX } from "./SearchSuggestions.action";
+import {
+  SET_SEARCH_SUGGESTIONS,
+  SET_ALGOLIA_INDEX,
+} from "./SearchSuggestions.action";
 
 export const getInitialState = () => ({
   search: "",
   data: {},
-  queryID:"",
-  querySuggestions:[],
+  queryID: "",
+  querySuggestions: [],
   algoliaIndex: null,
-
+  gender: "",
+  country: "",
 });
 
 export const SearchSuggestionsReducer = (state = getInitialState(), action) => {
-  const { type,algoliaIndex } = action;
+  const { type } = action;
 
   switch (type) {
     case SET_SEARCH_SUGGESTIONS:
-      const { search, data, queryID, querySuggestions } = action;
+      const { search, data, queryID, querySuggestions, gender, country } =
+        action;
       return {
         ...state,
         search,
         data,
         queryID,
         querySuggestions,
+        gender,
+        country,
       };
-      case SET_ALGOLIA_INDEX:
-        return {
-          ...state,
-          algoliaIndex,
-        };
+    case SET_ALGOLIA_INDEX:
+      const { algoliaIndex } = action;
+      return {
+        ...state,
+        algoliaIndex,
+      };
 
     default:
       return state;
