f059d18ad2f3fcf17f350f943f9c2ca2aa67ae40
Bhuvan Bakshi <bhuvanb@webmob.tech>
2022-08-04 15:56:17 +0530

    enable suggestion Enabled feature

--- src/component/SearchSuggestion/SearchSuggestion.component.js
+++ src/component/SearchSuggestion/SearchSuggestion.component.js
@@ -599,8 +599,7 @@ class SearchSuggestion extends PureComponent {
     let isRecommended = products.length === 0 && querySuggestions.length === 1;
     return (
       <>
-        {/* {suggestionEnabled && this.renderQuerySuggestions()} */}
-        {this.renderQuerySuggestions()}
+        {suggestionEnabled && this.renderQuerySuggestions()}
         {/* {this.renderBrands()} */}
         {/* {this.renderWishlistProducts()} */}
         {this.renderProducts()}
