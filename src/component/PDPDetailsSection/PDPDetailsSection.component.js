4b852576fab4640932cebe5c3668d3479b2be8a5
muskan-appinventiv <muskan.soni@appinventiv.com>
2022-09-27 16:25:10 +0530

    added removed code

--- src/component/PDPDetailsSection/PDPDetailsSection.component.js
+++ src/component/PDPDetailsSection/PDPDetailsSection.component.js
@@ -660,6 +660,7 @@ class PDPDetailsSection extends PureComponent {
       product
     } = this.props;
     const { innerWidth: width } = window;
+    document.body.style.overflowX = 'hidden';
     if (pdpWidgetsData?.length > 0 && pdpWidgetsAPIData?.length > 0) {
       return (
         <>
