b2ea2aa998f43abc9bcd284bbd2b57a28465475a
imbarathk <barath.kumar@6thstreet.com>
2022-10-10 19:45:57 +0530

    Clearing logs

--- src/component/GoogleTagManager/events/CheckoutBilling.event.js
+++ src/component/GoogleTagManager/events/CheckoutBilling.event.js
@@ -1,5 +1,6 @@
 import Event, { EVENT_GTM_CHECKOUT_BILLING } from "Util/Event";
 import BaseEvent from "./Base.event";
+import BrowserDatabase from "Util/BrowserDatabase";
 
 /**
  * Website places, from where was received event data
@@ -32,19 +33,31 @@ class CheckoutBillingEvent extends BaseEvent {
    */
   bindEvent() {
     Event.observer(EVENT_GTM_CHECKOUT_BILLING, () => {
-     this.handle(EVENT_GTM_CHECKOUT_BILLING);
+      this.handle(EVENT_GTM_CHECKOUT_BILLING);
     });
   }
 
   handler(data) {
+    const sha_email =
+      BrowserDatabase.getItem("TT_Data") &&
+      BrowserDatabase.getItem("TT_Data")?.mail
+        ? BrowserDatabase.getItem("TT_Data").mail
+        : null;
+    const sha_phone =
+      BrowserDatabase.getItem("TT_Data") &&
+      BrowserDatabase.getItem("TT_Data")?.phone
+        ? BrowserDatabase.getItem("TT_Data").phone
+        : null;
     if (this.spamProtection(SPAM_PROTECTION_DELAY)) {
       return;
-  }
+    }
     this.pushEventData({
       event: data,
+      sha256_email: sha_email,
+      sha256_phone_number: sha_phone,
       ecommerce: {
         currencyCode: this.getCurrencyCode(),
-    }
+      },
     });
   }
 }
