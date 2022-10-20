86691e55f947bbaac2a3cdc40c023887df7886ad
muskan-appinventiv <muskan.soni@appinventiv.com>
2022-10-20 11:41:02 +0530

    UI part for sort overlay

--- src/route/PLP/PLP.component.js
+++ src/route/PLP/PLP.component.js
@@ -7,10 +7,11 @@ import PLPFilters from "Component/PLPFilters";
 import PLPPages from "Component/PLPPages";
 import isMobile from "Util/Mobile";
 import { isArabic } from "Util/App";
-import { PureComponent } from "react";
+import { PureComponent, createRef } from "react";
 import CircleItemSliderSubPage from "../../component/DynamicContentCircleItemSlider/CircleItemSliderSubPage";
 // import DynamicContentCircleItemSlider from '../../component/DynamicContentCircleItemSlider';
 import "./PLP.style";
+import "../../component/CartCouponDetail/CartCouponDetail.style";
 import { connect } from "react-redux";
 import NoMatch from "Route/NoMatch";
 import Loader from "Component/Loader";
@@ -33,7 +34,9 @@ export class PLP extends PureComponent {
       circleBannerUrl: null,
       activeFilters: {},
       isArabic: isArabic(),
+      isSortByOverlayOpen: false,
     };
+    this.sortByOverlay = createRef();
   }
 
   componentDidMount() {
@@ -43,7 +46,7 @@ export class PLP extends PureComponent {
       let banner = JSON.parse(bannerData);
       this.setState({
         bannerData: banner,
-        circleBannerUrl: bannerUrl
+        circleBannerUrl: bannerUrl,
       });
     }
   }
@@ -86,7 +89,6 @@ export class PLP extends PureComponent {
   }
 
   renderPLPFilters() {
-
     return <PLPFilters {...this.props} isPLPSortBy={false} />;
   }
 
@@ -107,7 +109,7 @@ export class PLP extends PureComponent {
   }
 
   renderBanner() {
-    let isFromCircleItemSlider = window.location.href.includes('plp_config');
+    let isFromCircleItemSlider = window.location.href.includes("plp_config");
 
     if (this.state.bannerData && isFromCircleItemSlider)
       return (
@@ -142,65 +144,128 @@ export class PLP extends PureComponent {
     );
   };
 
-  renderSortByOverlay = () => {
+  hideSortByOverlay = (e) => {
+    e.stopPropagation();
+    this.setState({
+      isSortByOverlayOpen: false,
+    });
+    if (!this.state.isSortByOverlayOpen) {
+      const bodyElt = document.querySelector("body");
+      bodyElt.removeAttribute("style");
+    }
+  };
+
+  closeSortByOverlay = () => {
+    this.setState({
+      isSortByOverlayOpen: false,
+    });
+    const bodyElt = document.querySelector("body");
+    bodyElt.removeAttribute("style");
+  };
+
+  showCouponDetial = (e) => {
+    e.stopPropagation();
+    this.setState({
+      isSortByOverlayOpen: true,
+    });
+
+    const bodyElt = document.querySelector("body");
+    bodyElt.style.overflow = "hidden";
+  };
+
+  renderSortFilterOverlay = () => {
     return (
       <div block="SortOverlay">
-        <div block="CommonBlock">
+        <div block="CommonBlock" onClick={(e) => this.showCouponDetial(e)}>
           <img src={sort} alt="sort" />
           <span block="title">{__("Sort")}</span>
         </div>
         <div block="SortOverlay" elem="CenterLine">
           <img src={Line} alt="line" />
         </div>
+        <div block="HighlightOval"></div>
         <div block="CommonBlock">
           <img src={refine} alt="refine" block="CommonBlock" elem="RefineImg" />
           <span block="title">{__("Refine")}</span>
         </div>
       </div>
     );
-  }
+  };
+
+  renderSortByOverlay = () => {
+    const {filters} = this.props
+    console.log("muskan props", this.props.filters,this.props.filters['sort'].data);
+    return (
+      <div block="couponDetailPopup">
+        <div block="couponDetailOverlay">
+          <div block="couponDetialPopupBlock" ref={this.sortByOverlay}>
+            <p block="couponItemCode">
+              {__("SORT BY")}
+              <button
+                onClick={(e) => {
+                  this.hideSortByOverlay(e);
+                }}
+                block="closePopupbtn"
+              >
+                <span>Close</span>
+              </button>
+            </p>
+            {filters && Object.values(filters['sort'].data).map((filter,index)=>{
+                return (
+                <>
+                  <p block="couponItemName">{filter.label}</p>
+                  {index <= Object.values(filters['sort'].data).length-1 && <hr/>}
+                </>
+                )
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   render() {
-    const { signInPopUp } = this.state;
-    const { isArabic } = this.state;
-    const {pages, isLoading} = this.props;
-    if(!isLoading && (!pages["0"] || pages["0"].length === 0 || pages.undefined)){
-      return (
-        <NoMatch/>
-      )
+    const { isArabic, isSortByOverlayOpen } = this.state;
+    const { pages, isLoading } = this.props;
+    if (
+      !isLoading &&
+      (!pages["0"] || pages["0"].length === 0 || pages.undefined)
+    ) {
+      return <NoMatch />;
     }
-    if (      
+    if (
       (pages.undefined && pages.undefined.length > 0) ||
       (pages["0"] && pages["0"].length > 0)
     ) {
-      
-    return (
-      <main block="PLP" id="plp-main-scroll-id">
-        <ContentWrapper label={__("Product List Page")}>
-          {this.renderMySignInPopup()}
-          {this.renderPLPDetails()}
-          {this.state.bannerData && this.renderBanner()}
-          {this.renderPLPWidget()}
-          {isMobile.any() && this.renderSortByOverlay()}
-          <div>
-
-
-            <div block="Products" elem="Wrapper">
-              {this.renderPLPFilters()}
-              {this.renderPLPPages()}
-
-            </div>
-            {!isMobile.any() && (
-              <div block="SortBy" mods={{ isArabic }}>
-                {this.renderPLPSortBy()}
+      return (
+        <main block="PLP" id="plp-main-scroll-id">
+          <ContentWrapper label={__("Product List Page")}>
+            {this.renderMySignInPopup()}
+            {this.renderPLPDetails()}
+            {this.state.bannerData && this.renderBanner()}
+            {this.renderPLPWidget()}
+            {isMobile.any() && this.renderSortFilterOverlay()}
+            {isMobile.any() &&
+              isSortByOverlayOpen &&
+              this.renderSortByOverlay()}
+            <div>
+              <div block="Products" elem="Wrapper">
+                {this.renderPLPFilters()}
+                {this.renderPLPPages()}
               </div>
-            )}
-          </div>
-        </ContentWrapper>
-      </main>
-    );}
-
-      return  <Loader isLoading={isLoading} />
+              {!isMobile.any() && (
+                <div block="SortBy" mods={{ isArabic }}>
+                  {this.renderPLPSortBy()}
+                </div>
+              )}
+            </div>
+          </ContentWrapper>
+        </main>
+      );
     }
+
+    return <Loader isLoading={isLoading} />;
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PLP);
