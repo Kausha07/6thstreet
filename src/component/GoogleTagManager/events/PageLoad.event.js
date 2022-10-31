7a8b2b6819ab9006203633cf1736b745315655dc
imbarathk <barath.kumar@6thstreet.com>
2022-10-31 19:40:51 +0530

    Page Load event

--- src/component/GoogleTagManager/events/PageLoad.event.js
+++ src/component/GoogleTagManager/events/PageLoad.event.js
@@ -39,14 +39,46 @@ class PageLoadEvent extends BaseEvent {
 
   handler() {
     this.pushEventData({
+      eventCategory: "Home",
+      eventAction: "page_load",
       UserType:
         this.getCustomerId().toString().length > 0 ? "Logged In" : "Logged Out",
+      ClientID: this.getGAID(),
     });
   }
   getCustomerId() {
     return this.isSignedIn()
       ? this.getAppState().MyAccountReducer.customer.id || ""
       : "";
+  }
+  getGAID() {
+    const cookieValue = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("_ga"))
+      ?.split("=")[1];
+    if (cookieValue) {
+      const splitGAID = () => {
+        if (cookieValue.includes(".")) {
+          let cookieSplit = cookieValue.split(".");
+          if (cookieSplit.length == 4) {
+            return cookieSplit[2] + "." + cookieSplit[3];
+          }
+        } else if (cookieValue.includes("GA1.1.")) {
+          return cookieValue.split("GA1.1.")[1];
+        } else if (cookieValue.includes("GA1.2.")) {
+          return cookieValue.split("GA1.2.")[1];
+        } else if (cookieValue.includes("GA1.3.")) {
+          return cookieValue.split("GA1.3.")[1];
+        } else if (cookieValue.includes("GA1.3.")) {
+          return cookieValue.split("GA1.4.")[1];
+        } else {
+          return cookieValue;
+        }
+      };
+      return splitGAID();
+    } else {
+      return "";
+    }
   }
 }
 
