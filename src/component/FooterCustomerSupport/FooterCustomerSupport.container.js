2e9327ad55107dc91c31ab8278f9b61fee942bd0
Sarthak <sarthak@6thstreet.com>
2022-11-04 13:38:24 +0530

    fix: apply checks before accessing customer support variables and make offline notification visible if user is not connected to the internet

--- src/component/FooterCustomerSupport/FooterCustomerSupport.container.js
+++ src/component/FooterCustomerSupport/FooterCustomerSupport.container.js
@@ -39,7 +39,7 @@ export class FooterCustomerSupportContainer extends PureComponent {
             contactLabel,
             isContactEmail,
             openHoursLabel,
-            phone: phone.indexOf('00') === 0 ? phone.replace('00', '+') : phone
+            phone: phone && phone.includes("00") && phone.indexOf('00') === 0 ? phone.replace('00', '+') : phone
         };
     };
 
@@ -50,36 +50,42 @@ export class FooterCustomerSupportContainer extends PureComponent {
             language
         } = this.props;
 
-        const {
-            contact_using: {
-                text: {
-                    [language]: contactLabel
-                } = {},
-                type: contactType
-            } = {},
-            opening_hours: {
-                [language]: openHoursLabel
-            },
-            toll_free: phone
-        } = countries[country];
+        let isEmailSupported = false;
+        let isPhoneSupported = false;
+        let contactLabel = {};
+        let openHoursLabel = "";
+        let phone = "";
 
-        let isEmailSupported;
-        let isPhoneSupported;
-
-        if (
-            countries[country] &&
-            countries[country].contact_information &&
-            countries[country].contact_information.email &&
-            countries[country].contact_information.phone) {
-            isEmailSupported = countries[country].contact_information.email;
-            isPhoneSupported = countries[country].contact_information.phone;
+        if (countries[country]) {
+            if (countries[country].contact_information) {
+                if (countries[country].contact_information.email &&
+                    countries[country].contact_information.phone) {
+                    isEmailSupported = countries[country].contact_information.email;
+                    isPhoneSupported = countries[country].contact_information.phone;
+                }
+            }
+            if (countries[country].contact_using &&
+                countries[country].contact_using.options &&
+                countries[country].contact_using.options.text
+            ) {
+                contactLabel = countries[country].contact_using.options.text;
+            }
+            if (countries[country].opening_hours) {
+                if (language === 'en') {
+                    openHoursLabel = countries[country].opening_hours.en;
+                } else if (language === 'ar') {
+                    openHoursLabel = countries[country].opening_hours.ar;
+                }
+            }
+            if (countries[country].toll_free) {
+                phone = countries[country].toll_free;
+            }
         }
 
         return {
             isEmailSupported,
             isPhoneSupported,
             contactLabel,
-            isContactEmail: contactType === 'email',
             openHoursLabel,
             phone
         };
