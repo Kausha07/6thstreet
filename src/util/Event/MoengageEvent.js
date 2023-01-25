4174ebacb4410ec8eb0a2dc6d18de5ac0760373c
imbarathk <barath.kumar@6thstreet.com>
2023-01-25 21:48:47 +0530

    fix: OOPS error

--- src/util/Event/MoengageEvent.js
+++ src/util/Event/MoengageEvent.js
@@ -1,42 +1,42 @@
 import MoEngage from "react-moengage";
 
 export const MOE_trackEvent = (name, { options }) => {
-  if (MoEngage.moe) {
+  if (MoEngage.moe && window.Moengage) {
     Moengage.track_event(name, { options });
   }
 };
 export const MOE_AddFirstName = (firstName) => {
-  if (MoEngage.moe) {
+  if (MoEngage.moe && window.Moengage) {
     Moengage.add_first_name(firstName);
   }
 };
 
 export const MOE_addLastName = (lastName) => {
-  if (MoEngage.moe) {
+  if (MoEngage.moe && window.Moengage) {
     Moengage.add_last_name(lastName);
   }
 };
 
 export const MOE_addEmail = (email) => {
-  if (MoEngage.moe) {
+  if (MoEngage.moe && window.Moengage) {
     Moengage.add_email(email);
   }
 };
 
 export const MOE_addMobile = (phone) => {
-  if (MoEngage.moe) {
+  if (MoEngage.moe && window.Moengage) {
     Moengage.add_mobile(phone);
   }
 };
 
 export const MOE_AddUniqueID = (id) => {
-  if (MoEngage.moe) {
+  if (MoEngage.moe && window.Moengage) {
     Moengage.add_unique_user_id(id);
   }
 };
 
 export const MOE_destroySession = () => {
-  if (MoEngage.moe) {
+  if (MoEngage.moe && window.Moengage) {
     Moengage.destroy_session();
   }
 };
