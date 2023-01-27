b704e01723e4c5a767fe00c215a04dede23f1d76
muskan-appinventiv <muskan.soni@appinventiv.com>
2023-01-27 12:34:09 +0530

    fix : Index name issue on shop by brand page

--- packages/algolia-sdk/app/get-brands.js
+++ packages/algolia-sdk/app/get-brands.js
@@ -1,14 +1,11 @@
 import { searchParams } from "./config";
-import { getIndex } from "./utils";
 export default function getBrands(gender = "", options = {}) {
   const { index, client, env } = options;
-  let indexName = getIndex("en-ae", env);
-
   return new Promise((resolve, reject) => {
     const newSearchParams = Object.assign({}, searchParams);
     const queries = [];
     queries.push({
-      indexName:indexName,
+      indexName:index.indexName,
       params: {
         ...newSearchParams,
         facetFilters: [[`gender: ${gender}`],[`in_stock:1`]],
