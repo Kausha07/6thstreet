3a31f02f39ca1275f070ece3b841b2b2e4aecc1c
Avinash Soni <asoni14593@gmail.com>
2023-02-08 19:01:46 +0530

    fix: [SEAR-142] encode brand name in catalogue brand api call

--- src/util/API/provider/CatalogueAPI.js
+++ src/util/API/provider/CatalogueAPI.js
@@ -4,7 +4,7 @@ class CatalogueAPI {
     get(brand_name) {
         const CATALOG_BASE_URL = process.env.REACT_APP_CATALOGUE_URL;
         return doFetch(`${CATALOG_BASE_URL}catalogue/brands`
-            + `${brand_name ? '?name=' + brand_name : ''}`);
+            + `${brand_name ? '?name=' + encodeURIComponent(brand_name) : ''}`);
     }
 }
 
