dffb872fef78ba37e9e632e2fed303533f8d198c
Devendra Patidar <devendrapatidar@BGR-58120.local>
2023-02-13 12:21:17 +0530

    perf: remove unused import in file

--- src/component/Header/Header.container.js
+++ src/component/Header/Header.container.js
@@ -14,7 +14,7 @@ import {
   toggleOverlayByKey,
 } from "Store/Overlay/Overlay.action";
 import { appendWithStoreCode } from "Util/Url";
-import PDPDispatcher from "Store/PDP/PDP.dispatcher";
+// import PDPDispatcher from "Store/PDP/PDP.dispatcher";
 
 import Header from "./Header.component";
 
