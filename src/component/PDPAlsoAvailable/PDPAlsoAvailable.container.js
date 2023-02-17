4ecf6d601d9ce83929a5f1d077137160a1c52301
Avinash Soni <asoni14593@gmail.com>
2023-02-17 12:38:01 +0530

    fix: [SEAR-179] also available products not visible on pdp

--- src/component/PDPAlsoAvailable/PDPAlsoAvailable.container.js
+++ src/component/PDPAlsoAvailable/PDPAlsoAvailable.container.js
@@ -1,9 +1,15 @@
 import PropTypes from "prop-types";
 import { PureComponent } from "react";
+import { connect } from 'react-redux';
 
 import Algolia from "Util/API/provider/Algolia";
-
 import PDPAlsoAvailable from "./PDPAlsoAvailable.component";
+
+export const mapStateToProps = (state) => ({
+  isFetchFromAlgolia:
+    state.AppConfig.config.countries[state.AppState.country]['catalogue_from_algolia'],
+  relatedProducts: state.PDP.product.relatedProducts
+});
 
 export class PDPAlsoAvailableContainer extends PureComponent {
   static propTypes = {
@@ -14,38 +20,36 @@ export class PDPAlsoAvailableContainer extends PureComponent {
   state = {
     products: [],
     isAlsoAvailable: true,
-    firstLoad: true,
   };
 
   componentDidMount() {
-    const { firstLoad, products = [] } = this.state;
-
-    if (firstLoad && !products.length) {
+    const { products = [] } = this.state;
+    const { isFetchFromAlgolia,relatedProducts } = this.props;
+    if (isFetchFromAlgolia && !products.length) {
       this.getAvailableProducts();
+    }else if(!isFetchFromAlgolia){
+      this.setState({ products: relatedProducts });
+      this.setState({ isAlsoAvailable: relatedProducts.length === 0 });
     }
+  }
+
+  async getAvailableProduct(sku) {
+    const product = await new Algolia().getProductBySku({ sku })
+    return product;
   }
 
   getAvailableProducts() {
     const { productsAvailable = [] } = this.props;
-
     productsAvailable.map((productID) =>
       this.getAvailableProduct(productID).then((productData) => {
         let { products = [] } = this.state;
-
         if (productData.nbHits === 1) {
           this.setState({ products: [...products, productData.data] });
           products = this.state?.products || [];
         }
-
         this.setState({ isAlsoAvailable: products.length === 0 });
       })
     );
-  }
-
-  async getAvailableProduct(sku) {
-    const product = await new Algolia().getProductBySku({ sku });
-
-    return product;
   }
 
   render() {
@@ -58,4 +62,4 @@ export class PDPAlsoAvailableContainer extends PureComponent {
   }
 }
 
-export default PDPAlsoAvailableContainer;
+export default connect(mapStateToProps, null)(PDPAlsoAvailableContainer);
