b72273778c809319fad2218410e7322405bb0d14
Ronak Kanthalia <57457@appareluae.com>
2023-03-30 14:21:44 +0530

    [POE-810]: redirected url with 301 status

---
+++ redirectPath.js
@@ -0,0 +1,8 @@
+const redirectPath = {
+    "/naturaliser.html": "/naturalizer.html",
+    "/levis/women-28-1-m.html": "/levis.html",
+    "/anta/3xl-1-xs-1.html": "/anta.html",
+    "/women/bags/totes/mewien-tote-bag-pink.html": "/women/bags/hand-bags/tote-bags.html",
+    "/ruled-notebook-lcw-s1i471z8-qbc.html": "/kids/kids-accessories/school-supplies/books.html"
+}
+module.exports = redirectPath;
