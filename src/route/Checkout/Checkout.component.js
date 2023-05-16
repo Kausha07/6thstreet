3f14c4bfc0c4ba5b6e277d7359517203ef5bdf94
Ronak Kanthalia <57457@appareluae.com>
2023-05-16 13:43:59 +0530

    [POE-83]: default shipping address cross country fixed

--- src/route/Checkout/Checkout.component.js
+++ src/route/Checkout/Checkout.component.js
@@ -140,22 +140,24 @@ export class Checkout extends SourceCheckout {
       );
 
       const { city, area, country_code } = defaultAddress ? defaultAddress : addresses[0];
-      const { finalCity, finalArea } = this.getArabicCityArea(city, area);
-      let request = {
-        country: country_code,
-        city: isArabic() ? finalCity : city,
-        area: isArabic() ? finalArea : area,
-        courier: null,
-        source: null,
-      };
-      if(edd_info?.has_item_level) {
-        let items_in_cart = BrowserDatabase.getItem(CART_ITEMS_CACHE_KEY) || [];
-        request.intl_vendors=null;
-        let items = [];
-        items_in_cart.map(item => items.push({ sku : item.sku, intl_vendor : item?.full_item_info?.cross_border ? item?.full_item_info?.international_vendor : null}))
-        request.items = items;
+      if(country_code == getCountryFromUrl()) {
+        const { finalCity, finalArea } = this.getArabicCityArea(city, area);
+        let request = {
+          country: country_code,
+          city: isArabic() ? finalCity : city,
+          area: isArabic() ? finalArea : area,
+          courier: null,
+          source: null,
+        };
+        if(edd_info?.has_item_level) {
+          let items_in_cart = BrowserDatabase.getItem(CART_ITEMS_CACHE_KEY) || [];
+          request.intl_vendors=null;
+          let items = [];
+          items_in_cart.map(item => items.push({ sku : item.sku, intl_vendor : item?.full_item_info?.cross_border ? item?.full_item_info?.international_vendor : null}))
+          request.items = items;
+        }
+        estimateEddResponse(request, false);
       }
-      estimateEddResponse(request, false);
     }
   }
 
