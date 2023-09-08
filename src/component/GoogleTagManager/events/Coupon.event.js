ab3b5385c5fe7e482b8dd1b05c4adef5751d67db
imbarathk <barath.kumar@6thstreet.com>
2023-09-08 13:53:47 +0530

    modify: Coupon event details

--- src/component/GoogleTagManager/events/Coupon.event.js
+++ src/component/GoogleTagManager/events/Coupon.event.js
@@ -41,44 +41,50 @@ class CouponEvent extends BaseEvent {
 
   handler(event) {
     const cartItem = BrowserDatabase.getItem(CART_ITEMS_CACHE_KEY) || [];
-    let productName = [],
-      productID = [];
+
+    let formattedImpressions = {};
     if (cartItem.length > 0) {
-      cartItem.forEach((item) => {
-        let productKeys = item?.full_item_info;
-        productName.push(productKeys?.name);
-        productID.push(productKeys?.config_sku);
-      });
+      formattedImpressions = cartItem.map(
+        ({
+          full_item_info: {
+            name,
+            config_sku,
+            itemPrice,
+            color,
+            qty,
+            brand_name,
+          },
+        }) => ({
+          name: name || "",
+          id: config_sku || "",
+          qty: qty || "",
+          brand: brand_name || "",
+          price: itemPrice || "",
+          color: color || "",
+        })
+      );
     }
-    if(event.name){
-    this.pushEventData({
-      event: event.name,
-      eventCategory: "checkout_tracking",
-      eventAction: event.name,
-      couponCode: event.coupon,
-      productName:
-        productName.length == 0
-          ? ""
-          : productName.length == 1
-          ? productName.toString()
-          : productName,
-      productId:
-        productID.length == 0
-          ? ""
-          : productID.length == 1
-          ? productID.toString()
-          : productID,
-      UserType:
-        this.getCustomerId().toString().length > 0 ? "Logged In" : "Logged Out",
-      CustomerID: this.getCustomerId(),
-      PageType: this.getPageType(),
-      discount: event?.discount,
-      tax: event?.tax,
-      sub_total: event?.sub_total,
-      subtotal_incl_tax: event?.subtotal_incl_tax,
-      shipping: event?.shipping,
-      total: event?.total,
-    });
+
+    if (event.name) {
+      this.pushEventData({
+        event: event.name,
+        eventCategory: "checkout_tracking",
+        eventAction: event.name,
+        couponCode: event.coupon,
+        products: formattedImpressions,
+        UserType:
+          this.getCustomerId().toString().length > 0
+            ? "Logged In"
+            : "Logged Out",
+        CustomerID: this.getCustomerId(),
+        PageType: this.getPageType(),
+        discount: event?.discount,
+        tax: event?.tax,
+        sub_total: event?.sub_total,
+        subtotal_incl_tax: event?.subtotal_incl_tax,
+        shipping: event?.shipping,
+        total: event?.total,
+      });
     }
   }
 
