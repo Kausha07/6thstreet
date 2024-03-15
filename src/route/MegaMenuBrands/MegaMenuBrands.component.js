5f1c7c4843e264d3a693ba0ef3e65c3e7d80d832
kirankumar-medikurthy <kiran1999m1234@gmail.com>
2024-03-15 21:35:22 +0530

    fix:[pd-1130] added shimer for megamenu categories and brands pages

--- src/route/MegaMenuBrands/MegaMenuBrands.component.js
+++ src/route/MegaMenuBrands/MegaMenuBrands.component.js
@@ -1,9 +1,8 @@
-import { useEffect } from "react";
+import { useState } from "react";
 import "./MegaMenuBrands.style.scss";
-import { connect, useDispatch } from "react-redux";
-import CategoriesListDispatcher from "Store/MegaMenuCategoriesList/CategoriesList.dispatcher";
+import { connect } from "react-redux";
 import BrandSelectionGrid from "../../component/MobileMegaMenu/MegaMenuBrands/Components/BrandSelectionGrid/index";
-import BrandSelectionDirectoryList from "../../component/MobileMegaMenu/MegaMenuBrands/Components/BrandSelectionDirectoryList"
+import BrandSelectionDirectoryList from "../../component/MobileMegaMenu/MegaMenuBrands/Components/BrandSelectionDirectoryList";
 
 export const mapStateToProps = (state) => ({
   gender: state.AppState.gender,
@@ -14,14 +13,66 @@ export const mapStateToProps = (state) => ({
 });
 
 const MegaMenuBrands = (props) => {
-    const { gender } = props;
-  const BrandSelectionGridData = props?.megaMenuBannerAndDynamicSliderData?.[1]?.data || [];
+  const { gender } = props;
+  const [isLoading, setIsLoading] = useState(true);
+  const BrandSelectionGridData =
+    props?.megaMenuBannerAndDynamicSliderData?.[1]?.data || [];
+  const renderBrandSelectionGridShimer = () => {
+    return (
+      <div block="BrandSelectionGridWrapper">
+        <div
+          block="BrandSelectionGridWrapper"
+          elem="BrandSelectionGridCard"
+        ></div>
+        <div
+          block="BrandSelectionGridWrapper"
+          elem="BrandSelectionGridCard"
+        ></div>
+        <div
+          block="BrandSelectionGridWrapper"
+          elem="BrandSelectionGridCard"
+        ></div>
+        <div
+          block="BrandSelectionGridWrapper"
+          elem="BrandSelectionGridCard"
+        ></div>
+        <div
+          block="BrandSelectionGridWrapper"
+          elem="BrandSelectionGridCard"
+        ></div>
+        <div
+          block="BrandSelectionGridWrapper"
+          elem="BrandSelectionGridCard"
+        ></div>
+        <div
+          block="BrandSelectionGridWrapper"
+          elem="BrandSelectionGridCard"
+        ></div>
+        <div
+          block="BrandSelectionGridWrapper"
+          elem="BrandSelectionGridCard"
+        ></div>
+      </div>
+    );
+  };
+
   return (
-    <div block="megamenu-brands-main-container" id="megamenu-brands-main-container-id">
-      <BrandSelectionGrid BrandSelectionGridData={BrandSelectionGridData}/>
-      <BrandSelectionDirectoryList gender={props?.gender}/>
+    <div
+      block="megamenu-brands-main-container"
+      id="megamenu-brands-main-container-id"
+    >
+      {isLoading ? (
+        renderBrandSelectionGridShimer()
+      ) : (
+        <BrandSelectionGrid BrandSelectionGridData={BrandSelectionGridData} />
+      )}
+      <BrandSelectionDirectoryList
+        gender={props?.gender}
+        isLoading={isLoading}
+        setIsLoading={setIsLoading}
+      />
     </div>
   );
 };
 
-export default connect(mapStateToProps, null) (MegaMenuBrands);
+export default connect(mapStateToProps, null)(MegaMenuBrands);
