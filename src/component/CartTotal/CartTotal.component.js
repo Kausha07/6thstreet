c47f6d7837e9d5cefd3bd64c28b98b9784698367
pratikjadhav718 <pratikjadhav718@gmail.com>
2024-04-19 13:30:04 +0530

    fix: [POE-1690] issue in International shipping fee

--- src/component/CartTotal/CartTotal.component.js
+++ src/component/CartTotal/CartTotal.component.js
@@ -6,6 +6,8 @@ import { getFinalPrice } from "Component/Price/Price.config";
 import { getCurrency, isArabic as checkIsArabic } from "Util/App";
 import { getSideWideSavingPercentages } from "Component/SideWideCoupon/utils/SideWideCoupon.helper";
 import { getCountryFromUrl } from "Util/Url";
+import { getShippingFees } from "Util/Common/index";
+import { getLocaleFromUrl } from "Util/Url/Url";
 
 export const mapDispatchToProps = (dispatch) => ({
   getCart: () => CartDispatcher.getCart(dispatch, false, false),
@@ -16,20 +18,36 @@ export const mapStateToProps = (state) => {
     totals: state.CartReducer.cartTotals,
     isSignedIn: state.MyAccountReducer.isSignedIn,
     language: state.AppState.language,
+    international_shipping_fee: state.AppConfig.international_shipping_fee,
   };
 };
 
 function CartTotal(props) {
   const {
-    totals: { total_segments: totals = [], total = 0, },
+    totals: {
+      total_segments: totals = [],
+      total = 0,
+      items = [],
+      shipping_fee = 0,
+      international_shipping_amount = 0,
+    },
     block,
     pageType,
     cashOnDeliveryFee,
+    international_shipping_fee,
   } = props;
-
   const isArabic = checkIsArabic();
   const currency_code = getCurrency();
-  const isInternationalShipment = getValueFromTotals(totals, "intl_shipping");
+  let inventory_level_cross_border = false;
+  items.map((item) => {
+    if (
+      item.full_item_info &&
+      item.full_item_info.cross_border &&
+      parseInt(item.full_item_info.cross_border) > 0
+    ) {
+      inventory_level_cross_border = true;
+    }
+  });
   const CODFee = getValueFromTotals(totals, "msp_cashondelivery") || 0;
   const grandTotal =
   total > CODFee  || !cashOnDeliveryFee
@@ -71,6 +89,53 @@ function CartTotal(props) {
     );
   };
 
+  const renderPriceLineForShipping = (price, name, mods, allowZero = false) => {
+    const {
+      totals: { currency_code = getCurrency() },
+    } = props;
+    const locale = getLocaleFromUrl();
+    const [lang, country] = locale && locale.split("-");
+    const finalPrice = getFinalPrice(price, currency_code);
+    const shippingFee = parseInt(finalPrice)
+      ? finalPrice
+      : getShippingFees(country);
+
+    if (parseInt(finalPrice) === 0) {
+      return (
+        <li block="CartPage" elem="SummaryItem" mods={mods}>
+          <strong block="CartPage" elem="Text">
+            {name}
+          </strong>
+          <span>
+            {name !== __("International Shipping Fee") && (
+              <strong block="CartPage" elem="Price">
+                <del block="freeShipping" mods={{ isArabic }}>
+                  {`${
+                    parseFloat(price) || price === 0 ? currency_code : ""
+                  } ${shippingFee}`}
+                </del>
+              </strong>
+            )}
+            {__("FREE")}
+          </span>
+        </li>
+      );
+    }
+
+    return (
+      <li block="CartPage" elem="SummaryItem" mods={mods}>
+        <strong block="CartPage" elem="Text">
+          {name}
+        </strong>
+        <strong block="CartPage" elem="Price">
+          {`${
+            parseFloat(price) || price === 0 ? currency_code : ""
+          } ${shippingFee}`}
+        </strong>
+      </li>
+    );
+  };
+
   return (
     <div block={block} elem="OrderTotals">
       <h3 block="OrderTotalsHeading">{__("ORDER DETAILS")}</h3>
@@ -85,18 +150,15 @@ function CartTotal(props) {
             __("Coupon Savings"),
             { couponSavings: true }
           )}
-          {!isInternationalShipment
-            ? renderPriceLine(
-                getValueFromTotals(totals, "shipping") || __("FREE"),
-                __("Shipping Fee")
-              )
+          {!inventory_level_cross_border || !international_shipping_fee
+            ? renderPriceLineForShipping(shipping_fee, __("Shipping fee"))
             : null}
-          {isInternationalShipment
-            ? renderPriceLine(
-                getValueFromTotals(totals, "intl_shipping") || __("FREE"),
-                __("International Shipping Fee")
-              )
-            : null}
+          {international_shipping_fee &&
+            inventory_level_cross_border &&
+            renderPriceLineForShipping(
+              international_shipping_amount,
+              __("International Shipping Fee")
+            )}
           {renderPriceLine(
             getValueFromTotals(totals, "customerbalance"),
             __("Store Credit"),
