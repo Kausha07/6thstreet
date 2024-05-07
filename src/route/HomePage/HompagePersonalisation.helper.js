18bb07ec478dca61e1abdd08a72d6600b65f6335
kirankumar-medikurthy <kiran1999m1234@gmail.com>
2024-05-07 14:30:02 +0530

    feat:[pd-1259] added fallback url for not found user segment

--- src/route/HomePage/HompagePersonalisation.helper.js
+++ src/route/HomePage/HompagePersonalisation.helper.js
@@ -5,19 +5,36 @@ import Event, {
   MOE_trackEvent,
 } from "Util/Event";
 
-export const getHomePagePersonalizationJsonFileUrl = (
+export const getHomePagePersonalizationContent = async (
   devicePrefix,
   gender,
   customer,
-  abTestingConfig
+  abTestingConfig,
+  getStaticFile,
+  HOME_STATIC_FILE_KEY,
 ) => {
   const userSegementValue = customer?.user_segment
     ? customer.user_segment
     : abTestingConfig?.HPP?.defaultUserSegment;
-  if (userSegementValue) {
-    return `${devicePrefix}${userSegementValue}_${gender}.json`;
+
+    const fileName = userSegementValue
+      ? `${devicePrefix}${userSegementValue}_${gender}.json`
+      : `${devicePrefix}${gender}.json`;
+  /** Adding Fallback user segement and fallback filename, if user segment other that new_user or repeat_user*/
+  const fallBackUserSegment = abTestingConfig?.HPP?.defaultUserSegment || "new_user";
+  const fallBackFileName = `${devicePrefix}${fallBackUserSegment}_${gender}.json`;
+  let dynamicContent = [];
+  try {
+    dynamicContent = await getStaticFile(HOME_STATIC_FILE_KEY, {
+      $FILE_NAME: fileName,
+    });
+  }catch(e) {
+    dynamicContent = await getStaticFile(HOME_STATIC_FILE_KEY, {
+      $FILE_NAME: fallBackFileName,
+    });
+    console.error(e);
   }
-  return `${devicePrefix}${gender}.json`;
+  return dynamicContent;
 };
 
 
