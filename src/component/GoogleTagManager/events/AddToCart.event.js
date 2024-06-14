6f0ba4ac69e542cfb70e9a64c0114d8902dc8bd6
niteshpoojary <nitesh.pujary@6thstreet.com>
2024-06-14 11:58:59 +0530

    feat:[PD-1396] add_to_cart and view_item GA events changes

--- src/component/GoogleTagManager/events/AddToCart.event.js
+++ src/component/GoogleTagManager/events/AddToCart.event.js
@@ -51,6 +51,12 @@ class AddToCartEvent extends BaseEvent {
     this.pushEventData({
       sha256_email: sha_email,
       sha256_phone_number: sha_phone,
+      product_rating :product?.product_rating ?? "",
+      no_of_ratings :product?.no_of_ratings ?? "",
+      is_flash_sale :product?.is_flash_sale ?? "",
+      user_view_count :product?.user_view_count ?? "",
+      user_added_to_cart_count :product?.user_added_to_cart_count ?? "",
+      is_following_brand :product?.is_following_brand ?? "",
       ecommerce: {
         currency: this.getCurrencyCode(),
         currencyCode:  this.getCurrencyCode(),
@@ -73,12 +79,6 @@ class AddToCartEvent extends BaseEvent {
             item_size: product?.size ?? "",
             item_size_type : product?.size_id ?? "",
             variant_availability :product?.variant_availability ?? "",
-            product_rating :product?.product_rating ?? "",
-            no_of_ratings :product?.no_of_ratings ?? "",
-            is_flash_sale :product?.is_flash_sale ?? "",
-            user_view_count :product?.user_view_count ?? "",
-            user_added_to_cart_count :product?.user_added_to_cart_count ?? "",
-            is_following_brand :product?.is_following_brand ?? "",
           }
         ]
       },
