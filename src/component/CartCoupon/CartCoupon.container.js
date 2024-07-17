3002ee760fed5cafd63f8668bd7434b99fcc8e62
pratikjadhav718 <pratikjadhav718@gmail.com>
2024-07-17 13:21:35 +0530

    fix: [BETA-76] remove coupon throwing an Internal server error

--- src/component/CartCoupon/CartCoupon.container.js
+++ src/component/CartCoupon/CartCoupon.container.js
@@ -5,7 +5,12 @@ import CartDispatcher from 'Store/Cart/Cart.dispatcher';
 
 export const mapDispatchToProps = (dispatch) => ({
     applyCouponToCart: (couponCode) => CartDispatcher.applyCouponCode(dispatch, couponCode),
-    removeCouponFromCart: () => CartDispatcher.removeCouponCode(dispatch)
+    removeCouponFromCart: (data={}) => CartDispatcher.removeCouponCode(dispatch, data)
 });
 
-export default connect(null, mapDispatchToProps)(SourceCartCouponContainer);
+export const mapStateToProps = (state) => ({
+    isSignedIn: state.MyAccountReducer.isSignedIn,
+    config: state.AppConfig.config,
+  });
+
+export default connect(mapStateToProps, mapDispatchToProps)(SourceCartCouponContainer);
